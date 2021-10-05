8180ebbe731e59c4f27462d06ccb757621f4510e
LeoFuna <leo.funa@gmail.com>
2021-10-05 10:30:39 -0300

    task 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,4 +15,29 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+async function readOneFile(filePath) {
+  try {
+    const data = await fs.readFile(filePath, 'utf-8');
+    const dataNotString = JSON.parse(data);
+    return dataNotString;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+app.get('/talker', async (_req, res) => {
+  const talkers = await readOneFile('./talker.json');
+  return res.status(200).json(talkers);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const talkers = await readOneFile('./talker.json');
+  const { id } = req.params;
+  const filteredTalker = talkers.find((talker) => Number(talker.id) === Number(id));
+  if (!filteredTalker) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return res.status(200).json(filteredTalker);
 });

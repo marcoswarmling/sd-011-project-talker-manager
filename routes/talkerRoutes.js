c66d2256230d2472505d7117f014dd4b7899d3b0
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-10-05 10:30:41 -0300

    Add requirement 4

--- routes/talkerRoutes.js
+++ routes/talkerRoutes.js
@@ -1,5 +1,12 @@
 const express = require('express');
-const readFile = require('../utils/useFiles');
+const { readFile, writeFile } = require('../utils/useFiles');
+const {
+  validateToken,
+  validateTalkerName,
+  validateTalkerAge,
+  validateTalkBody,
+  validateTalkeBodyInfos,
+} = require('../middlewares/validateNewTalker');
 
 const router = express.Router();
 
@@ -7,7 +14,7 @@ router.get(
   '/',
   async (_req, res) => {
     try {
-      const talkers = await readFile('./talker.json');
+      const talkers = await readFile();
 
       if (talkers.length === 0) {
         return res.status(200).json([]);
@@ -26,7 +33,7 @@ router.get(
     try {
       const { id } = req.params;
   
-      const talkers = await readFile('./talker.json');
+      const talkers = await readFile();
   
       const findTalker = talkers.find((talker) => talker.id === Number(id));
   
@@ -35,6 +42,33 @@ router.get(
       }
   
       res.status(200).json(findTalker);
+    } catch (err) {
+      return res.status(404).json({ code: err.code, message: err.message });
+    }
+  },
+);
+
+router.post(
+  '/',
+  validateToken,
+  validateTalkerName,
+  validateTalkerAge,
+  validateTalkBody,
+  validateTalkeBodyInfos,
+  async (req, res) => {
+    try {
+      const { name, age, talk } = req.body;
+
+      const talkers = await readFile();
+      const lastId = talkers[talkers.length - 1].id;
+
+      console.log(lastId);
+
+      const newTalkers = [...talkers, { name, age, id: lastId + 1, talk }];
+
+      await writeFile(newTalkers);
+
+      res.status(201).json({ name, age, id: lastId + 1, talk });
     } catch (err) {
       return res.status(404).json({ code: err.code, message: err.message });
     }

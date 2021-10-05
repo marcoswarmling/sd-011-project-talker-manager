aaa91022808121a7a0fc7a76fad24a24f447b9b0
marcoswarmling <marcoswarmling15@gmail.com>
2021-10-05 17:49:09 -0300

    requisito 3

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const talkerRouter = require('./routers/getTalker');
+const loginRouter = require('./routers/login');
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,6 +15,7 @@ app.get('/', (_request, response) => {
 });
 
 app.use('/talker', talkerRouter);
+app.use('/login', loginRouter);
 
 app.listen(PORT, () => {
   console.log('Online');

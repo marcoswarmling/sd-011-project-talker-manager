9bcf2243a8616c0154ede1decc1129873687e700
rodrigoruan <rodrigopython16@gmail.com>
2021-10-05 15:53:39 -0300

    Added exercise 3

--- talker/talker.js
+++ talker/talker.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
 const express = require('express');
+const crypto = require('crypto');
 
 const router = express.Router();
+
+const loginValidations = require('../validations/login');
 
 // 1
 router.get('/talker', (_req, res) => {
@@ -21,10 +24,24 @@ router.get('/talker/:id', (req, res) => {
   const findTalker = response.find((talker) => talker.id === Number(id));
 
   if (!findTalker) {
-    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    return res
+      .status(404)
+      .json({ message: 'Pessoa palestrante não encontrada' });
   }
 
   res.status(200).json(findTalker);
 });
+
+// 3
+router.post(
+  '/login',
+  loginValidations.validateEmail,
+  loginValidations.validatePassword,
+  (_req, res) => {
+    const token = crypto.randomBytes(8).toString('hex');
+    
+    res.status(200).json({ token });
+  },
+);
 
 module.exports = router;

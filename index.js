25a344d24ed549959205a649a3ecc2f375878a19
LeoFuna <leo.funa@gmail.com>
2021-10-05 11:15:10 -0300

    Requisito 3

--- index.js
+++ index.js
@@ -41,3 +41,20 @@ app.get('/talker/:id', async (req, res) => {
   }
   return res.status(200).json(filteredTalker);
 });
+
+function validateLogin(req, res, next) {
+  const { email, password } = req.body;
+  if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  if (!/^\S+@\S+\.\S+$/.test(email)) { // expressão encontrada em https://stackoverflow.com/questions/201323/how-can-i-validate-an-email-address-using-a-regular-expression
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  next();
+}
+
+app.post('/login', validateLogin, (_req, res) => res.status(200).json(
+  { token: '7mqaVRXJSp886CGr' },
+));

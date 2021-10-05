a2d4af26334d51651855653ccbc235f55fbc36df
Ghislaine <glenox79@gmail.com>
2021-10-05 15:53:26 -0300

    01 - getAllTalkers

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+// const express = require('express');
+// const bodyParser = require('body-parser');
 
-const app = express();
-app.use(bodyParser.json());
+const customExpress = require('./config/customExpress');
+
+const app = customExpress();
+// app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';

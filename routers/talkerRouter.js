dff6371caa3990e6487e34ebb950c79ca1a6cc64
Marcelo <marsleite@gmail.com>
2021-10-05 20:57:39 -0300

    requirement 2

--- routers/talkerRouter.js
+++ routers/talkerRouter.js
@@ -11,4 +11,15 @@ router.get('/talker', async (_req, res) => {
   return res.status(200).json(talker);
 });
 
+router.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const data = await fs.readFile('./talker.json', 'utf8');
+  const talker = JSON.parse(data);
+  const response = talker.find((r) => r.id === Number(id));
+
+  if (!response) return res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
+  
+  return res.status(200).json(response);
+});
+
 module.exports = router;

6734032152b62ccb1d962f99bf55dcad1bec2af6
Gabriel Pimentel Fernandes <gabrielfernandessilva.ba@gmail.com>
2021-10-05 15:14:02 -0300

    Terceiro Requisito

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -39,7 +39,59 @@ const getTalkerId = async (req, res) => {
   } 
 };
 
+const setValidEmail = (req, res, next) => {
+  const { email } = req.body;
+
+  if (!email || email === '') {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+
+  if (!email.includes('@') || !email.includes('.com')) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+
+  next();
+};
+
+const setValidPassword = (req, res, next) => {
+  const { password } = req.body;
+
+  if (!password || password === '') {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+
+  next();
+};
+
+const createToken = (_req, res) => {
+  const newToken = '1234567abdertygh';
+  
+  return res.status(200).json({ token: `${newToken}` });
+};
+
+const generateToken = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+  
+  if (token.length !== 16) {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+
+  next();
+};
+
 module.exports = {
   getTalkers,
   getTalkerId,
+  setValidPassword,
+  setValidEmail,
+  createToken,
+  generateToken,
 };
--- router/validation.js
+++ router/validation.js
@@ -3,8 +3,15 @@ const router = require('express').Router();
 const {
   getTalkers,
   getTalkerId,
+  setValidPassword,
+  setValidEmail,
+  createToken,
  } = require('../middlewares/validation');
 
 router.get('/talker', getTalkers);
 
 router.get('/talker/:id', getTalkerId);
+
+router.post('/login', setValidPassword, setValidEmail, createToken);
+
+module.exports = router;

b3fcdbbed44e40784f2cc83bbd9d71625f62e6f1
Tales Coelho dos Santos <tales-coelho@live.com>
2021-10-05 15:26:08 -0300

    requisito 4 ao 6

--- middleware/validation.js
+++ middleware/validation.js
@@ -1,8 +1,10 @@
-const fs = require('fs').promises;
+const fs = require('fs');
+
+const path = './talker.json';
 
 const getTalker = async (req, res) => {
   try {
-    const talker = await fs.readFile('./talker.json', 'utf8');
+    const talker = await fs.readFileSync(path, 'utf8');
     if (!talker) {
       return res.status(200).json([]);
     }
@@ -14,7 +16,7 @@ const getTalker = async (req, res) => {
 
 const getTalkerId = async (req, res) => {
   try {
-    const talker = await fs.readFile('./talker.json', 'utf8');
+    const talker = await fs.readFileSync(path, 'utf8');
     if (!talker) {
       return res.status(200).json([]);
     }
@@ -62,65 +64,104 @@ const postLogin = async (req, res) => {
   res.status(200).json({ token: '7mqaVRXJSp886CGr' });
 };
 
-function validToken(req, res, next) {
+// req 4 --------
+const validToken = (req, res, next) => {
   const token = req.headers.authorization;
   if (!token) {
     return res.status(401).json({ message: 'Token não encontrado' });
   }
-  if (!token.length !== 16) {
+  if (token.length !== 16) {
     return res.status(401).json({ message: 'Token inválido' });
   }
   next();
-}
+};
 
-function validName(req, res, next) {
+const validName = (req, res, next) => {
   const { name } = req.body;
   if (!name) {
     return res.status(400).json({ message: 'O campo "name" é obrigatório' });
   }
-  if (!name.length < 3) {
+  if (name.length < 3) {
     return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
   }
   next();
-}
+};
 
-function validAge(req, res, next) {
+const validAge = (req, res, next) => {
   const { age } = req.body;
   if (!age) {
     return res.status(400).json({ message: 'O campo "age" é obrigatório' });
   }
-  if (!age.length < 18) {
+  if (Number(age) < 18) {
     return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
   }
   next();
+};
+
+function verifyRate(rate) {
+  return rate < 1 || rate > 5;
+}
+
+function verifyTalkItems(talk) {
+  return !talk || (talk.rate !== 0 && !talk.rate) || !talk.watchedAt;
 }
 
 function validTalk(req, res, next) {
   const { talk } = req.body;
-  if (!talk || [!talk.rate, !talk.watchedAt].includes(true)) {
-    return res.status(400).json({ message: 'rio e "watchedAt" e "rate" não podem ser vazios' });
+
+  if (verifyTalkItems(talk)) {
+    return res.status(400)
+    .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
   }
 
   if (!/\d{2}\/\d{2}\/\d{4}$/.test(talk.watchedAt)) {
     return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
   }
 
-  if (!/^[1-5]$/.test(talk.rate)) {
-    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  if (verifyRate(talk.rate)) {
+    return res.status(400).json({
+      message: 'O campo "rate" deve ser um inteiro de 1 à 5',
+    });
   }
+
   next();
 }
 
 const postTalker = async (req, res) => {
+    const { name, age, talk } = req.body;
+    const talker = JSON.parse(fs.readFileSync(path, 'utf8'));
+    const id = talker.reduce((acc, val) => Math.max(acc, val.id), 0) + 1;
+    const newTalk = {
+      id,
+      name,
+      age,
+      talk,
+    };
+    res.status(201).json(newTalk);
+    fs.writeFileSync(path, JSON.stringify([...talker, newTalk]));
+};
+
+const putTalker = async (req, res) => {
   const { name, age, talk } = req.body;
-  const talker = await JSON.parse(fs.readFile('./talker.json', 'utf8'));
-  const id = talker.reduce((acc, val) => Math.max(acc, val), 0);
-  return res.status(201).json({
-    id,
+  const { id } = req.params;
+  const talker = JSON.parse(fs.readFileSync(path, 'utf8'));
+  const newTalk = {
+    id: Number(id),
     name,
     age,
     talk,
-  });
+  };
+  const filterTalkers = [...talker.filter((val) => val.id !== Number(id)), newTalk];
+  res.status(200).json(newTalk);
+  fs.writeFileSync(path, JSON.stringify(filterTalkers));
+};
+
+const deleteTalker = async (req, res) => {
+  const { id } = req.params;
+  const talker = JSON.parse(fs.readFileSync(path, 'utf8'));
+  const filterTalkers = talker.filter((val) => val.id !== Number(id));
+  res.status(200).json({ message: 'Pessoa palestrante deletada com sucesso' });
+  fs.writeFileSync(path, JSON.stringify(filterTalkers));
 };
 
 module.exports = {
@@ -132,4 +173,6 @@ module.exports = {
   validAge,
   validTalk,
   postTalker,
+  putTalker,
+  deleteTalker,
 };
--- router/validation.js
+++ router/validation.js
@@ -8,11 +8,17 @@ const {
   validAge,
   validTalk,
   postTalker,
+  putTalker,
+  deleteTalker,
+  searchTalker,
 } = require('../middleware/validation');
 
 router.get('/talker', getTalker);
 router.get('/talker/:id', getTalkerId);
 router.post('/login', postLogin);
-router.post('/talker', validName, validAge, validTalk, validToken, postTalker);
+router.post('/talker', validToken, validName, validAge, validTalk, postTalker);
+router.put('/talker/:id', validToken, validName, validAge, validTalk, putTalker);
+router.delete('/talker/:id', validToken, deleteTalker);
+router.get('/talker/search?q=searchTerm', validToken, searchTalker);
 
 module.exports = router;

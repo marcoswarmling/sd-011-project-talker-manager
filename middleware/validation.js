93e26ff007aaa5c2222767a38ec287351750d9e9
Tales Coelho dos Santos <tales-coelho@live.com>
2021-10-05 14:27:03 -0300

    req 3

--- middleware/validation.js
+++ middleware/validation.js
@@ -29,4 +29,107 @@ const getTalkerId = async (req, res) => {
   }
 };
 
-module.exports = { getTalker, getTalkerId };
+function validEmail(email) {
+  if (!email) {
+    return 'O campo "email" é obrigatório';
+  }
+  if (!email.includes('@') || !email.includes('.com')) {
+    return 'O "email" deve ter o formato "email@email.com"';
+  }
+  return false;
+}
+
+function validPassword(password) {
+  if (!password) {
+    return 'O campo "password" é obrigatório';
+  }
+  if (password.length < 6) {
+    return 'O "password" deve ter pelo menos 6 caracteres';
+  }
+  return false;
+}
+
+const postLogin = async (req, res) => {
+  const { email, password } = req.body;
+  const emailValid = validEmail(email);
+  const passwordValid = validPassword(password);
+  if (emailValid) {
+    return res.status(400).json({ message: emailValid });
+  }
+  if (passwordValid) {
+    return res.status(400).json({ message: passwordValid });
+  }
+  res.status(200).json({ token: '7mqaVRXJSp886CGr' });
+};
+
+function validToken(req, res, next) {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+  if (!token.length !== 16) {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+  next();
+}
+
+function validName(req, res, next) {
+  const { name } = req.body;
+  if (!name) {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
+  if (!name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  next();
+}
+
+function validAge(req, res, next) {
+  const { age } = req.body;
+  if (!age) {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (!age.length < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  next();
+}
+
+function validTalk(req, res, next) {
+  const { talk } = req.body;
+  if (!talk || [!talk.rate, !talk.watchedAt].includes(true)) {
+    return res.status(400).json({ message: 'rio e "watchedAt" e "rate" não podem ser vazios' });
+  }
+
+  if (!/\d{2}\/\d{2}\/\d{4}$/.test(talk.watchedAt)) {
+    return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+
+  if (!/^[1-5]$/.test(talk.rate)) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  next();
+}
+
+const postTalker = async (req, res) => {
+  const { name, age, talk } = req.body;
+  const talker = await JSON.parse(fs.readFile('./talker.json', 'utf8'));
+  const id = talker.reduce((acc, val) => Math.max(acc, val), 0);
+  return res.status(201).json({
+    id,
+    name,
+    age,
+    talk,
+  });
+};
+
+module.exports = {
+  getTalker,
+  getTalkerId,
+  postLogin,
+  validToken,
+  validName,
+  validAge,
+  validTalk,
+  postTalker,
+};
--- router/validation.js
+++ router/validation.js
@@ -2,9 +2,17 @@ const router = require('express').Router();
 const {
   getTalker,
   getTalkerId,
+  postLogin,
+  validToken,
+  validName,
+  validAge,
+  validTalk,
+  postTalker,
 } = require('../middleware/validation');
 
 router.get('/talker', getTalker);
 router.get('/talker/:id', getTalkerId);
+router.post('/login', postLogin);
+router.post('/talker', validName, validAge, validTalk, validToken, postTalker);
 
 module.exports = router;

7c0e9c11131b624f0c10a23ebde990ead94929ef
Amandha W. Barok <amandha.barok@gmail.com>
2021-10-05 11:42:13 -0700

    first and second requiriment done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const fs = require('fs');
 const app = express();
 app.use(bodyParser.json());
 
@@ -14,4 +14,35 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+// fs.readFile('./talker.json', (err, content) => {
+//   if(err) {
+//     console.log(`Erro ao ler o arquivo: ${err.message}`);
+//     return;
+//   }
+//   let contentFile = content.toString('utf8');
+//   talkers = JSON.parse(contentFile);
+// });
+
+function getTalkers() {
+  try {
+    const data = fs.readFileSync('./talker.json', 'utf-8');
+    return JSON.parse(data);
+  } catch (err) {
+    console.log(`Erro ao ler o arquivo: ${err}`);
+  }
+}
+
+app.get('/talker', function (req, res) {
+  let talkers = getTalkers();
+  res.status(200).json(talkers);
+});
+
+app.get('/talker/:id', function (req, res) {
+  let talkers = getTalkers();
+  const { id } = req.params;
+  const talker = talkers.find((t) => t.id === parseInt(id));
+  if(!talker) return res.status(404).json({ "message": "Pessoa palestrante não encontrada" });
+  res.status(200).json(talker);
 });

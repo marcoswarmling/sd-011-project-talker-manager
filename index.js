8b71a668b6cb6098df5f9937604be82619cc4c2d
filizzola <juliofilizzola@hotmail.com>
2021-10-05 09:13:15 -0300

    desafio 2

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const messages = 'Pessoa palestrante não encontrada';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -19,6 +20,22 @@ app.get('/talker', async (req, res) => {
     if (!response) return res.status(HTTP_OK_STATUS).json([]);
 
     res.status(HTTP_OK_STATUS).json(JSON.parse(response));
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+app.get('/talker/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const talker = await fs.readFile('./talker.json', 'utf8');
+    const result = JSON.parse(talker);
+    const response = result.find((value) => value.id === Number(id));
+
+    if (!response) return res.status(404).json({ message: messages });
+
+    res.status(HTTP_OK_STATUS).json(response);
   } catch (err) {
     res.status(400).json(err);
   }

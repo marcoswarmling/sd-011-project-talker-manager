e8d3a78990a2d88c57e4c36a94846e2de0fd8014
Marcelo <marsleite@gmail.com>
2021-10-05 14:24:52 -0300

    req 4 - done

--- routes/talkerRouter.js
+++ routes/talkerRouter.js
@@ -3,6 +3,13 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs').promises;
 
+const { 
+  withOutName,
+  tokenValid,
+  ageValid,
+  talkValid,
+} = require('../middlewares/createTalkerValid');
+
 router.get('/', async (_req, res) => {
   try {
     const data = await fs.readFile('./talker.json', 'utf8');
@@ -22,6 +29,20 @@ router.get('/:id', async (req, res) => {
     if (!response) return res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
 
     return res.status(200).json(response);
+});
+
+router.post('/', withOutName, tokenValid, ageValid, talkValid, async (req, res) => {
+  const { name, age, talk } = req.body;
+
+  const data = await fs.readFile('./talker.json', 'utf8');
+  const talker = JSON.parse(data);
+
+  const id = talker.length + 1;
+
+  talker.push({ id, name, age, talk });
+  const writeFile = (content) => fs.writeFile('./talker.json', JSON.stringify(content));
+  writeFile(talker);
+  res.status(201).json({ name, age, talk, id });
 });
 
 module.exports = router;

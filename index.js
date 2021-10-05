cd1aded0835c293d01c9100d3e4f85f83adba6ed
Marco Galindo <marco1408@gmail.com>
2021-10-05 14:41:55 -0300

    começando o projeto

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,21 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (req, res) => {
+  fs.readFile('talker.json', 'utf8')
+    .then((data) => res.status(200).send(data))
+    .catch((err) => console.log(err));
+});
+
+app.get('/talker/:id', (req, res) => {
+  const { id } = req.params;
+  fs.readFile('talker.json', 'utf8')
+    .then((data) => data.json())
+    .then((response) => response.find((e) => e.id === parseInt(id, 10)))
+    .then((talker) => res.status(200).send(talker))
+    .catch((err) => console.log(err));
 });
 
 app.listen(PORT, () => {

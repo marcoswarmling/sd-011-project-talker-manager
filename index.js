ffc4a986747c0a49f79e202a5899b49344a4152b
Thyago Pessoa <flash_thyago@hotmail.com>
2021-10-05 14:52:48 -0300

    Desafio 1

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+app.get('/talker', async (_req, res) => {
+  const response = JSON.parse(await fs.readFile('talker.json', 'utf8'));
+  if (response.length < 1) return res.status(200).json([]);
+  res.status(200).json(response);
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {

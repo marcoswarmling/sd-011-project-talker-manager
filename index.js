c977961facd6be085d0bf7c174dfd492d6337c84
Vinicius <ViniciusAlvesRocha>
2021-10-05 16:53:02 -0300

    attempt fixing challenge 1

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+
+const talkerJSON = './talker.json';
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +13,24 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+// Requisito 2:
+/*app.get('/talker/:id', (req, _res) => {
+  const { id } = req.params;
+  talkers.find((talker) => talker.id === id);
+});
+*/
+// Requisito 1:
+app.get('/talker', async (_req, res) => {
+  const talkers = await fs.readFile(talkerJSON, 'utf-8', (error, data) => {
+    if (error) {
+      console.log('Error!');
+    }
+    return data;
+  });
+
+  res.status(200).json(talkers);
 });
 
 app.listen(PORT, () => {

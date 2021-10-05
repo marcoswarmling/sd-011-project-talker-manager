ad59b19bba9f6f9ab54997c86039712422ff0c37
jamneto <netomoura22@gmail.com>
2021-10-05 19:42:05 -0300

    cump do req 2

--- talkerRoutes.js
+++ talkerRoutes.js
@@ -7,4 +7,15 @@ router.get('/', async (_req, res) => {
   res.status(200).json(result);
 });
 
+router.get('/:id', async (req, res) => {
+  const talkers = await fs.readFile('./talker.json');
+  const { id } = req.params;
+  const result = JSON.parse(talkers);
+
+  const talker = result.find((t) => t.id === parseInt(id, 10));
+
+  if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  return res.status(200).json(talker);
+});
+
 module.exports = router;

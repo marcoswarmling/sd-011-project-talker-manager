ab61ef429469bbe22626c4478e51a7316af82d8d
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-10-05 14:07:12 -0300

    Requisito 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,17 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+// 1 - Crie o endpoint GET /talker
+app.get('/talker', (req, res) => {
+  const data = JSON.parse(fs.readFileSync('talker.json', 'utf-8'));
+
+  // Caso não exista nenhuma pessoa palestrante cadastrada o endpoint deve retornar um array vazio e o status 200.
+  if (data.length === 0) return res.status(200).json([]);
+
+  // O endpoint deve retornar um array com todas as pessoas palestrantes cadastradas.
+    res.status(200).json(data);
 });
 
 app.listen(PORT, () => {

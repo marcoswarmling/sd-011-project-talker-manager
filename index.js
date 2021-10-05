05d140994c476d14d5ff1ea101bfbad2d8ec06b8
Tulio Sploradori <tuliospl@gmail.com>
2021-10-05 16:27:42 -0300

    Teste

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const talker = require('./talker');
+
+const talker = [
+  {
+    name: 'Henrique Albuquerque',
+    age: 62,
+    id: 1,
+    talk: {
+      watchedAt: '23/10/2020',
+      rate: 5,
+    },
+  },
+  {
+    name: 'Heloísa Albuquerque',
+    age: 67,
+    id: 2,
+    talk: {
+      watchedAt: '23/10/2020',
+      rate: 5,
+    },
+  },
+  {
+    name: 'Ricardo Xavier Filho',
+    age: 33,
+    id: 3,
+    talk: {
+      watchedAt: '23/10/2020',
+      rate: 5,
+    },
+  },
+  {
+    name: 'Marcos Costa',
+    age: 24,
+    id: 4,
+    talk: {
+      watchedAt: '23/10/2020',
+      rate: 5,
+    },
+  },
+];
 
 const app = express();
 app.use(bodyParser.json());
@@ -37,8 +75,7 @@ app.get('/talker/:id', (req, response) => {
 
 const validateEmail = (req, res, next) => {
   const { email } = req.body;
-  // eslint-disable-next-line no-useless-escape
-  const reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/;
+  const reg = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/;
   if (!email) {
     res.status(404).send({
       message: 'O campo \'email\' é obrigatório',

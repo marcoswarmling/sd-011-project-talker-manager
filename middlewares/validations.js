2b9823d841586872b85db4346011fcb13ad5bd40
rodolforezende <rodolforrcont@gmail.com>
2021-10-05 14:51:03 -0300

    Requisito 4 and Lint Corrections

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -33,6 +33,25 @@ const getToken = (req, res) => {
   res.status(200).json({ token: crypto.randomBytes(8).toString('hex') });
 };
 
+const postTalker = async (req, res) => {
+  const { name, age, talk: { watchedAt, rate } } = req.body;
+  const response = await fs.readFile(jsonFile, 'utf-8');
+  const responseConvert = JSON.parse(response);
+  const dataTalker = {
+    name,
+    age,
+    id: responseConvert.length + 1,
+    talk: {
+      watchedAt,
+      rate,
+    },
+  };
+  responseConvert.push(dataTalker);
+
+  await fs.writeFile(jsonFile, JSON.stringify(responseConvert));
+  res.status(201).send(dataTalker);
+};
+
 const emailValidation = (req, res, next) => {
   const { email } = req.body;
 
@@ -61,9 +80,96 @@ const passwordValidation = (req, res, next) => {
   next();
 };
 
+const tokenValidation = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+
+  if (token.length !== 16) {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+
+  next();
+};
+
+const nameValidation = (req, res, next) => {
+  const { name } = req.body;
+
+  if (!name || name === '') {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+}
+    
+  if (name.length < 3) {
+      return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+
+  next();
+};
+
+const ageValidation = (req, res, next) => {
+  const { age } = req.body;
+
+  if (!age || age === '') {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+}
+    
+  if (parseInt(age, 10) < 18) {
+      return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+
+  next();
+};
+
+const talkValidation = (req, res, next) => {
+  const { talk } = req.body;
+  if (!talk || !talk.watchedAt) {
+    return res
+      .status(400)
+      .json({ 
+        message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
+  }
+
+  next();
+};
+
+const watchedAtValidation = (req, res, next) => {
+  const { talk } = req.body;
+  const regexDate = /^\d{2}\/\d{2}\/\d{4}$/;
+
+  if (!regexDate.test(talk.watchedAt)) {
+    return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  next();
+};
+
+const rateValidation = (req, res, next) => {
+  const { talk } = req.body;
+  
+  if (parseInt(talk.rate, 10) < 1 || parseInt(talk.rate, 10) > 5) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+
+  if (!talk.rate) {
+    return res
+      .status(400)
+      .json({ message: 
+        'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
+  }
+  next();
+};
+
 module.exports = { 
 getTalkers, 
 getTalkerById, 
 getToken, 
 emailValidation, 
-passwordValidation };
+passwordValidation,
+tokenValidation,
+nameValidation,
+ageValidation,
+talkValidation,
+watchedAtValidation,
+rateValidation, 
+postTalker };
--- router/validations.js
+++ router/validations.js
@@ -1,9 +1,29 @@
 const router = require('express').Router();
 
-const { getTalkers, getTalkerById, emailValidation, passwordValidation, getToken, tokenValidation } = require('../middlewares/validations');
+const { 
+getTalkers, 
+getTalkerById, 
+emailValidation, 
+passwordValidation, 
+getToken, 
+tokenValidation, 
+nameValidation, 
+ageValidation, 
+talkValidation, 
+watchedAtValidation, 
+rateValidation, 
+postTalker } = require('../middlewares/validations');
 
 router.get('/talker', getTalkers);
 router.get('/talker/:id', getTalkerById);
 router.post('/login', emailValidation, passwordValidation, getToken);
+router.post('/talker', 
+  tokenValidation, 
+  nameValidation, 
+  ageValidation, 
+  talkValidation, 
+  watchedAtValidation, 
+  rateValidation, 
+  postTalker);
 
 module.exports = router;

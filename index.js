d28a2c4ea155e6b89934f6403b75357e3c3ae02c
Gui Pereira <guilhermepdo1508@gmail.com>
2021-10-05 16:14:20 -0300

    Requisito 1 feito

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const rescue = require('express-rescue');
+const fs = require('fs/promises');
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
@@ -10,6 +11,16 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', rescue(async (_request, response) => {
+  const file = await fs.readFile('./talker.json', 'utf8');
+  if (!file) return response.status(HTTP_OK_STATUS).json([]);
+  response.status(HTTP_OK_STATUS).json(JSON.parse(file));
+}));
+
+app.use((err, _req, res, _next) => {
+  res.status(500).json({ error: `Erro: ${err.message}` });
 });
 
 app.listen(PORT, () => {

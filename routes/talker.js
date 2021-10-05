9c99a4fc156b0927bfa0f2dae392a2bf0da8de5d
Luciano Pimenta <l.a.p.pimenta.j@gmail.com>
2021-10-05 15:56:24 -0300

    Requisito 2 - Concluido

--- routes/talker.js
+++ routes/talker.js
@@ -2,23 +2,19 @@ const router = require('express').Router();
 
 const fs = require('fs').promises;
 
-const validateFileContent = async (_req, res, next) => {
+router.get('/', async (_req, res) => {
   const fileContent = await fs.readFile('./talker.json');
   const people = JSON.parse(fileContent);
-  if (people === []) { 
-    return res.status(200).json([]);
-}
-  next();
-};
+  return res.status(200).json(people);
+});
 
-router.get('/', validateFileContent, async (_req, res, _next) => {
-  try {
-    const fileContent = await fs.readFile('./talker.json');
-    const people = JSON.parse(fileContent);
-    return res.status(200).json(people);
-  } catch (error) {
-    console.log(error);
-  }
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const fileContent = await fs.readFile('./talker.json');
+  const people = JSON.parse(fileContent);
+  const filter = people.find((value) => value.id === Number(id));
+  if (!filter) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  return res.status(200).json(filter);
 });
 
 module.exports = router;

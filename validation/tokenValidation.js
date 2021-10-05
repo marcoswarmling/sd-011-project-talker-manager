e92d9caf0a4c727eac5a9baf3183bbd9516b728f
Arthur Hermann <arthurhermannc@gmail.com>
2021-10-05 09:44:16 -0300

    create token validation

---
+++ validation/tokenValidation.js
@@ -0,0 +1,12 @@
+const tokenValidation = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) return res.status(401).json({ message: 'Token não encontrado' });
+
+  if (authorization !== '7mqaVRXJSp886CGr') {
+    return res.status(401).json({ message: 'Token inválido' });
+  } 
+
+  next();
+};
+
+module.exports = tokenValidation;

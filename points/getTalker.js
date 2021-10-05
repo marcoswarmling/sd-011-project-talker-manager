8958e992f1357e2053abe5c3b13fe24654bf1728
marcoswarmling <marcoswarmling15@gmail.com>
2021-10-05 15:38:09 -0300

    requisito 2

--- points/getTalker.js
+++ points/getTalker.js
@@ -9,8 +9,25 @@ const talkersAll = async () => {
     return JSON.parse(talkers);
   };
 
-router.get('/', async (req, res) => {
+  const talkersFilteredByID = async (sId) => {
+    const talkerFile = './talker.json';
+    const talkers = await fs.readFile(talkerFile);
+    const talkerParse = JSON.parse(talkers);
+   return talkerParse.find(({ id }) => id === sId);
+  };
+
+router.get('/', async (_req, res) => {
     const getTalker = await talkersAll();
+res.status(200).send(getTalker);
+});
+
+router.get('/:id', async (req, res) => {
+const response = req.params;
+const getTalker = await talkersFilteredByID(Number(response.id));
+if (!getTalker) {
+    res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
+}
+
 res.status(200).send(getTalker);
 });
 

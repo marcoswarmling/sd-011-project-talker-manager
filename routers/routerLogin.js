761ce7763f37df71aa4e6d294433ce3a503d1d66
Felipe Buso <felipe_buso@hotmail.com>
2021-10-05 16:46:45 -0300

    requisito 3

---
+++ routers/routerLogin.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const { genRandomToken } = require('../helpers/genTokens');
+const { checkEmail } = require('../helpers/checkEmail');
+
+router.post('/', (req, res) => {
+  const { email, password } = req.body;
+  if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  const checkedEmail = checkEmail(email);
+  if (!checkedEmail) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  if (password.split('').length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+
+  const generatedToken = genRandomToken(16);
+
+  res.status(200).json({ token: generatedToken });
+});
+
+module.exports = router;

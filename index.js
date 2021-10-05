8119df3aabd418c1d18734d1c813257ced5401c3
marlon307 <marlon-ramosb@hotmail.com>
2021-10-05 18:24:07 -0300

    Rq 3

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const talker = require('./routes/talker');
 const talkerID = require('./routes/talkerID');
+const login = require('./routes/login');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(talker);
 app.use(talkerID);
+app.use(login);
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';

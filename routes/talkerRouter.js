a514d627173fa06a739d78e7eb1fded97d1722cb
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-10-05 15:54:29 -0300

    realiza desafio 2

--- routes/talkerRouter.js
+++ routes/talkerRouter.js
@@ -3,6 +3,7 @@ const fs = require('fs').promises;
 
 const data = './talker.json';
 const HTTP_OK_STATUS = 200;
+const HTTP_ERROR_STATUS = 404;
 
 router.get('/', async (_req, res) => {
   const response = await fs.readFile(data, 'utf-8');
@@ -10,6 +11,19 @@ router.get('/', async (_req, res) => {
   if (response.length < 1) return res.status(HTTP_OK_STATUS).send([]);
 
   return res.status(HTTP_OK_STATUS).json(JSON.parse(response));
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const response = await fs.readFile(data, 'utf-8');
+  const result = JSON.parse(response).find((r) => r.id === Number(id));
+
+  if (!result) {
+    return res.status(HTTP_ERROR_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  return res.status(HTTP_OK_STATUS).json(result);
 });
 
 module.exports = router;

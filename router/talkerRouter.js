3d39530c61ab02dafe9ab47ae2213792432b576e
Arthur Hermann <arthurhermannc@gmail.com>
2021-10-05 09:38:35 -0300

    create endpoint get /talker

--- router/talkerRouter.js
+++ router/talkerRouter.js
@@ -3,4 +3,12 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs').promises;
 
+const db = './talker.json';
+
+router.get('/', async (_req, res) => {
+  const response = await fs.readFile(db, 'utf-8');
+  if (response.length === 0) return res.status(200).send([]);
+  return res.status(200).json(JSON.parse(response));
+});
+
 module.exports = router;

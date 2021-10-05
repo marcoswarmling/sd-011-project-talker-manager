b12326fa15fdcf87f93d9ccc12e74393228601ca
Felipe Buso <felipe_buso@hotmail.com>
2021-10-05 16:03:00 -0300

    requisito 2

--- routers/routerTalker.js
+++ routers/routerTalker.js
@@ -9,4 +9,16 @@ router.get('/', async (_req, res) => {
   res.status(200).json(talkers);
 });
 
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const talkers = await getTalkers('talker.json');
+  if (!talkers) res.status(200).json([]);
+
+  const findTalker = talkers.find((t) => t.id === parseInt(id, 10));
+  if (!findTalker) res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+  res.status(200).json(findTalker);
+});
+
 module.exports = router;

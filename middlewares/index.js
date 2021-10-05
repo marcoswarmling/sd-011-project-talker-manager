9e54fdc8352d0ead63400ece7357a84cbf6b3af6
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-10-05 16:18:48 -0300

    requisito 4 concluido

--- middlewares/index.js
+++ middlewares/index.js
@@ -47,8 +47,67 @@ const verifyLogin = (req, res) => {
   res.status(200).json({ token: generateToken() });
 };
 
+const verifyToken = (req, res, next) => {
+  if (!req.headers.authorization) return res.status(401).json({ message: 'Token não encontrado' });
+  if (req.headers.authorization.length !== 16) {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+  next();
+};
+
+const verifyName = (req, res, next) => {
+  if (!req.body.name) return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  if (req.body.name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  next();
+};
+
+const verifyAge = (req, res, next) => {
+  if (!req.body.age) return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  if (Number(req.body.age) < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  next();
+};
+
+const verifyTalkHate = (req, res, next) => {
+  if (req.body.talk.rate > 5 || req.body.talk.rate < 1) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  next();
+};
+
+const verifyTalk = (req, res, next) => {
+  const regex = /^\d{2}\/\d{2}\/\d{4}$/;
+  if (!req.body.talk || !req.body.talk.rate || !req.body.talk.watchedAt) {
+    return res.status(400)
+    .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
+  }
+  if (!regex.test(req.body.talk.watchedAt)) {
+    return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  next();
+};
+
+const addTalker = async (req, res) => {
+  const { name, age, talk } = req.body;
+
+  const array = JSON.parse(await fs.readFile(talker, 'utf-8'));
+
+  array.push({ id: array.length + 1, name, age, talk });
+  fs.writeFile(talker, JSON.stringify(array));
+  res.status(201).json({ id: array.length, name, age, talk });
+};
+
 module.exports = {
   allSpeaker,
   findSpeaker,
   verifyLogin,
+  addTalker,
+  verifyToken,
+  verifyName,
+  verifyAge,
+  verifyTalkHate,
+  verifyTalk,
 };
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,12 @@ const {
   allSpeaker,
   findSpeaker,
   verifyLogin,
+  addTalker,
+  verifyToken,
+  verifyName,
+  verifyAge,
+  verifyTalkHate,
+  verifyTalk,
 } = require('../middlewares/index');
 
 router.get('/talker', allSpeaker);
@@ -10,5 +16,7 @@ router.get('/talker', allSpeaker);
 router.get('/talker/:id', findSpeaker);
 
 router.post('/login', verifyLogin);
+
+router.post('/talker', verifyToken, verifyName, verifyAge, verifyTalk, verifyTalkHate, addTalker);
 
 module.exports = router;

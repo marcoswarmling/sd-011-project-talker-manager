9051722205baf02023626a8355381b89b7a6676e
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-10-05 16:14:24 -0300

    corrige rota do arquivo talker.json

--- router/talkerRoutes.js
+++ router/talkerRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const { readFile } = require('../helper/readWriteHelper');
 
-const PATHNAME = '/home/rafael/Documents/projects/sd-011-project-talker-manager/talker.json';
+const PATHNAME = './talker.json';
 
 router.get('/', async (_req, res) => {
   const result = await readFile(PATHNAME) || [];

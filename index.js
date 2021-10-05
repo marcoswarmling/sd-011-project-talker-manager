b8ca0368993c8d96736f92117aba17398c932c37
Bruna Campos <92brunacampos@gmail.com>
2021-10-05 15:54:13 -0300

    get /talker

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const talkerFile = './talker.json';
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,9 +10,24 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+// --------------------------------------------------------//
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+// --------------------------------------------------------//
+
+// test getAllTalkers.test.js
+app.get('/talker', (_req, res) => {
+  const talker = JSON.parse(fs.readFileSync(talkerFile, 'utf8'));
+  
+  if (talker.length > 0) {
+    return res.status(200).json(talker);
+  }
+  
+  if (talker.length === 0) {
+    return res.status(200).json([]);
+  }
 });
 
 app.listen(PORT, () => {

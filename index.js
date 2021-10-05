d494cd5976a089e25d3f67074b5cdedac57cdbac
LeoFuna <leo.funa@gmail.com>
2021-10-05 14:02:35 -0300

    Requisito 4

--- index.js
+++ index.js
@@ -55,6 +55,59 @@ function validateLogin(req, res, next) {
   next();
 }
 
-app.post('/login', validateLogin, (_req, res) => res.status(200).json(
-  { token: '7mqaVRXJSp886CGr' },
-));
+const crypto = require('crypto');
+
+app.post('/login', validateLogin, (_req, res) => {
+  const token = crypto.randomBytes(8).toString('hex');
+  return res.status(200).json({ token });
+  });
+
+const validateToken = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) return res.status(401).json({ message: 'Token não encontrado' });
+  if (authorization.length !== 16) return res.status(401).json({ message: 'Token inválido' });
+  next();
+};
+
+const validateNameAge = (req, res, next) => {
+  const { name, age } = req.body;
+  if (!name) return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  if (!age) return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  if (Number(age) < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  next();
+};
+
+const validateExistTalk = (req, res, next) => {
+  const { talk } = req.body;
+  if (!talk || !talk.watchedAt || !talk.rate) {
+    return res.status(400).json(
+      { message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' },
+    );
+  }
+  next();
+};
+
+function validateTalk(req, res, next) {
+  const { talk } = req.body;
+  if (!/..\/..\/..../.test(talk.watchedAt)) {
+    return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  if (talk.rate < 1 || talk.rate > 5) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  next();
+}
+
+app.post('/talker', validateToken, validateNameAge, validateExistTalk, validateTalk, 
+async (req, res) => {
+  const { name, age, talk } = req.body;
+  const talkers = await readOneFile('./talker.json');
+  const newTalkers = [...talkers, { id: talkers.length + 1, name, age, talk }];
+  await fs.writeFile('./talker.json', JSON.stringify(newTalkers));
+  res.status(201).json({ id: talkers.length + 1, name, age, talk });
+});

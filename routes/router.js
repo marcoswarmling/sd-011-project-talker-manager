d4ebef3017abc40009dc0e501759ea61dc3e6808
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-10-05 16:03:38 -0300

    req 2

--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,18 @@
 const router = require('express').Router();
 
-const { readFileContent } = require('../middlewares/middlewares');
+const { readFileContent, getTalkerById } = require('../utils/utils');
 
 router.get('/', async (_req, res) => {
   const fileContent = await readFileContent('./talker.json') || [];
   res.status(200).json(fileContent);
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkerById = await getTalkerById(id);
+  if (!talkerById) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+  res.status(200).json(talkerById);
 });
 
 module.exports = router;

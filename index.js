1dea4b6d6c77b09e5bbf19e182986ac7106841b6
KaioHPs <kaiohpsalternativo@gmail.com>
2021-10-05 17:13:27 -0300

    Requisitos 1 e 2

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+const rescue = require('express-rescue');
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,9 +9,33 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const validateFile = rescue((req, _res, next) => {
+  const result = fs.readFileSync('./talker.json');
+  req.fileContent = JSON.parse(result);
+  next();
+});
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', validateFile, (req, res) => {
+  const { fileContent } = req;
+  if (!fileContent.length) return res.status(HTTP_OK_STATUS).json([]);
+  res.status(HTTP_OK_STATUS).json(fileContent);
+});
+
+app.get('/talker/:id', validateFile, (req, res) => {
+  const { id } = req.params;
+  const { fileContent } = req;
+  const foundTalker = fileContent.find((t) => t.id === parseInt(id, 10));
+  if (!foundTalker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  res.status(HTTP_OK_STATUS).json(foundTalker);
+});
+
+app.use((err, _req, res, _next) => { 
+  res.status(500).json({ error: `Erro: ${err.message}` });
 });
 
 app.listen(PORT, () => {

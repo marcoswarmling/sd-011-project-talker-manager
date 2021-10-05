ac40609cb0457414819921e44608739c51dc8d41
Thyago Pessoa <flash_thyago@hotmail.com>
2021-10-05 16:58:05 -0300

    Desafio 7

--- index.js
+++ index.js
@@ -19,6 +19,13 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+app.get('/talker/search', validToken, async (req, res) => {
+  const response = JSON.parse(await fs.readFile(talker, 'utf8'));
+  const { q } = req.query;
+  const filteredTalkers = response.filter((r) => r.name.includes(q));
+  res.status(200).json(filteredTalkers); 
+});
+
 app.get('/talker', async (_req, res) => {
   const response = JSON.parse(await fs.readFile(talker, 'utf8'));
   if (response.length < 1) return res.status(200).json([]);

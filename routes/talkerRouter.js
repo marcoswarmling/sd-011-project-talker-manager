c791539365e77dd348bf183ebd128f86fea69053
Daniel <danielfdoficial@gmail.com>
2021-10-05 15:45:49 -0300

    add desafio 2

--- routes/talkerRouter.js
+++ routes/talkerRouter.js
@@ -4,7 +4,20 @@ const talkersJson = require('../helper/fs');
 
 router.get('/', async (req, res) => {
   const data = await talkersJson();
+
   res.status(200).json(data);
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const data = await talkersJson();
+
+  const findTalker = data.find((talker) => Number(talker.id) === Number(id));
+
+  if (!findTalker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+  res.status(200).json(findTalker);
 });
 
 module.exports = router;

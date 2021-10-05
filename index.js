adcb03fee40f3d758b3a5e1d66084c483027b77b
Arthur Hermann <arthurhermannc@gmail.com>
2021-10-05 09:37:23 -0300

    create talkerRouter

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const talkerRouter = require('./router/talkerRouter');
+
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
@@ -11,6 +13,8 @@ const PORT = '3000';
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+app.use('/talker', talkerRouter);
 
 app.listen(PORT, () => {
   console.log('Online');

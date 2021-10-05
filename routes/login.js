b24a99d143a801268ca2e1cc217dc5792ebb3773
marlon307 <marlon-ramosb@hotmail.com>
2021-10-05 19:28:49 -0300

    Rq 3 update 4

--- routes/login.js
+++ routes/login.js
@@ -3,21 +3,26 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-module.exports = app.post('/login', (req, res) => {
+function validCredencies(req, res, next) {
   const { email, password } = req.body;
   const validEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/igm;
 
-  if (email === '') {
+  if (email === undefined) {
     return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
   if (!validEmail.test(email)) {
     return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
-  if (password === '') {
+  if (password === undefined) {
     return res.status(400).json({ message: 'O campo "password" é obrigatório' });
   }
   if (password.length < 6) {
     return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
-  res.status(200).json({ message: '7mqaVRXJSp886CGr' });
-});
+  next();
+}
+
+module.exports = app.post('/login', validCredencies, (_req, res) =>
+  res.status(200).json({
+    message: '7mqaVRXJSp886CGr',
+  }));

4a87606d069b1ea2b0d7c21a02274c3e8696ae23
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-10-05 16:31:02 -0300

    cria a rota /talker/:id

--- helper/readWriteHelper.js
+++ helper/readWriteHelper.js
@@ -3,7 +3,6 @@ const fs = require('fs').promises;
 const readFile = async (path) => {
   try {
     const content = await fs.readFile(path, 'utf8');
-    console.log(content);
     return JSON.parse(content);
   } catch (error) {
     return null;
--- router/talkerRoutes.js
+++ router/talkerRoutes.js
@@ -9,4 +9,14 @@ router.get('/', async (_req, res) => {
   res.status(200).json(result);
 });
 
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const result = await readFile(PATHNAME);
+  const talker = result.find((talker) => talker.id === parseInt(id));
+
+  if(!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+  res.status(200).json(talker);
+});
+
 module.exports = router;

fbe6e69d685488cd9f5556a828a3554b48cb6bf1
Marco Galindo <marco1408@gmail.com>
2021-10-05 15:09:23 -0300

    corrigindo req 3

--- validations.js
+++ validations.js
@@ -1,15 +1,21 @@
 function validateEmail(req, res, next) {
   const { email } = req.body;
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!email || !regex.test(email)) {
+  if (!email) {
     return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!regex.test(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
   next();
 }
 
 function validatePassword(req, res, next) {
   const { password } = req.body;
-  if (!password || password.length < 6) {
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
     return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
   next();

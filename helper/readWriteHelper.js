b46df70f4a665152832ec4700067cbc495fc52ad
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-10-05 16:05:17 -0300

    requisito 1

---
+++ helper/readWriteHelper.js
@@ -0,0 +1,27 @@
+const fs = require('fs').promises;
+
+const readFile = async (path) => {
+  try {
+    const content = await fs.readFile(path, 'utf8');
+    console.log(content);
+    return JSON.parse(content);
+  } catch (error) {
+    return null;
+  }
+};
+
+const writeFile = async (path, content) => {
+  try {
+    const file = await readFile(path);
+    file.push(content);
+    await fs.writeFile(path, JSON.stringify(file));
+    return file;
+  } catch (error) {
+    return null;
+  }
+};
+
+module.exports = {
+  readFile,
+  writeFile,
+};
--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const talkerRouter = require('./router/talkerRoutes');
+
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+app.use('/talker', talkerRouter);
 
 app.listen(PORT, () => {
   console.log('Online');

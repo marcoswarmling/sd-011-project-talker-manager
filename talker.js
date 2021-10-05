626cccc77b9c23fefd1a7e5e5b582931905ecd72
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-10-05 19:14:04 -0300

    feat: add req-2

--- talker.js
+++ talker.js
@@ -5,12 +5,33 @@ const router = express.Router();
 
 const PATH = 'talker.json';
 
+const talkerData = async (file) => {
+  const data = await fs.readFile(file, 'utf-8');
+  return JSON.parse(data);
+};
+
 router.get('/', async (_request, response) => {
   try {
     const data = await fs.readFile(PATH, 'utf-8');
     return response.status(200).json(JSON.parse(data));
   } catch (error) {
     return response.status(200).json([]);
+  }
+});
+
+router.get('/:id', async (request, response) => {
+  const { id } = request.params;
+  try {
+    const data = await talkerData(PATH);
+    const idTalker = data.find((talk) => talk.id === Number(id));
+
+    if (!idTalker) {
+      return response.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+
+    return response.status(200).json(idTalker);
+  } catch (error) {
+    return response.status(500).end();
   }
 });
 

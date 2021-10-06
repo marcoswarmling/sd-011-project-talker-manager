42052d9fbbc4cd6499c2a97abe1352ed6dd63eb1
Marcelo <marsleite@gmail.com>
2021-10-06 09:01:50 -0300

    refactoring req4 to evaluator

--- middlewares/createdTalker.js
+++ middlewares/createdTalker.js
@@ -1,5 +1,3 @@
-const moment = require('moment');
-
 function withOutName(req, res, next) {
   const { name } = req.body;
 
@@ -49,7 +47,8 @@ function rateInterval(rate, res) {
 }
 
 function validWatchedAt(watchedAt, res) {
-  const result = moment(watchedAt, 'DD/MM/YYYY', true).isValid();
+  const dateRgx = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+  const result = dateRgx.test(watchedAt);
   if (!result) {
     return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
   }

bdff4bb3cfda4d11192b576b75ea31b23c14c65f
Vinicius <viniciusfod@gmail.com>
2021-10-06 16:22:06 -0300

    req-3

---
+++ routes/loginRouter.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const crypto = require('crypto');
+
+const validateEmail = require('../middlewares/validateEmail');
+const validatePassword = require('../middlewares/validatePassword');
+
+const router = express.Router();
+
+router.post('/login', validateEmail, validatePassword, (req, res) => {
+  const token = crypto.randomBytes(16).toString('hex');
+  res.status(200).json({ token });
+});
+
+module.exports = router;

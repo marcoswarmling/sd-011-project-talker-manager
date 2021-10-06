dfcd46e6e6cc3d47e2686763c07b124a01c6f275
Marconi <marconieng@gmail.com>
2021-10-06 18:52:49 -0300

    Adiciona requisito 1

--- index.js
+++ index.js
@@ -6,10 +6,18 @@ app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const { getTalkers } = require('./helpers');
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res) => {
+  const talkers = await getTalkers();
+  console.log(talkers);
+
+  res.status(200).json(talkers);
 });
 
 app.listen(PORT, () => {

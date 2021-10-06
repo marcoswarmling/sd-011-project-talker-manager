bed065083e57d159d0096a10eb1e98f60c7ec2c3
menezesrenan7 <renan_menezes07@hotmail.com>
2021-10-06 13:30:25 -0300

    Acrescenta return na funçao  validateName dentro do arquivo validations.js.

--- validations.js
+++ validations.js
@@ -40,7 +40,7 @@ function validateName(req, res, next) {
     return res.status(400).json({ message: 'O campo "name" é obrigatório' });
   }
   if (name.length < 3) {
-    res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
   }
   next();
 }

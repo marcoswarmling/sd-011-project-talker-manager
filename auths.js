be174a77bc784b6d0242cd09e30ea52d0bb02d2a
gabiazevedo <gabiazevedoms@gmail.com>
2021-10-06 13:48:35 -0300

    Passando req 4

--- auths.js
+++ auths.js
@@ -42,5 +42,95 @@ function validatePassword(req, res, next) {
 
   next();
 }
+
+function validateName(req, res, next) {
+  const { name } = req.body;
+
+  if (!name || name === '') {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
+
+  if (name.length < 3) {
+    console.log('entrou');
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  next();
+}
+
+function validateAge(req, res, next) {
+  const { age } = req.body;
+
+  if (!age || age === '') {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
   
-module.exports = { validateToken, validateEmail, validatePassword, generateToken };
+  if (age < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+
+  next();
+}
+
+function validateRate(req, res, next) {
+  const { talk } = req.body;
+
+  if (!talk.rate || talk.rate === '') {
+    return res.status(400)
+    .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
+  }
+
+  if (Number.isInteger(talk.rate) !== true) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  next();
+}
+
+function validateTalk(req, res, next) {
+  const { talk } = req.body;
+
+  if (!talk || talk === '') {
+    return res.status(400)
+    .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
+  }
+
+  next();
+}
+
+function validateWatchedAt(req, res, next) {
+  const { talk } = req.body;
+
+  if (!talk.watchedAt || talk.watchedAt === '') {
+    return res.status(400)
+    .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
+  }
+
+  next();
+}
+
+function validateObjTalk(req, res, next) {
+  const { talk } = req.body;
+  const formatDate = /^[0-9]{2}\/{1}[0-9]{2}\/{1}[0-9]{4}$/g;
+
+  if (!formatDate.test(talk.watchedAt)) {
+    return res.status(400)
+    .json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+
+  if (talk.rate < 1 || talk.rate > 5) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  next();
+}
+  
+module.exports = {
+  validateToken,
+  validateEmail,
+  validatePassword,
+  generateToken,
+  validateName,
+  validateAge,
+  validateRate,
+  validateTalk,
+  validateObjTalk,
+  validateWatchedAt,
+};
--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const rescue = require('express-rescue');
 const talkers = require('./functionsAsync');
-const { generateToken, validateEmail, validatePassword } = require('./auths');
+const { generateToken, validateEmail,
+  validatePassword, validateToken,
+  validateName, validateAge, validateRate,
+  validateTalk, validateObjTalk, validateWatchedAt } = require('./auths');
 
 const app = express();
 app.use(bodyParser.json());
@@ -38,12 +41,28 @@ app.get('/talker/:id', rescue(async (req, res) => {
   res.status(200).json(talkerID);
 }));
 
-// Requisito 3 -  Crie o endpoint POST /login
+// Requisito 3 - Crie o endpoint POST /login
 
 app.post('/login', validateEmail, validatePassword, (_req, res) => {
   const token = generateToken();
   res.status(200).json({ token });  
 });
+
+// Requisito 4 - Crie o endpoint POST /talker
+
+app.post('/talker', validateToken, validateName,
+validateAge, validateTalk, validateWatchedAt,
+validateObjTalk, validateRate,
+ rescue(async (req, res) => {
+  const talkersAsync = await talkers.getTalkers();
+  const { name, age, talk } = req.body;
+  const newTalkerId = talkersAsync.length + 1;
+  const newTalker = { id: newTalkerId, name, age, talk };
+  talkersAsync.push(newTalker);
+ 
+  await talkers.setTalkers(talkersAsync);
+  res.status(201).json(newTalker);
+}));
 
 app.listen(PORT, () => {
   console.log('Online');

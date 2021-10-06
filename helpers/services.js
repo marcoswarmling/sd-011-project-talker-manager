6201d7ce5af696dce25ca74cfb89cf785fa9aaf7
anaclarabck <ackyotoku@gmail.com>
2021-10-06 11:52:39 -0300

    [UPDATE] improved function to check date

--- helpers/services.js
+++ helpers/services.js
@@ -1,4 +1,5 @@
 // https://www.codegrepper.com/code-examples/javascript/random+letter+in+javascript+generator
+// https://stackoverflow.com/questions/8855687/secure-random-token-in-node-js (another way: import crypto) const token = crypto.randomBytes(8).toString('hex');
 const generateToken = (size) => {
   const randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let token = '';
@@ -8,4 +9,29 @@ const generateToken = (size) => {
   return token;
 };
 
-module.exports = { generateToken };
+// https://stackoverflow.com/questions/6177975/how-to-validate-date-with-format-mm-dd-yyyy-in-javascript/49178339
+// regex is enough
+const checkMonthYear = (year, month) => {
+  const isValidYearAndMonth = year >= 1000 || month <= 12 || month > 0;
+  return !!isValidYearAndMonth;
+};
+
+const checkDay = (day, month, year) => {
+  const monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  if (year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0)) {
+    monthLength[1] = 29;
+  }
+  return day > 0 && day <= monthLength[month - 1];
+};
+
+const isValidDate = (dateString) => {
+  if (!/^\d{2}\/\d{2}\/\d{4}$/.test(dateString)) return false;
+  const splitedDate = dateString.split('/');
+  const day = Number(splitedDate[0]);
+  const month = Number(splitedDate[1]);
+  const year = Number(splitedDate[2]);
+  const checkDayMonthYear = checkMonthYear(year, month) && checkDay(day, month, year);
+  return checkDayMonthYear;
+};
+
+module.exports = { generateToken, isValidDate };
--- middleware/validations.js
+++ middleware/validations.js
@@ -1,26 +1,4 @@
-// https://stackoverflow.com/questions/6177975/how-to-validate-date-with-format-mm-dd-yyyy-in-javascript/49178339
-const checkDate = (date) => {
-  if (!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(date)) return false;
-  // const splitedDate = date.split('/');
-  // const day = Number(splitedDate[0]);
-  // const month = Number(splitedDate[1]);
-  // const year = Number(splitedDate[2]);
-  // const checkYearAndMonth = year < 1000 || month <= 0 || month > 12
-  // if (checkYearAndMonth) return false;
-  // const monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-  // if (year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0)) {
-  //   monthLength[1] = 29;
-  // }
-  // return day > 0 && day <= monthLength[month - 1];
-  return true;
-};
-
-const validateToken = (req, res, next) => {
-  const token = req.headers.authorization;
-  if (!token) return res.status(401).json({ message: 'Token não encontrado' });
-  if (token.length !== 16) return res.status(401).json({ message: 'Token inválido' });
-  next();
-};
+const { isValidDate } = require('../helpers/services');
 
 const validateEmail = (req, res, next) => {
   const { email } = req.body;
@@ -38,6 +16,13 @@ const validatePassword = (req, res, next) => {
   if (password.length < 6) {
     return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
+  next();
+};
+
+const validateToken = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) return res.status(401).json({ message: 'Token não encontrado' });
+  if (token.length !== 16) return res.status(401).json({ message: 'Token inválido' });
   next();
 };
 
@@ -61,7 +46,7 @@ const validateAge = (req, res, next) => {
 
 const validateWatchedAt = (req, res, next) => {
   const { talk: { watchedAt } } = req.body;
-  if (!checkDate(watchedAt)) {
+  if (!isValidDate(watchedAt)) {
     return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
   }
   next();
@@ -77,21 +62,19 @@ const validateRate = (req, res, next) => {
   next();
 };
 
-// eslint-disable-next-line complexity
 const validateTalk = (req, res, next) => {
   const { talk } = req.body;
   const message = 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios';
-  if (!talk) return res.status(400).json({ message });
-  const { rate, watchedAt } = talk;
-  const validRateAndWatchedAt = (rate || rate === 0) && watchedAt;
-  if (!validRateAndWatchedAt) return res.status(400).json({ message });
+  if (!talk || !((talk.rate || talk.rate === 0) && talk.watchedAt)) {
+    return res.status(400).json({ message });
+  }
   next();
 };
 
 module.exports = { 
-  validateToken,
   validateEmail,
   validatePassword,
+  validateToken,
   validateName,
   validateAge,
   validateWatchedAt,

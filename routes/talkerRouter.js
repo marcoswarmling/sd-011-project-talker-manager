b1c44ad4f812004e241a987f25a3a7ffd7ed5e09
Bernardo Prado <bernardoprado22@yahoo.com.br>
2021-10-06 18:41:46 -0300

    desafio 1

---
+++ routes/talkerRouter.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const fs = require('fs');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  try {
+    const talkers = fs.readFileSync('./talker.json', 'utf8');
+    res.status(200).json(JSON.parse(talkers));
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+module.exports = router;

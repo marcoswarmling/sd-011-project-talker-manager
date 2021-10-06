78ca6501d2892304ad04938f5635a5eaa7bf1575
fernando lasmar <f3rnandolasmar@gmail.com>
2021-10-06 16:29:34 -0300

    Adicionando endpoint talker/:id

--- routes/talker.js
+++ routes/talker.js
@@ -2,10 +2,19 @@ const router = require('express').Router();
 
 const fs = require('fs');
 
-router.get('/', (req, res) => {
-  const talker = fs.readFileSync('./talker.json', 'utf-8');
+router.get('/talker', (req, res) => {
+  const talkers = fs.readFileSync('./talker.json', 'utf-8');
+  res.status(200).json(JSON.parse(talkers));
+});
 
-  res.status(200).json(JSON.parse(talker));
+router.get('/talker/:id', (req, res) => {
+  const { id } = req.params;
+  const talkers = fs.readFileSync('./talker.json', 'utf-8');
+  const speakerFiltered = JSON.parse(talkers).find((speaker) => speaker.id === Number(id));
+  if (!speakerFiltered) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return res.status(200).json(speakerFiltered);
 });
 
 module.exports = router;

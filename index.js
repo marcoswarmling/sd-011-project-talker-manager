c167257e168f12e88180e3e1c5c4bc2a12c5c493
jhenriquemargrafm <jh.margraf@gmail.com>
2021-10-05 23:32:03 -0300

    Uso do promisify e readFile

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const util = require('util');
+const fs = require('fs');
+
+const readFile = util.promisify(fs.readFile);
+
 const app = express();
 app.use(bodyParser.json());
 
@@ -10,6 +15,13 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+// Requisito 1 - GET com todos os talkers
+app.get('/talkers', async (_req, res) => {
+  readFile('./talker.json', 'utf8').then((data) => {
+      res.status('200').json(JSON.parse(data));
+  });
 });
 
 app.listen(PORT, () => {

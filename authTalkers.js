2b1449e8e9881597eef9206af1c86f5020a50655
Hamander <1hamander@gmail.com>
2021-10-06 00:29:33 -0300

    Requisito 4 Removido

--- authTalkers.js
+++ authTalkers.js
@@ -1,15 +1,48 @@
 const crypto = require('crypto');
-const { getTalkers } = require('./readTalker');
+const fs = require('fs').promises;
 
 const userToken = crypto.randomBytes(8).toString('hex');
 
-const IDVerification = (req, res, next) => {
-  const { id } = req.params;
-  const talkers = getTalkers();
-  const talkersIds = talkers.map((talker) => Number(talker.id));
-  const talkerIncluded = talkersIds.includes(Number(id));
-  if (!talkerIncluded) { 
-    res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+const TokenCreation = async (_req, res) => {
+  try {
+    await res.status(200).send({ token: userToken });
+  } catch (err) {
+    res.status(400).send({ message: err });
+  }
+};
+
+const getAllTalkers = async (_req, res) => {
+  try {
+    const talkers = await fs.readFile('./talker.json', 'utf-8');
+    if (!talkers) res.status(200).json([]);
+    res.status(200).json(JSON.parse(talkers));
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+const IDVerification = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const talkers = await fs.readFile('./talker.json', 'utf-8');
+    const result = JSON.parse(talkers);
+    const talkerIncluded = result.find((talker) => talker.id === Number(id));
+    if (!talkerIncluded) { 
+      res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+    res.status(200).json(talkerIncluded);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+const PasswordVerification = (req, res, next) => {
+  const { password } = req.body;
+  if (!password) {
+    return res.status(400).send({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res.status(400).send({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
   next();
 };
@@ -17,97 +50,124 @@ const IDVerification = (req, res, next) => {
 const EmailVerification = (req, res, next) => {
   const { email } = req.body;
   if (!email) {
-    res.status(400).json({ message: 'O campo "email" é obrigatório' });
+    return res.status(400).send({ message: 'O campo "email" é obrigatório' });
   }
   if (!(email.includes('.com') && email.includes('@'))) {
-    res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+    return res.status(400).send({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
   next();
 };
 
-const PasswordVerification = (req, res, next) => {
-  const { password } = req.body;
-  if (!password || password === '') {
-    res.status(400).json({ message: 'O campo "password" é obrigatório' });
-  }
-  if (password.length < 6) {
-    res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
-  }
-  next();
-};
+// const TokenVerification = (req, res, next) => {
+//   const { authorization } = req.headers;
 
-const TokenCreation = (_req, res) => {
-  res.status(200).json({ token: userToken });
-};
+//   if (!authorization) {
+//     return res.status(401).json({ 
+//       message: 'Token não encontrado',
+//     });
+//   }
 
-const TokenVerification = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization) return res.status(401).json({ message: 'Token não encontrado' });
-  if (authorization && authorization.length !== 16) {
-    return res.status(401).json({ message: 'Token inválido' });
-  }
-  next();
-};
+//   if (authorization.length !== 16) {
+//     return res.status(401).json({
+//       message: 'Token inválido',
+//     }); 
+//   }
+//   next();
+// };
 
-const nameVerify = (req, res, next) => {
-  const { name } = req.body;
-  if (!name) {
-    res.status(400).json({ message: 'O campo "name" é obrigatório' });
-  }
-  if (name && name.length < 3) {
-    res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
-  }
-  next();
-};
+// const nameVerify = (req, res, next) => {
+//   const { name } = req.body;
+//   if (!name) {
+//     return res.status(400).send({ message: 'O campo "name" é obrigatório' });
+//   }
+//   if (name && name.length < 3) {
+//     res.status(400).send({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+//   }
+//   next();
+// };
 
-const AgeVerify = (req, res, next) => {
-  const { age } = req.body;
-  if (!age) {
-    res.status(400).json({ message: 'O campo "age" é obrigatório' });
-  }
-  if (age < 18) {
-    res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
-  }
-  next();
-};
+// const AgeVerify = (req, res, next) => {
+//   const { age } = req.body;
+//   if (!age) {
+//     res.status(400).send({ message: 'O campo "age" é obrigatório' });
+//   }
+//   if (age < 18) {
+//     res.status(400).send({ message: 'A pessoa palestrante deve ser maior de idade' });
+//   }
+//   next();
+// };
 
-const watchedAtVerification = (req, res, next) => {
-  const { talk } = req.body;
-  const dateRegex = /^\d{2}\/\d{2}\/\d{4}$/;
-  if (!talk || !talk.watchedAt) {
-    res.status(400).json({
-       message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
-    });
-  }
-  if (!dateRegex.test(talk.watchedAt)) {
-    res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
-  }
-  next();
-};
+// const validTalk = (req, res, next) => {
+//   const { talk } = req.body;
 
-const ratedVerification = (req, res, next) => {
-  const { talk } = req.body;
-  if (Number(talk.rate) < 1 || Number(talk.rate) > 5) {
-    res.status(400).json({ 
-      message: 'O campo "rate" deve ser um inteiro de 1 à 5',
-    });
-  }
-  if (!talk.rate) {
-    return res.status(400).send({
-      message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
-    });
-  }
-  next();
-};
+//   if (!talk || !talk.watchedAt) {
+//     return res.status(400).send({
+//       message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+//     });
+//   }
+
+//   next();
+// };
+
+// const watchedAtVerification = (req, res, next) => {
+//   const { talk } = req.body;
+//   const dateRegex = /^\d{2}\/\d{2}\/\d{4}$/;
+//   if (!talk || !talk.watchedAt) {
+//     res.status(400).json({
+//        message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+//     });
+//   }
+//   if (!dateRegex.test(talk.watchedAt)) {
+//     res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+//   }
+//   next();
+// };
+
+// const ratedVerification = (req, res, next) => {
+//   const { talk } = req.body;
+//   if (Number(talk.rate) < 1 || Number(talk.rate) > 5) {
+//     res.status(400).json({ 
+//       message: 'O campo "rate" deve ser um inteiro de 1 à 5',
+//     });
+//   }
+//   if (!talk.rate) {
+//     return res.status(400).send({
+//       message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+//     });
+//   }
+//   next();
+// };
+
+// const talkerSetter = async (req, res) => {
+//   const { name, age, talk: { rate, watchedAt } } = req.body;
+
+//   const talker = await fs.readFile('./talker.json', 'utf-8');
+//   const result = JSON.parse(talker);
+//   const newTalker = {
+//     name,
+//     age,
+//     id: result.length + 1,
+//     talk: {
+//       rate,
+//       watchedAt,
+//     },
+//   };
+//   result.push(newTalker);
+//   await fs.writeFile('./talker.json', JSON.stringify(result));
+//   res.status(201).send(newTalker);
+// };
 
 module.exports = {
-  AgeVerify,
-  nameVerify,
+  // AgeVerify,
+  // nameVerify,
+  // validTalk,
+  // talkerSetter,
+  getAllTalkers,
   TokenCreation,
   IDVerification,
   EmailVerification,
-  TokenVerification,
-  ratedVerification,
+  // TokenVerification,
+  // ratedVerification,
   PasswordVerification,
-  watchedAtVerification,
+  // watchedAtVerification,
 };
--- routers/index.js
+++ routers/index.js
@@ -1,36 +1,27 @@
 const router = require('express').Router();
-const fs = require('fs').promises;
 
-const { getTalkers } = require('../readTalker');
 const {
-  AgeVerify,
-  nameVerify,
+  // AgeVerify,
+  // nameVerify,
+  // validTalk,
+  // talkerSetter,
   TokenCreation,
+  getAllTalkers,
   IDVerification,
-  TokenVerification,
-  ratedVerification,
+  // TokenVerification,
+  // ratedVerification,
   EmailVerification,
   PasswordVerification,
-  watchedAtVerification,
+  // watchedAtVerification,
 } = require('../authTalkers');
 
 // Requisito 1
 
-router.get('/talker', (_req, res) => {
-  const talkers = getTalkers();
-  res.status(200).send(talkers);
-});
+router.get('/talker', getAllTalkers);
 
 // Requisito 2
 
-router.get('/talker/:id', IDVerification, (req, res) => {
-  const { id } = req.params;
-  const talkers = getTalkers();
-  const filteredTalker = talkers.find(
-    (talker) => Number(talker.id) === Number(id),
-  );
-  return res.status(200).json(filteredTalker);
-});
+router.get('/talker/:id', IDVerification);
 
 // Requisito 3
 
@@ -38,20 +29,15 @@ router.post('/login', PasswordVerification, EmailVerification, TokenCreation);
 
 // Requisito 4
 
-router.post(
-  '/talker',
-  TokenVerification,
-  watchedAtVerification,
-  ratedVerification,
-  nameVerify,
-  AgeVerify,
-  async (req, res) => {
-    const { name, age, talk } = req.body;
-    const talkers = getTalkers();
-    talkers.push({ id: talkers.length + 1, name, age, talk });
-    await fs.writeFile('./talker.json', JSON.stringify(talkers));
-    return res.status(201).json({ id: talkers.length, name, age, talk });
-  },
-);
+// router.post(
+//   '/talker',
+//   TokenVerification,
+//   validTalk,
+//   ratedVerification,
+//   watchedAtVerification,
+//   AgeVerify,
+//   nameVerify,
+//   talkerSetter,
+// );
 
 module.exports = router;

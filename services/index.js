4653101e2b640b16c981a06dda85cdc534092fa5
Gui Pereira <guilhermepdo1508@gmail.com>
2021-10-05 23:34:43 -0300

    Consertar bugs

---
+++ services/index.js
@@ -0,0 +1,17 @@
+// const fs = require('fs').promises;
+
+// const talker = '../talker.json';
+
+// function readTalker() {
+//   return fs.readFile(talker, 'utf8').then(
+//     (data) => JSON.parse(data),
+//   );
+// }
+
+// function writeTalker(data) {
+//   fs.writeFile(talker, data, 'utf-8', (error) => {
+//     if (error) throw error;
+//   });
+// }
+
+// module.exports = { readTalker, writeTalker };

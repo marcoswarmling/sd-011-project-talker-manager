279c316287a6be4bde8120a6101f3a810e483172
Bruno Duarte <brunoarduarte@gmail.com>
2021-10-06 12:29:49 -0300

    Desafio 2

--- routes/talker.js
+++ routes/talker.js
@@ -7,4 +7,15 @@ router.get('/', async (_req, res) => {
   res.status(200).json(JSON.parse(talkers));
 });
 
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await fs.readFile('./talker.json', 'utf-8');
+  const search = JSON.parse(talkers).find((item) => item.id === Number(id));
+  if (!search) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  return res.status(200).json(search);
+});
+
 module.exports = router;

a57ac31b76d3d3bb90e3502a7597a399848a8f38
Henrique Pozzolini <hpmoreira05@gmail.com>
2021-10-06 11:49:26 -0300

    requisito 3

--- index.js
+++ index.js
@@ -8,6 +8,30 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+function generateToken(length) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i += 1) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+ }
+ return result;
+}
+
+const authMiddleware = (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  const regex = (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/).test(email);
+  if (!regex) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  next();
+};
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -30,8 +54,13 @@ app.get('/talker/:id', async (req, res) => {
     if (!teste) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
     res.status(200).json(teste);
   } catch (error) {
-    res.status(404).json({ message: 'Algo deu errado, tente novamente.' });
+    res.status(400).json({ message: 'Algo deu errado, tente novamente.' });
   }
+});
+
+app.post('/login', authMiddleware, (req, res) => {
+  const token = generateToken(16);
+  res.status(200).json({ token });
 });
 
 app.listen(PORT, () => {

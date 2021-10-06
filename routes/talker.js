4084e12c01ef06230af4423135699db3dd845d9c
Bruno Duarte <brunoarduarte@gmail.com>
2021-10-06 11:27:53 -0300

    Desafio 1 concluido

--- routes/talker.js
+++ routes/talker.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const fs = require('fs');
+const fs = require('fs').promises;
 
-router.get('/', (_req, res) => {
-  const talkers = fs.readFileSync('./talker.json', 'utf-8');
+router.get('/', async (_req, res) => {
+  const talkers = await fs.readFile('./talker.json', 'utf-8');
   
   res.status(200).json(JSON.parse(talkers));
 });

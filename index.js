7c0819e28d4a5bd78846f56802002641c52966d0
theusseveen22 <mateus.krauss@gmail.com>
2021-10-05 21:02:42 -0300

    Adiciona requisito 1 e 2

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
@@ -10,6 +10,29 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (_req, res) => {
+  const dados = fs.readFileSync('talker.json', 'utf-8');
+
+  if (!dados) {
+   return res.status(200).json([]);
+    }
+
+   return res.status(200).send(JSON.parse(dados));
+});
+
+app.get('/talker/:id', (req, res) => {
+  const dados = fs.readFileSync('talker.json', 'utf-8');
+  const newdados = JSON.parse(dados);
+
+  const { id } = req.params;
+
+    const dadosId = newdados.find((i) => Number(i.id) === Number(id));
+
+    if (!dadosId) res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
+
+    return res.status(200).send(dadosId);
 });
 
 app.listen(PORT, () => {

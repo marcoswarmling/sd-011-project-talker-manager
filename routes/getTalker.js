744761c449ef65686b801782603b9e36ac0a5f3c
IgorMendez <igor.emp@hotmail.com>
2021-10-05 21:00:38 -0300

    Desafio 2

--- routes/getTalker.js
+++ routes/getTalker.js
@@ -9,4 +9,14 @@ router.get('/', (_req, res) => {
   res.status(200).json(parseTalk);
 });
 
+router.get('/:id', (_req, res) => {
+  const { id } = _req.params;
+  const talker = fs.readFileSync('./talker.json');
+  const parseTalk = JSON.parse(talker);
+  const person = parseTalk.find((el) => el.id === +id);
+  
+  if (!person) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  res.status(200).json(person);
+});
+
 module.exports = router;

eab8e6f0a82f5ef85cc2620c9c29cc04a34ce717
marcoswarmling <marcoswarmling15@gmail.com>
2021-10-06 02:11:41 -0300

    nos testes locais esta 100% agora falta o evaluator

--- routers/PostTalker.js
+++ routers/PostTalker.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const fs = require('fs');
 
+const talkerJSON = './talker.json';
+
 const router = express.Router();
 const { 
     validateToken,
@@ -18,11 +20,11 @@ router.post('/',
   validateTalkIncrements,
   async (req, res) => {
   const { name, age, talk } = req.body;
-  const talkerContent = fs.readFileSync('./talker.json');
+  const talkerContent = fs.readFileSync(talkerJSON);
   const talker = JSON.parse(talkerContent);
   const id = talker[talker.length - 1].id + 1;
   talker.push({ id, name, age, talk });
-  await fs.writeFileSync('./talker.json', JSON.stringify(talker, null, 2));
+  await fs.writeFileSync(talkerJSON, JSON.stringify(talker, null, 2));
   return res.status(201).json({ id, name, age, talk });
 });
 
@@ -35,29 +37,23 @@ validateTalkIncrements,
 async (req, res) => {
     const { id } = req.params;
     const { name, age, talk } = req.body;
-    const talkers = fs.readFileSync('./talker.json', 'utf8');
+    const talkers = fs.readFileSync(talkerJSON, 'utf8');
     const talkerParse = JSON.parse(talkers);
     const talkerIndex = talkerParse.findIndex((talker) => talker.id === +id);
     const editedTalker = { id: +id, name, age, talk };
     talkerParse[talkerIndex] = editedTalker;
-   fs.writeFileSync('./talker.json', JSON.stringify(talkerParse, null, 2));
+   fs.writeFileSync(talkerJSON, JSON.stringify(talkerParse, null, 2));
     return res.status(200).json(editedTalker);
 });
 
-/* router.delete('/:id',
-validateToken,
-validateAge,
-validateName,
-validateTalk,
-validateTalkIncrements,
-async (req, res) => {
+router.delete('/:id', validateToken, (req, res) => {
     const { id } = req.params;
-    const talkers = fs.readFileSync('./talker.json', 'utf8');
-    const talkerParse = JSON.parse(talkers);
-    const talkerIndex = talkerParse.findIndex((talker) => talker.id === +id);
-    talkerParse.splice(1, talkerIndex);
-   fs.writeFileSync('./talker.json', JSON.stringify(talkerParse, null, 2));
-    return res.status(200).json(talkerParse);
-}); */
+      const talkers = JSON.parse(fs.readFileSync(talkerJSON, 'utf8'));
+      const newList = talkers.filter((talker) => Number(talker.id) !== Number(id));
+      fs.writeFileSync(talkerJSON, JSON.stringify(newList));
+      res.status(200).json({
+        message: 'Pessoa palestrante deletada com sucesso',
+      });
+  });
 
 module.exports = router;
--- routers/getTalker.js
+++ routers/getTalker.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const fs = require('fs');
-
+const {validateToken} = require('../middlewares/ValidatePostTalker')
 const router = express.Router();
 
 router.get('/', (_req, res) => {
@@ -8,6 +8,21 @@ router.get('/', (_req, res) => {
   if (!file) return res.status(200).json([]);
   res.status(200).json(JSON.parse(file));
 });
+
+router.get(
+  '/search',
+  validateToken,
+  (req, res) => {
+    const { q } = req.query;
+    const talkersList = fs.readFileSync('./talker.json', 'utf8');
+    const talkerParse = JSON.parse(talkersList)
+    const talker = talkerParse.filter((item) => {return item.name.includes(q)});
+    if(!q){res.status(200).send(talkerParse)
+    }else{res.status(200).send(talker) }
+
+  }
+);
+
 
 router.get('/:id', (req, res) => {
   const talkers = fs.readFileSync('./talker.json', 'utf-8');
@@ -20,5 +35,8 @@ router.get('/:id', (req, res) => {
 }
   return res.status(200).json(filteredTalkers);
 });
+
+
+
 
 module.exports = router;

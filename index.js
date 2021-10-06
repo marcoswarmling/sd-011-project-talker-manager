109e04987917bfe7d3a8f539006dd9d1bd3c86ad
Ivaldo Souza <ivaldo.souza@datapar.com.br>
2021-10-05 22:04:03 -0300

    Desafio 2

--- index.js
+++ index.js
@@ -13,16 +13,32 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+const fechAPI = './talker.json';
+const msg = 'Pessoa palestrante não encontrada';
+
 // Requisito 1
 
 app.get('/talker', (_req, res) => {
-  const data = JSON.parse(fs.readFileSync('./talker.json', 'utf-8'));
+  const data = JSON.parse(fs.readFileSync(fechAPI, 'utf-8'));
 
   if (!data) {
     return res.status(200).json([]);
   }
 
   res.status(200).json(data);
+});
+
+// Requisito 2
+
+app.get('/talker/:id', (req, res) => {
+  const data = JSON.parse(fs.readFileSync(fechAPI, 'utf-8'));
+
+  const { id } = req.params;
+
+  const dataFind = data.find((item) => item.id === Number(id));
+  
+  if (!dataFind) res.status(400).json({ message: 'Pessoa palestrante não encontrada' });
+  res.status(200).json(dataFind);
 });
 
 app.listen(PORT, () => {

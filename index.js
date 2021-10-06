7032145a636c719cfc225884422d007dae9232df
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-10-06 17:04:32 -0300

    adiciona requisito 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,4 +15,13 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+app.get('/talker', async (_req, res) => {
+  try {
+  const talker = await fs.readFile('./talker.json', 'utf-8');
+  res.status(HTTP_OK_STATUS).json(JSON.parse(talker));
+} catch (err) {
+  res.status(400).json({ message: err.message });
+}
 });

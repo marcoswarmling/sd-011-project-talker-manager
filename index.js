77d1f340d980c3d50dd6b91bf82331ec7ed295b2
guinhoanjos <iago.anjos@hotmail.com>
2021-10-06 19:44:48 -0300

    requisito 1 e 2

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+// const talker = require('./talker.json');
+
+async function readTalker() {
+  const talker = await fs.readFile('./talker.json');
+  return JSON.parse(talker);
+}
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,6 +14,23 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+app.get('/talker', async (_req, res) => {
+  const talker = await readTalker();
+  res.status(HTTP_OK_STATUS).json(talker);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const talker = await readTalker();
+  const { id } = req.params;
+  const resultado = await talker.find((obj) => obj.id === Number(id));
+  console.log(resultado);
+  if (resultado === undefined) {
+    return res.status(404).send({ 
+      message: 'Pessoa palestrante não encontrada', 
+    }); 
+  }
+  res.status(HTTP_OK_STATUS).send(resultado);
+});
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();

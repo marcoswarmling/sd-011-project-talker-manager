a448d0a73808626d5cc49ce2f56e4edeeed0aa43
marcoswarmling <marcoswarmling15@gmail.com>
2021-10-05 22:53:15 -0300

    requisito 4 quase terminado

--- routers/PostTalker.js
+++ routers/PostTalker.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const fs = require('fs');
+
 const router = express.Router();
 const { 
     validateToken,
     validateAge,
     validateName,
     validateTalk,
-} = require('../middlewares/ValidatePostTalker')
+    validateTalkIncrements,
+} = require('../middlewares/ValidatePostTalker');
+
+const talkerWrite = (req) => {
+    const talkerJSON = fs.readFileSync('./talker.json', 'utf8');
+    const talkerParse = JSON.parse(talkerJSON);
+    const { name, age, talk } = req;
+    const id = talkerParse.length + 1;
+    talkerParse.push({ name, age, id, talk });
+ fs.writeFileSync('./talker.json', JSON.stringify(talkerParse));
+ };
 
 router.post(
 '/',
@@ -14,20 +25,12 @@ router.post(
  validateAge, 
  validateName,
  validateTalk,
+ validateTalkIncrements,
 (req, res) => {
-const talkerRequest = req.body
-talkerWrite(talkerRequest)
-return res.status(201).json(talkerRequest)
-})
-
-const talkerWrite = (req) => {
-   const talkerJSON = fs.readFileSync('./talker.json', 'utf8')
-   const talkerParse = JSON.parse(talkerJSON)
-   const {name,age,talk} = req
-   const id = talkerParse.length + 1
-   talkerParse.push({name,age,id,talk})
-fs.writeFileSync('./talker.json', JSON.stringify(talkerParse))
-
-}
+const talkerRequest = req.body;
+talkerWrite(talkerRequest);
+return res.status(201).json(talkerRequest);
+},
+);
 
 module.exports = router;

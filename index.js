2a0a93d537f5c97ec70a64a85d234f455cbf5d7a
Josan Johnata <josanjohnata@gmail.com>
2021-10-06 18:36:13 -0300

    Requisito 2 corrigindo o lint mais uma vez

--- index.js
+++ index.js
@@ -1,38 +1,36 @@
-const express = require("express");
-const fs = require("fs").promises;
+const express = require('express');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
-const PORT = "3000";
+const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get("/", (_request, response) => {
+app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.get("/:id", async (req, res) => {
+app.get('/:id', async (req, res) => {
   const { id } = req.params;
-  const talker = await fs.readFile("./talker.json", "utf8");
-  const findId = JSON.parse(talker).find(
-    (element) => element.id === Number(id)
-  );
+  const talker = await fs.readFile('./talker.json', 'utf8');
+  const findId = JSON.parse(talker).find((element) => element.id === Number(id));
   if (!findId) {
     return res
       .status(404)
-      .json({ message: "Pessoa palestrante não encontrada" });
+      .json({ message: 'Pessoa palestrante não encontrada' });
   }
   return res.status(200).json(findId);
 });
 
-app.get("/talker", async (_req, res) => {
-  const data = await fs.readFile("./talker.json", "utf8");
+app.get('/talker', async (_req, res) => {
+  const data = await fs.readFile('./talker.json', 'utf8');
   const talker = JSON.parse(data);
 
   return res.status(200).json(talker);
 });
 
 app.listen(PORT, () => {
-  console.log("Online");
+  console.log('Online');
 });

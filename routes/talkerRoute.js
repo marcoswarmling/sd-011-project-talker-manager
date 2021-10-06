086e94361283898da77c3b77f04f1849a1afc6a2
Lucas Maestrelli <lucasmaestrelli95@gmail.com>
2021-10-06 19:50:37 -0300

    git add.

---
+++ routes/talkerRoute.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+
+const fs = require('fs').promises;
+
+const router = Router();
+
+router.get('/talker', async (_req, res) => {
+    const dados = await fs.readFile('./talker.json');
+    res.status(200).json(JSON.parse(dados));
+  });
+module.exports = router;

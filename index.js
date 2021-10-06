d74554626f368b9971ac85647b0f3da01f947649
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-10-06 18:14:16 -0300

    task 1 part 1

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// References
+//
+// util:
+// https://nodejs.org/dist/latest-v8.x/docs/api/util.html#util_util
+// https://github.com/nodejs/node/blob/v16.9.0/lib/util.js
+//
+// fs:
+// https://nodejs.dev/learn/the-nodejs-fs-module
+
+const util = require('util');
+const fs = require('fs');
+
+const file = util.promisify(fs.file);
+
 const app = express();
 app.use(bodyParser.json());
 
@@ -10,6 +24,13 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+// task 1
+app.get('/talker', async (_request, response) => {
+  file('./talker.json', 'utf8').then((data) => {
+    response.status('200').json(JSON.parse(data));
+  });
 });
 
 app.listen(PORT, () => {

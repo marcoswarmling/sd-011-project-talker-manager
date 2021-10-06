e781e5dbf3433f023526abbe6d8054683fde72e4
Bruno Duarte <brunoarduarte@gmail.com>
2021-10-06 08:40:29 -0300

    Desafio 1

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
+const talkerRoutes = require('./routes/talker');
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
@@ -11,6 +11,8 @@ const PORT = '3000';
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+app.use(talkerRoutes);
 
 app.listen(PORT, () => {
   console.log('Online');
---
+++ routes/talker.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const fs = require('fs').promises;
+
+router.get('/talker', async (_req, res) => {
+  const talkers = await fs.readFile('./talker.json', 'utf-8');
+  
+  res.status(200).json(JSON.parse(talkers));
+});
+
+module.exports = router;

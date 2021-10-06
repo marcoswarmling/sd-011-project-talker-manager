dad6b367849021360a300fe0f792835b9a3d353c
Josan Johnata <josanjohnata@gmail.com>
2021-10-06 18:15:26 -0300

    Requisito 2

--- index.js
+++ index.js
@@ -12,6 +12,19 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+app.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talker = await fs.readFile('./talker.json', 'utf8');
+  console.log('esse é o talker', talker);
+  const findId = JSON.parse(talker).find((element) => element.id === Number(id));
+  console.log('esse é o findId', findId);
+  if(!findId) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada'
+  });
+}
+  return res.status(200).json(findId);
+})
+
 app.get('/talker', async (_req, res) => {
   const data = await fs.readFile('./talker.json', 'utf8');
   const talker = JSON.parse(data);

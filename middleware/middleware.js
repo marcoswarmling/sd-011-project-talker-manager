325365b5775753aaebb98cd893031490f8bda490
filizzola <juliofilizzola@hotmail.com>
2021-10-06 08:55:47 -0300

    Movendo DeleteTalker

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -8,4 +8,13 @@ const searchTalk = async (req, res) => {
   res.status(HTTP_OK_STATUS).json(response);
 };
 
-module.exports = { searchTalk };
+const deleteTalker = async (req, res) => {
+  const { id } = req.params;
+  const talker = await fs.readFile(path, 'utf8');
+  const result = JSON.parse(talker);
+  const response = result.find((value) => value.id !== Number(id));
+  await fs.writeFile(path, JSON.stringify(response));
+  res.status(HTTP_OK_STATUS).send({ message: deleteT });
+};
+
+module.exports = { searchTalk, deleteTalker };
--- middleware/validation.js
+++ middleware/validation.js
@@ -198,25 +198,7 @@ const setEditTalker = async (req, res) => {
   res.status(200).send(talk);
 };
 
-const deleteTalker = async (req, res) => {
-  const { id } = req.params;
 
-  const talker = await fs.readFile(path, 'utf8');
-  const result = JSON.parse(talker);
-  const response = result.find((value) => value.id !== Number(id));
-  await fs.writeFile(path, JSON.stringify(response));
-  res.status(HTTP_OK_STATUS).send({ message: deleteT });
-};
-
-const searchTalk = async (req, res) => {
-  const { q } = req.query;
-
-  const talker = await fs.readFile(path, 'utf8');
-  const result = JSON.parse(talker);
-  const response = result.filter((value) => value.name.includes(q));
-
-  res.status(HTTP_OK_STATUS).json(response);
-};
 
 module.exports = { 
   getAllTalkers,
@@ -233,5 +215,4 @@ module.exports = {
   setTalker,
   setEditTalker,
   deleteTalker,
-  searchTalk,
 };

3fec903ea96588c5b057e2f8a3bd1642b1e4cd9b
Lucas Maestrelli <lucasmaestrelli95@gmail.com>
2021-10-06 19:35:17 -0300

    começando

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+app.get('/talker', async (_req, res) => {
+  const dados = fs.readFile('./talker.json');
+  res.status(200).json(JSON.parse(dados));
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {

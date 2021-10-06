d9cad50ea07ca089e8043408cb31e5377bd43bb8
Maran <maranmorimoto@gmail.com>
2021-10-05 22:17:15 -0300

    requirements 01 and 02 done.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const express = require('express');
+const express = require('express'); // utilizo o require de express pq ele me ajuda a lidar com rotas
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
-const app = express();
+const app = express(); // dou o nome de app para o express(), para que fique mais facil de fazer o get, post, put e delete só chamando app Ex. app.get(), que é igual à express().get() 
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
@@ -14,4 +15,20 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+app.get('/talker', async (_req, res) => {
+  const getTalkers = await fs.readFile('./talker.json', 'utf-8');
+  const getTalkersTransf = JSON.parse(getTalkers);
+  return res.status(200).json(getTalkersTransf);
+});
+
+app.get('/talker/:id', async (req, res) => {
+    const getTalkers = await fs.readFile('./talker.json', 'utf-8');
+    const getTalkersTransf = JSON.parse(getTalkers);
+    const { id } = req.params;
+    const talkerId = getTalkersTransf.find((talker) => talker.id === Number(id));
+    console.log(talkerId);
+    if (!talkerId) return res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
+    return res.status(200).json(talkerId);
 });

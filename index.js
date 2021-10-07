541f42e28c5922c3403ffdec180832ef37a42469
DADA <davimarque2000@gmail.com>
2021-10-07 14:29:06 -0300

    desafio 1

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
+const talkers = JSON.parse(fs.readFileSync('./talker.json', 'utf-8'));
 const app = express();
 app.use(bodyParser.json());
 
@@ -10,6 +12,11 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (req, res) => {
+  if (!talkers) { res.status(HTTP_OK_STATUS).json([]); }
+  res.status(HTTP_OK_STATUS).json(talkers);
 });
 
 app.listen(PORT, () => {

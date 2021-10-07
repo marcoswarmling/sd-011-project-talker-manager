06dd03960dd7f0fe2a986519e009dcf87116c34e
guinhoanjos <iago.anjos@hotmail.com>
2021-10-07 18:49:48 -0300

    criado funcoes q verificam email/ senha/ e gera token

---
+++ checkInFunctions.js
@@ -0,0 +1,43 @@
+// Site explicando como exportar varias funcoes: https://qastack.com.br/programming/16631064/declare-multiple-module-exports-in-node-js
+
+function makeid() {
+  // funcao retirada e adaptada de: https://www.ti-enxame.com/pt/javascript/gere-stringcaracteres-aleatorios-em-javascript/967048592/
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let i = 0; i < 16; i += 1) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+
+  return text;
+}
+
+function validateEmail(req, res, next) {
+  // Funcao do Cesar Bhering
+  const { email } = req.body;
+  if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  if (!email.includes('@') || !email.includes('.com')) { // verifica se o email contem "@" ou ".com"
+ return res
+      .status(400)
+      .json({ message: 'O "email" deve ter o formato "email@email.com"' }); 
+}
+  next();
+}
+console.log(makeid());
+
+function validatePassword(req, res, next) {
+  const { password } = req.body;
+  console.log(password, typeof (password));
+  if (password === undefined || password === '') {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' }); 
+  }
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' }); 
+  }
+  next();
+}
+module.exports = { 
+  makeid,
+  validateEmail,
+  validatePassword,
+};

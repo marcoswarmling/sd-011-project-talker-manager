20252e69556bd4d044f5fe2c03780ebbc5b70587
Rubia <rubiaemilly29@gmail.com>
2021-10-07 01:48:53 -0300

    desafio 3

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const talkerRoutes = require('./routes/talkerRoutes');
 const { errorMiddlewares } = require('./middlewares/error');
+const loginRoutes = require('./routes/loginRoutes');
 
 const app = express();
 
-app.use(express.json());
+app.use(bodyParser.json());
+/* app.use(express.json()); */
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
 app.use('/talker', talkerRoutes);
+
+app.use('/login', loginRoutes.router);
 
 app.get('/', (_request, response) => {
 // não remova esse endpoint, e para o avaliador funcionar

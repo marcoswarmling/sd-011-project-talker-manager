c9817c6529d22d030b90d55f7f48a9363c7d9ea9
jhenriquemargrafm <jh.margraf@gmail.com>
2021-10-07 15:08:21 -0300

    Req 5 - logica do POST, substituindo valores no JSON

--- middlewares/talker.js
+++ middlewares/talker.js
@@ -5,18 +5,20 @@ const crypto = require('crypto');
 const router = express.Router();
 
 const validateLogin = require('./login');
-const validateNewTalker = require('./newTalker');
+const validateTalker = require('./validateTalker');
+
+const list = './talker.json';
 
 // Requisito 1 - GET com todos os talkers
 router.get('/talker', (_req, res) => {
-  const talkers = JSON.parse(fs.readFileSync('./talker.json', 'utf-8'));
+  const talkers = JSON.parse(fs.readFileSync(list, 'utf-8'));
   res.status(200).json(talkers);
 });
 
 // Requisito 2 - GET com endpoint /talker/:id
 router.get('/talker/:id', (req, res) => {
   const { id } = req.params;
-  const talkers = JSON.parse(fs.readFileSync('./talker.json', 'utf-8'));
+  const talkers = JSON.parse(fs.readFileSync(list, 'utf-8'));
   const chosenTalker = talkers.find((talker) => talker.id === parseInt(id, 10));
 
   if (!chosenTalker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
@@ -32,19 +34,41 @@ router.post('/login', validateLogin.checkEmail, validateLogin.checkPassword, (_r
 // Requisito 4 - Endpoint POST /talker
 router.post(
   '/talker',
-  validateNewTalker.checkToken,
-  validateNewTalker.checkAge,
-  validateNewTalker.checkName,
-  validateNewTalker.checkTalk,
+  validateTalker.checkToken,
+  validateTalker.checkAge,
+  validateTalker.checkName,
+  validateTalker.checkTalk,
   (req, res) => {
-    const talkers = JSON.parse(fs.readFileSync('./talker.json', 'utf-8'));
+    const talkers = JSON.parse(fs.readFileSync(list, 'utf-8'));
     const { name, age, talk } = req.body;
     const newId = talkers.length + 1;
     talkers.push({ id: newId, name, age, talk });
 
-    fs.writeFileSync('./talker.json', JSON.stringify(talkers));
+    fs.writeFileSync(list, JSON.stringify(talkers));
 
     res.status(201).json({ id: newId, name, age, talk });
+  },
+);
+
+// Requisito 5 - Endpoint PUT /talker/:id
+router.put(
+  '/talker/:id',
+  validateTalker.checkToken,
+  validateTalker.checkAge,
+  validateTalker.checkName,
+  validateTalker.checkTalk,
+  (req, res) => {
+    const { id } = req.params;
+    const talkers = JSON.parse(fs.readFileSync(list, 'utf-8'));
+    const { name, age, talk } = req.body;
+
+    const editTalker = talkers.filter((talker) => talker.id !== Number(id));
+
+    editTalker.push({ id: Number(id), name, age, talk });
+
+    fs.writeFileSync(list, JSON.stringify(editTalker));
+
+    res.status(200).json({ id: Number(id), name, age, talk });
   },
 );
 

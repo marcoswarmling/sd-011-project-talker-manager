17a0c1400e8c33703828bc62aaf6f95269278c76
Naitwa Alexandre <naitwa-alexandre@gmail.com>
2021-10-07 11:07:03 -0300

    02

--- Ultils.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-function getTalkers() {
-  fs.readFileSync('./talker.json', 'utf8')
-    .then((contentFile) => JSON.parse(contentFile));
-}
-                       
-module.exports = getTalkers;
--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
-const fs = require('fs');
-// const ultils = require('./Ultils');
+const fs = require('fs').promises;
 
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-app.get('/talker', (_req, res) => {
-  const talkers = fs.readFileSync('./talker.json', 'utf8')
-  .then((contentFile) => JSON.parse(contentFile));
-  res.status(HTTP_OK_STATUS).json(talkers);
+async function readTalker() {
+  const talker = await fs.readFile('./talker.json');
+  return JSON.parse(talker);
+}
+
+app.get('/talker', async (_req, res) => {
+  const talker = await readTalker();
+  res.status(HTTP_OK_STATUS).json(talker);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const talker = await readTalker();
+  const { id } = req.params;
+  const resultado = await talker.find((obj) => obj.id === Number(id));
+  console.log(resultado);
+  if (resultado === undefined) {
+    return res.status(404).send({ 
+      message: 'Pessoa palestrante não encontrada', 
+    }); 
+  }
+  res.status(HTTP_OK_STATUS).send(resultado);
 });
 
 // não remova esse endpoint, e para o avaliador funcionar

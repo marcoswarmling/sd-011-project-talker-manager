634edef9e20532c2dfda27918f466456ab0bb844
jhenriquemargrafm <jh.margraf@gmail.com>
2021-10-07 07:47:54 -0300

    Req2 agora com readFileSync

--- middlewares/talker.js
+++ middlewares/talker.js
@@ -9,4 +9,14 @@ router.get('/talker', (_req, res) => {
   res.status(200).json(talkers);
 });
 
+// Requisito2 - GET com endpoint /talker/:id
+router.get('/talker/:id', (req, res) => {
+  const { id } = req.params;
+  const talkers = JSON.parse(fs.readFileSync('./talker.json', 'utf-8'));
+  const chosenTalker = talkers.find((talker) => talker.id === parseInt(id, 10));
+
+  if (!chosenTalker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  res.status(200).json(chosenTalker);
+});
+
 module.exports = router;

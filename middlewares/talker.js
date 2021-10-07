c24bbe1692d3b2f0cce9252222878657d656c887
jhenriquemargrafm <jh.margraf@gmail.com>
2021-10-07 09:41:42 -0300

    Req4 - uso de writeFileSync e condiçoes do JSON para o POST

--- middlewares/talker.js
+++ middlewares/talker.js
@@ -5,6 +5,7 @@ const crypto = require('crypto');
 const router = express.Router();
 
 const validateLogin = require('./login');
+const validateNewTalker = require('./newTalker');
 
 // Requisito 1 - GET com todos os talkers
 router.get('/talker', (_req, res) => {
@@ -27,5 +28,24 @@ router.post('/login', validateLogin.checkEmail, validateLogin.checkPassword, (_r
     const token = crypto.randomBytes(8).toString('hex');
     res.status(200).json({ token });
   });
+
+// Requisito 4 - Endpoint POST /talker
+router.post(
+  '/talker',
+  validateNewTalker.checkToken,
+  validateNewTalker.checkAge,
+  validateNewTalker.checkName,
+  validateNewTalker.checkTalk,
+  (req, res) => {
+    const talkers = JSON.parse(fs.readFileSync('./talker.json', 'utf-8'));
+    const { name, age, talk } = req.body;
+    const newId = talkers.length + 1;
+    talkers.push({ id: newId, name, age, talk });
+
+    fs.writeFileSync('./talker.json', JSON.stringify(talkers));
+
+    res.status(201).json({ id: newId, name, age, talk });
+  },
+);
 
 module.exports = router;

b339b47d57ea4dface5d54fb8ccc1dcf81f8d396
Vinicius <viniciusfod@gmail.com>
2021-10-06 21:48:22 -0300

    lint

--- routes/talkerRouter.js
+++ routes/talkerRouter.js
@@ -8,28 +8,16 @@ const validateName = require('../middlewares/validateName');
 const validateAge = require('../middlewares/validateAge');
 const validateToken = require('../middlewares/validateToken');
 const validateFields = require('../middlewares/validateFields');
+const validateRate = require('../middlewares/validateRate');
 
 const talkerFile = './talker.json';
 
 router.get('/', (_req, res) => {
   try {
-    const talker = fs.readFileSync(talkerFile, 'utf8');
-    res.status(200).json(JSON.parse(talker));
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-});
-
-router.get('/:id', (req, res) => {
-  try {
-    const { id } = req.params;
     const talkers = fs.readFileSync(talkerFile, 'utf8');
-    const talker = JSON.parse(talkers).find((t) => Number(t.id) === Number(id));
-    return talker
-      ? res.status(200).json(talker)
-      : res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    res.status(200).json(JSON.parse(talkers));
   } catch (err) {
-    res.status(404).json({ error: err.message });
+    res.status(400).json({ message: err.message });
   }
 });
 
@@ -48,56 +36,68 @@ router.get('/search', validateToken, (req, res) => {
   res.status(200).end();
 });
 
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  try {
+    const talkers = JSON.parse(fs.readFileSync(talkerFile, 'utf8'));
+    const talker = talkers.find((person) => Number(person.id) === Number(id));
+    return talker 
+      ? res.status(200).json(talker)
+      : res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+});
+
 router.post('/',
   validateToken,
-  validateFields,
-  validateTalk,
-  validateTalkObject,
+  validateName, 
   validateAge,
-  validateName, (req, res) => {
+  validateTalk,
+  validateFields,
+  validateTalkObject,
+  (req, res) => {
     const { name, age, talk } = req.body;
+
     try {
-      const talkers = fs.readFileSync(talkerFile, 'utf8');
+      const talkers = JSON.parse(fs.readFileSync(talkerFile, 'utf8'));
       const lastTalker = talkers[talkers.length - 1];
-      const newTalker = {
-        id: Number(lastTalker.id) + 1,
-        name,
-        age,
-        talk,
-      };
+      const newTalker = { name, age, talk, id: Number(lastTalker.id) + 1 };
       talkers.push(newTalker);
-      fs.writeFileSync(talkerFile, 'utf8', JSON.stringify(talkers));
+
+      fs.writeFileSync(talkerFile, JSON.stringify(talkers));
       res.status(201).json(newTalker);
     } catch (err) {
       res.status(400).json({ message: err.message });
     }
-  });
+});
 
 router.put('/:id',
   validateToken,
-  validateFields,
-  validateTalk,
-  validateTalkObject,
+  validateName, 
   validateAge,
-  validateName, (req, res) => {
-    const { id } = req.params;
-    const { name, age, talk } = req.body;
-    try {
-      const talkers = JSON.parse(fs.readFileSync(talkerFile, 'utf8'));
-      const editedTalker = { name, age, talk, id: Number(id) };
-      const newList = talkers.map((talker) => {
-        if (Number(talker.id) === Number(id)) {
-          return editedTalker;
-        }
-        return talker;
-      });
+  validateTalk,
+  validateRate,
+  validateFields,
+  validateTalkObject, (req, res) => {
+  const { id } = req.params;
+  const { name, age, talk } = req.body;
+  try {
+    const talkers = JSON.parse(fs.readFileSync(talkerFile, 'utf8'));
+    const editedTalker = { name, age, talk, id: Number(id) };
+    const newList = talkers.map((talker) => {
+      if (Number(talker.id) === Number(id)) {
+        return editedTalker;
+      }
+      return talker;
+    });
 
-      fs.writeFileSync(talkerFile, 'utf8', JSON.stringify(newList));
-      res.status(200).json(editedTalker);
-    } catch (err) {
-      res.status(400).json({ message: err.message });
-    }
-  });
+    fs.writeFileSync(talkerFile, JSON.stringify(newList));
+    res.status(200).json(editedTalker);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 router.delete('/:id', validateToken, (req, res) => {
   const { id } = req.params;
@@ -115,4 +115,4 @@ router.delete('/:id', validateToken, (req, res) => {
   }
 });
 
-module.exports = router;
+module.exports = router; 

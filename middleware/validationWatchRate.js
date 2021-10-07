0560bcfb76de0f86a724582a12b754e379abd04b
jorgemeyrelles <jotaengpuc@gmail.com>
2021-10-07 13:30:59 -0300

    feat: requisito 4

--- middleware/validationWatchRate.js
+++ middleware/validationWatchRate.js
@@ -3,13 +3,13 @@ const HTTP_ERROR_STATUS = 400;
 const watchRateValid = (req, res, next) => {
   const { talk } = req.body;
 
-  const dateFormat = /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(0[1-9]|1[1-9]|2[1-9])$/;
+  const dateFormat = /^[0-9]{2}[/]{1}[0-9]{2}[/]{1}[0-9]{4}$/g;
 
-  if (dateFormat.test(talk.watchedAt)) {
+  if (!dateFormat.test(talk.watchedAt)) {
     return res.status(HTTP_ERROR_STATUS)
       .json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
   }
-  if (parseInt(talk.rate, 10) < 0 && parseInt(talk.rate, 10) >= 5) {
+  if (talk.rate < 1 || talk.rate > 5) {
     return res.status(HTTP_ERROR_STATUS)
       .json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   }

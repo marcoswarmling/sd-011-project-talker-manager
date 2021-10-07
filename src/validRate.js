1ada6dd5210040d954ede1e9067110f7f1386ef2
Leandro Lopes <leandro_carlos@uol.com.br>
2021-10-07 18:12:50 -0300

    finalmente resolvido ex 5

--- src/validRate.js
+++ src/validRate.js
@@ -1,11 +1,9 @@
 const validRate = (req, res, next) => {
-    const { talk: { rate } } = req.body;
-    const rateEntreUmeCinco = /^[1-5]*$/;
-  
-    if (!rateEntreUmeCinco.test(rate)) {
+  if (Number.isInteger(req.body.talk.rate) 
+  && req.body.talk.rate >= 1 && req.body.talk.rate <= 5) {
+    return next();
+  } 
       return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
-    }
-    next();
   };
-
+ 
 module.exports = validRate;
--- src/validTalk.js
+++ src/validTalk.js
@@ -1,12 +1,10 @@
 const validTalk = (req, res, next) => {
-    const { talk } = req.body;
-     if (!talk || !talk.watchedAt || !talk.rate) {
-      return res.status(400).json(
-        { message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' },
-      );
-    }
-  
-    next();
+  if (!req.body.talk || typeof req.body.talk.rate !== 'number' || !req.body.talk.watchedAt) {
+    return res.status(400)
+    .json({ message: 
+      'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+      });
+  }
+  next();
 };
-
 module.exports = validTalk;

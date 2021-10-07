2ad19daf3cd37a68ad0c2f61eaa66e7f20dd98b8
Vinicius <viniciusfod@gmail.com>
2021-10-06 21:33:00 -0300

    :(

--- middlewares/validateEmail.js
+++ middlewares/validateEmail.js
@@ -1,13 +1,13 @@
 function validateEmail(req, res, next) {
   const { email } = req.body;
 
-  const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.([a-z]+))?$/i;
+  const regexEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.([a-z]+))?$/i;
 
   if (!email || email === '') {
     return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
 
-  if (!emailRegex.test(email)) {
+  if (!regexEmail.test(email)) {
     return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
 

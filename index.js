1525377928ede5c9bd250409c097374589bdd099
lucasSCsantos <luc.cristovam10@gmail.com>
2021-10-07 13:41:04 -0300

    tudo menos lint

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const fs = require('fs').promises;
 const bodyParser = require('body-parser');
-const { talkerDataMiddleware, authMiddleware, getToken } = require('./talkerData');
+const { 
+  talkerDataMiddleware,
+  authMiddleware,
+  getToken,
+  authPostMiddleware,
+  authDeleteMiddleware,
+} = require('./talkerData');
 
 const app = express();
 app.use(bodyParser.json());
@@ -16,28 +23,86 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+app.post('/talker', authDeleteMiddleware, authPostMiddleware, (req, res) => {
+  const { data } = req;
+  const { body } = req;
+  const { name, age, talk } = body;
+  const id = data[data.length - 1].id + 1;
+  data.push({ name, id, age, talk });
+  fs.writeFile(TALKER, JSON.stringify(data))
+    .then(() => {
+      res.status(201).json({ name, id, age, talk });
+    })
+    .catch((err) => {
+      res.status(400).json({ message: `Erro ao escrever o arquivo: ${err.message}` });
+    });
+});
 
-//req1
+app.put('/talker/:id', authDeleteMiddleware, authPostMiddleware, (req, res) => {
+  const { id } = req.params;
+  const { name, age, talk } = req.body;
+  const { data } = req;
+
+  const talkerIndex = data.findIndex((r) => r.id === parseInt(id, 10));
+
+  if (talkerIndex === -1) return res.status(404).json({ message: 'Talker not find!' });
+
+  data[talkerIndex] = { ...data[talkerIndex], name, age, talk };
+
+  fs.writeFile(TALKER, JSON.stringify(data))
+    .then(() => {
+      res.status(200).json({ name, id: parseInt(id, 10), age, talk });
+    })
+    .catch((err) => {
+      res.status(400).json({ message: `Erro ao escrever o arquivo: ${err.message}` });
+    });
+});
+
+app.delete('/talker/:id', authDeleteMiddleware, (req, res) => {
+  const { id } = req.params;
+  const { data } = req;
+
+  const talkerIndex = data.findIndex((r) => r.id === parseInt(id, 10));
+
+  if (talkerIndex === -1) return res.status(404).json({ message: 'Talker not found!' });
+
+  data.splice(talkerIndex, 1);
+
+  fs.writeFile(TALKER, JSON.stringify(data))
+    .then(() => {
+      res.status(200).json({ message: 'Pessoa palestrante deletada com sucesso' });
+    })
+    .catch((err) => {
+      res.status(400).json({ message: `Erro ao escrever o arquivo: ${err.message}` });
+    });
+});
+
 app.get('/talker', (req, res) => {
-  const data = req.data;
+  const { data } = req;
 
   if (data.length === 0) return res.status(HTTP_OK_STATUS).json([]);
 
   res.status(HTTP_OK_STATUS).json(data);
 });
 
-//req2
+app.get('/talker/search', authDeleteMiddleware, (req, res) => {
+  const { q } = req.query;
+  const { data } = req;
+  const filteredData = data.filter((d) => d.name.includes(q));
+  if (!filteredData) return res.status(200).json([]);
+  res.status(200).json(filteredData);
+});
+
 app.get('/talker/:id', (req, res) => {
   const { id } = req.params;
-  const data = req.data;
-  const talker = data.find((d) => d.id === parseInt(id));
+  const { data } = req;
+  const talker = data.find((d) => d.id === parseInt(id, 10));
 
   if (!talker) res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 
   res.status(200).json(talker);
 });
 
-//req3
 app.post('/login', authMiddleware, (req, res) => {
   res.status(200).json({
     token: getToken(),
--- talkerData.js
+++ talkerData.js
@@ -1,36 +1,107 @@
 const fs = require('fs');
 const crypto = require('crypto');
+
 const TALKER = 'talker.json';
 
 const talkerDataMiddleware = (req, res, next) => {
-	const data = JSON.parse(fs.readFileSync(TALKER, 'utf8'));
-
-	req.data = data;
-
-	next();
-}
+  const data = JSON.parse(fs.readFileSync(TALKER, 'utf8'));
+  req.data = data;
+  next();
+};
 
 const authMiddleware = (req, res, next) => {
-	const { email, password } = req.body;
-	console.log(email, password)
-	const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+  const { email, password } = req.body;
+  const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 
-	if (!email || email === '') return res.status(400).json({ message: "O campo \"email\" é obrigatório" });
-	if (!regex.test(email)) return res.status(400).json({ message: "O \"email\" deve ter o formato \"email@email.com\"" });
-	if (!password || password === '') return res.status(400).json({ message: "O campo \"password\" é obrigatório" });
-	if (password.length < 6) return res.status(400).json({ message: "O \"password\" deve ter pelo menos 6 caracteres" });
+  if (!email || email === '') return res.status(400).json({
+    message: 'O campo "email" é obrigatório',
+  });
 
-	next();
+  if (!regex.test(email)) {
+    return res.status(400).json({
+      message: 'O "email" deve ter o formato "email@email.com"',
+    });
+  }
 
-}
+  if (!password || password === '') {
+    return res.status(400).json({
+      message: 'O campo "password" é obrigatório',
+    });
+  }
+
+  if (password.length < 6) {
+    return res.status(400).json({
+      message: 'O "password" deve ter pelo menos 6 caracteres',
+    });
+  }
+
+  next();
+};
+
+const isEmptyObject = (object) => 
+  Object.values(object).length > 0 
+  ? false 
+  : true;
+
+const validateTalk = (talk) => {
+  if (!talk || isEmptyObject(talk) || !talk.watchedAt || talk.rate === undefined) return false;
+  return true;
+};
+
+const valiDate = (date) => {
+  const dateRegex = /(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d/;
+  if (!dateRegex.test(date)) return false;
+  return true;
+};
+
+const authDeleteMiddleware = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) return res.status(401).json({ message: 'Token não encontrado' });
+  if (token.length !== 16) return res.status(401).json({ message: 'Token inválido' });
+
+  next();
+};
+
+const authPostMiddleware = (req, res, next) => {
+  const { name, age, talk, } = req.body;
+
+  if (!name || name === '') return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  if (name.length < 3) return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  if (!age) return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  if (age < 18) return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+
+  if (!validateTalk(talk)) {
+    return res.status(400).json({
+      message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios'
+    });
+  }
+
+  if (!valiDate(talk.watchedAt)) {
+    return res.status(400).json({
+      message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"',
+    });
+  
+  }
+  
+  if (talk.rate < 1 || talk.rate > 5 ) {
+    return res.status(400).json({
+      message: 'O campo "rate" deve ser um inteiro de 1 à 5',
+    });
+  }
+
+  next();
+};
 
 const getToken = () => {
-	const token = crypto.randomBytes(8).toString('hex');
-	return token;
-}
+  const token = crypto.randomBytes(8).toString('hex');
+  return token;
+};
 
 module.exports = {
-	talkerDataMiddleware,
-	authMiddleware,
-	getToken,
-}
+  talkerDataMiddleware,
+  authMiddleware,
+  authPostMiddleware,
+  authDeleteMiddleware,
+  getToken,
+};

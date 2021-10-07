0e8f90259df708ea265137cf06e3f3cb2a9169d5
jhenriquemargrafm <jh.margraf@gmail.com>
2021-10-07 08:19:07 -0300

    Req3- uso de require-crypto

--- middlewares/talker.js
+++ middlewares/talker.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
 const express = require('express');
+const crypto = require('crypto');
 
 const router = express.Router();
+
+const validateLogin = require('./login');
 
 // Requisito 1 - GET com todos os talkers
 router.get('/talker', (_req, res) => {
@@ -9,7 +12,7 @@ router.get('/talker', (_req, res) => {
   res.status(200).json(talkers);
 });
 
-// Requisito2 - GET com endpoint /talker/:id
+// Requisito 2 - GET com endpoint /talker/:id
 router.get('/talker/:id', (req, res) => {
   const { id } = req.params;
   const talkers = JSON.parse(fs.readFileSync('./talker.json', 'utf-8'));
@@ -18,5 +21,11 @@ router.get('/talker/:id', (req, res) => {
   if (!chosenTalker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   res.status(200).json(chosenTalker);
 });
+
+// Requisito 3 - Endpoint POST /login
+router.post('/login', validateLogin.checkEmail, validateLogin.checkPassword, (_req, res) => {
+    const token = crypto.randomBytes(8).toString('hex');
+    res.status(200).json({ token });
+  });
 
 module.exports = router;

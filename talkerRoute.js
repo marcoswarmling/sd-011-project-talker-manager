b897d25e9f39e5c5797c717b96f3c4cf02948733
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-10-07 15:33:41 -0300

    [Cesar BHering] - Projeto Talker Manager

---
+++ talkerRoute.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+const loadSpeakers = require('./utils');
+
+router.use(loadSpeakers);
+
+router.get('/', (req, res) => {
+  if (req.speakers.length === 0) return res.status(200).json([]);
+  res.status(200).json(req.speakers);
+});
+
+module.exports = router;
---
+++ utils.js
@@ -0,0 +1,15 @@
+const fs = require('file-system');
+
+const loadSpeakers = (req, res, next) => {
+  let speakers = [];
+  try {
+    const data = fs.readFileSync('talker.json', 'utf8');
+    speakers = JSON.parse(data);
+  } catch (err) {
+    res.status(200).json([]);
+  }
+  req.speakers = speakers;
+  next();
+};
+
+module.exports = loadSpeakers;

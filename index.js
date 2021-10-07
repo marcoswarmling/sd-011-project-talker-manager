2ed3d7098c63c185db804daa7ecf934087bd6236
Matheus Gaspar <msouzagaspar@gmail.com>
2021-10-07 14:25:54 +0200

    requisito 2

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND = 404;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -13,10 +14,21 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.get('/talker', (request, response) => {
+app.get('/talker', (_request, response) => {
   const data = fs.readFileSync('./talker.json');
   const talkers = JSON.parse(data);
   response.status(HTTP_OK_STATUS).json(talkers);
+});
+
+app.get('/talker/:id', (request, response) => {
+  const talkerId = Number(request.params.id);
+  const data = fs.readFileSync('./talker.json');
+  const talkers = JSON.parse(data);
+  const talker = talkers.find((t) => t.id === talkerId);
+  if (!talker) {
+    return response.status(HTTP_NOT_FOUND).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return response.status(HTTP_OK_STATUS).json(talker);
 });
 
 app.listen(PORT, () => {

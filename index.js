b71439e9addc7fbd9a4e5de8797dd4d711002b23
Maran <maranmorimoto@gmail.com>
2021-10-07 13:36:24 -0300

    requirement 04 done.

--- index.js
+++ index.js
@@ -57,3 +57,73 @@ app.post('/login', validateEmail, validatePassword, (_req, res) => {
   const token = randomToken(16);
   return res.status(200).json({ token });
 });
+
+function validateToken(req, res, next) {
+  const { authorization } = req.headers;
+  const token = authorization;
+  if (!token) return res.status(401).send({ message: 'Token não encontrado' });
+  if (token.length < 16) return res.status(401).send({ message: 'Token inválido' });
+  next();
+}
+
+function validateName(req, res, next) {
+  const { name } = req.body;
+  if (!name) return res.status(400).send({ message: 'O campo "name" é obrigatório' });
+  if (name.length < 3) {
+    return res.status(400).send({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  next();
+}
+
+function validateAge(req, res, next) {
+  const { age } = req.body;
+  if (!age) return res.status(400).send({ message: 'O campo "age" é obrigatório' });
+  if (age < 18) {
+    return res.status(400).send({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  next();
+}
+
+function validateTalk(req, res, next) {
+  const { talk } = req.body;
+  if (!talk || !talk.watchedAt || talk.rate === undefined || talk.rate === '') {
+    return res.status(400).send({ 
+      message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+    });
+  }
+  next();
+}
+
+function validatewatchedAt(req, res, next) {
+  const { talk: { watchedAt } } = req.body;
+  const regexDate = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/;
+
+  if (!regexDate.test(watchedAt)) {
+    return res.status(400).send({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  next();
+}
+
+function validateRate(req, res, next) {
+  const { talk: { rate } } = req.body;
+  if (rate < 1 || rate > 5 || !Number.isInteger(rate)) {
+    return res.status(400).send({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  next();
+}
+
+app.post('/talker', 
+  validateToken,
+  validateName,
+  validateAge,
+  validateTalk,
+  validatewatchedAt,
+  validateRate, async (req, res) => {
+  const { name, age, talk } = req.body;
+  const readFile = await fs.readFile('./talker.json', 'utf-8');
+  const talkers = JSON.parse(readFile);
+  const id = talkers.length + 1;
+  const addTalker = [...talkers, { name, age, id, talk }];
+  await fs.writeFile('./talker.json', JSON.stringify(addTalker));
+  return res.status(201).send({ name, age, id, talk });
+});

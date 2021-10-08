d13860a16452bb297dfb570f4e929315d43ceb9c
guinhoanjos <iago.anjos@hotmail.com>
2021-10-08 20:11:04 -0300

    validatename mudado

--- addTalkerFunctions.js
+++ addTalkerFunctions.js
@@ -12,7 +12,9 @@ function validateToken(req, res, next) {
 
 function validateName(req, res, next) {
   const { name } = req.body;
-  if (!name || name === '') res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  if (!name || name === '' || name === undefined) { 
+    res.status(400).json({ message: 'O campo "name" é obrigatório' }); 
+}
   if (name.length < 3) {
  res.status(400).json({ 
     message: 'O "name" deve ter pelo menos 3 caracteres', 

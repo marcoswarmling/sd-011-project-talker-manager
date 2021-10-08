3a354acc6ce5d3bd599ebdc7ee45297e3a00de40
Matheus Gaspar <msouzagaspar@gmail.com>
2021-10-08 12:09:53 +0200

    requisito 4

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs');
-const { HTTP_OK_STATUS, HTTP_NOT_FOUND } = require('./httpStatusCodes');
-const { validateEmail, validatePassword } = require('./middlewares');
+const fs = require('fs').promises;
+const { HTTP_OK_STATUS, HTTP_NOT_FOUND, HTTP_CREATED_STATUS } = require('./httpStatusCodes');
+const { 
+  validateEmail,
+  validatePassword,
+  authentication,
+  validateNewTalker,
+} = require('./middlewares');
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,15 +19,15 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.get('/talker', (_request, response) => {
-  const data = fs.readFileSync('./talker.json');
+app.get('/talker', async (_request, response) => {
+  const data = await fs.readFile('./talker.json');
   const talkers = JSON.parse(data);
   response.status(HTTP_OK_STATUS).json(talkers);
 });
 
-app.get('/talker/:id', (request, response) => {
+app.get('/talker/:id', async (request, response) => {
   const talkerId = Number(request.params.id);
-  const data = fs.readFileSync('./talker.json');
+  const data = await fs.readFile('./talker.json');
   const talkers = JSON.parse(data);
   const talker = talkers.find((t) => t.id === talkerId);
   if (!talker) {
@@ -37,6 +42,24 @@ app.post('/login',
   (_request, response) => {
   const token = 'randomToken12345';
   return response.status(HTTP_OK_STATUS).json({ token });
+});
+
+app.post('/talker', authentication, ...validateNewTalker, async (request, response) => {
+  const { name, age, talk } = request.body;
+  const data = await fs.readFile('./talker.json');
+  const talkers = JSON.parse(data);
+  const newTalker = { 
+    name,
+    age,
+    id: talkers.length + 1,
+    talk,
+  };
+  const newData = [
+    ...talkers,
+    newTalker,
+  ];
+  await fs.writeFile('./talker.json', JSON.stringify(newData));
+  return response.status(HTTP_CREATED_STATUS).json(newTalker);
 });
 
 app.listen(PORT, () => {
--- middlewares.js
+++ middlewares.js
@@ -1,4 +1,5 @@
-const { HTTP_BAD_REQUEST } = require('./httpStatusCodes');
+const { isMatch } = require('date-fns');
+const { HTTP_BAD_REQUEST, HTTP_UNAUTHORIZED } = require('./httpStatusCodes');
 
 const validateEmail = (request, response, next) => {
   const { email } = request.body;
@@ -35,4 +36,95 @@ const validatePassword = (request, response, next) => {
   return next();
 };
 
-module.exports = { validateEmail, validatePassword };
+const authentication = (request, response, next) => {
+  const { authorization: authToken } = request.headers;
+  if (!authToken) {
+    return response.status(HTTP_UNAUTHORIZED).json({
+      message: 'Token não encontrado',
+    });
+  }
+  if (authToken.length !== 16) {
+    return response.status(HTTP_UNAUTHORIZED).json({
+      message: 'Token inválido',
+    });
+  }
+  return next();
+};
+
+const validateName = (request, response, next) => {
+  const { name } = request.body;
+  if (!name) {
+    return response.status(HTTP_BAD_REQUEST).json({
+      message: 'O campo "name" é obrigatório',
+    });
+  }
+  if (name.length < 3) {
+    return response.status(HTTP_BAD_REQUEST).json({
+      message: 'O "name" deve ter pelo menos 3 caracteres',
+    });
+  }
+  return next();
+};
+
+const validateAge = (request, response, next) => {
+  const { age } = request.body;
+  if (!age) {
+    return response.status(HTTP_BAD_REQUEST).json({
+      message: 'O campo "age" é obrigatório',
+    });
+  }
+  if (age < 18) {
+    return response.status(HTTP_BAD_REQUEST).json({
+      message: 'A pessoa palestrante deve ser maior de idade',
+    });
+  }
+  return next();
+};
+
+const validateTalk = (request, response, next) => {
+  const { talk } = request.body;
+  if (!talk || !talk.watchedAt || !talk.rate) {
+    return response.status(HTTP_BAD_REQUEST).json({
+      message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+    });
+  }
+
+  return next();
+};
+
+const validateTalkDate = (request, response, next) => {
+  const { talk: { watchedAt } } = request.body;
+  const isValidDate = isMatch(watchedAt, 'dd/MM/yyyy');
+  if (!isValidDate) {
+    return response.status(HTTP_BAD_REQUEST).json({
+      message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"',
+    });
+  }
+  return next();
+};
+
+const validateTalkRate = (request, response, next) => {
+  const { talk: { rate } } = request.body;
+  const isValidRate = Number.isInteger(rate) && rate >= 1 && rate <= 5;
+  if (!isValidRate) {
+    return response.status(HTTP_BAD_REQUEST).json({
+      message: 'O campo "rate" deve ser um inteiro de 1 à 5',
+    });
+  }
+  return next();
+};
+
+const validateNewTalker = [
+  validateName,
+  validateAge,
+  validateTalk,
+  validateTalkDate,
+  validateTalkRate,
+];
+
+module.exports = {
+  validateEmail,
+  validatePassword,
+  authentication,
+  validateNewTalker,
+};

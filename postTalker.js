6c4fc701f7312c5cf9f53ca9f14a9c0b12fb84ce
Wilquer82 <wilquer.fig@gmail.com>
2021-10-09 11:00:57 -0300

    four + index + validations + lint

--- postTalker.js
+++ postTalker.js
@@ -2,7 +2,6 @@ const readFile = require('./models/utils');
 
 const HTTP_CREATED_STATUS = 201;
 
-
 const createdTalker = async (req, res) => {
   const { name, age, talk } = req.body;
   const create = await readFile.readFileTalker();

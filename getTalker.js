0dd51a8877cfd64cd5f143b0fe8fee9ab046f4ee
Wilquer82 <wilquer.fig@gmail.com>
2021-10-09 09:37:11 -0300

    one

---
+++ getTalker.js
@@ -0,0 +1,13 @@
+const readFile = require('./models/utils');
+
+const HTTP_OK_STATUS = 200;
+
+const getTalkers = async (req, res) => {
+  const talkers = await readFile.readFileTalker();
+
+  if (talkers === []) return res.status(200).json([]);
+
+  res.status(HTTP_OK_STATUS).json(talkers);
+};
+
+module.exports = getTalkers;

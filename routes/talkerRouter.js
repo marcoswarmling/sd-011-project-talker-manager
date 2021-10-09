19d3ae2cebca36cd9c8026e08a9d32e6092074b8
Dário Junior <dario.junior.rs@gmail.com>
2021-10-09 13:41:38 -0300

    Resolve 2

--- routes/talkerRouter.js
+++ routes/talkerRouter.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const DATA_PATH = path.join(__dirname, '../', 'talker.json');
 const HTTP_OK_STATUS = 200;
 const BAD_RESQUEST_STATUS = 400;
+const NOT_FOUND_STATUS = 404;
 
 router.get('/', (req, res) => {
   try {
@@ -13,6 +14,21 @@ router.get('/', (req, res) => {
     res.status(HTTP_OK_STATUS).json(talkers);
   } catch (err) {
     res.status(BAD_RESQUEST_STATUS).json({ message: err.message });
+  }
+});
+
+router.get('/:id', (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = fs.readFileSync(DATA_PATH, 'utf8');
+    const talkers = JSON.parse(data);
+    const foundedTalker = talkers.find((t) => t.id === Number(id));
+    if (!foundedTalker) {
+      return res.status(NOT_FOUND_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+    return res.status(HTTP_OK_STATUS).send(foundedTalker);
+  } catch (err) {
+    res.status(BAD_RESQUEST_STATUS).send({ message: err.message });
   }
 });
 

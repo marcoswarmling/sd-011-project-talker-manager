716161868ba5dd21040011955737068bf96a3a6a
Wilquer82 <wilquer.fig@gmail.com>
2021-10-09 10:21:56 -0300

    two + index

---
+++ getTalkerId.js
@@ -0,0 +1,19 @@
+const readFile = require('./models/utils');
+
+const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND_STATUS = 404;
+
+const getTalkerId = async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readFile.readFileTalker();
+
+  const talk = talkers.find((talker) => talker.id === Number(id));
+
+  if (!talk) {
+    return res.status(HTTP_NOT_FOUND_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  res.status(HTTP_OK_STATUS).json(talk);
+};
+
+module.exports = getTalkerId;

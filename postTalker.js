8ac5b2a71bf3961d929482aaa8c874514a87389b
Wilquer82 <wilquer.fig@gmail.com>
2021-10-09 11:24:00 -0300

    four + index + validations + lint

--- postTalker.js
+++ postTalker.js
@@ -1,6 +1,8 @@
 const readFile = require('./models/utils');
 
 const HTTP_CREATED_STATUS = 201;
+const HTTP_UNAUTHORIZED_STATUS = 401;
+const HTTP_BAD_REQUEST_STATUS = 400;
 
 const createdTalker = async (req, res) => {
   const { name, age, talk } = req.body;
@@ -16,6 +18,83 @@ const createdTalker = async (req, res) => {
   res.status(HTTP_CREATED_STATUS).json(newUser);
 };
 
+const authorizationToken = async (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(HTTP_UNAUTHORIZED_STATUS).json({ message: 'Token não encontrado' });
+  }
+  if (authorization.length !== 16) {
+    return res.status(HTTP_UNAUTHORIZED_STATUS).json({ message: 'Token inválido' });
+  }
+  next();
+};
+
+const checkName = (req, res, next) => {
+  const { name } = req.body;
+  if (!name || name === '') {
+    return res.status(HTTP_BAD_REQUEST_STATUS)
+      .json({ message: 'O campo "name" é obrigatório' });
+  }
+  if (name.length < 3) {
+    return res.status(HTTP_BAD_REQUEST_STATUS)
+      .json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  next();
+};
+
+const checkAge = (req, res, next) => {
+  const { age } = req.body;
+  if (!age || age === '') {
+    return res.status(HTTP_BAD_REQUEST_STATUS)
+      .json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (age < 18) {
+    return res.status(HTTP_BAD_REQUEST_STATUS)
+      .json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  next();
+};
+
+const checkWatchedAt = (req, res, next) => {
+  const { talk } = req.body;
+  const validDate = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/; // https://www.regextester.com
+  if (!validDate.test(talk.watchedAt)) {
+    return res.status(HTTP_BAD_REQUEST_STATUS)
+      .json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  next();
+};
+
+const checkRate = (req, res, next) => {
+  const { talk } = req.body;
+  if (talk.rate < 1 || talk.rate > 5) {
+    return res.status(HTTP_BAD_REQUEST_STATUS)
+      .json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  next();
+};
+
+const checkTalk = (req, res, next) => {
+  const { talk } = req.body;
+  if (!talk || !talk.watchedAt) {
+    return res.status(HTTP_BAD_REQUEST_STATUS)
+      .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e' 
+      + ' "rate" não podem ser vazios' });
+  }
+  if (!talk.rate && talk.rate !== 0) {
+    return res.status(HTTP_BAD_REQUEST_STATUS)
+      .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e'
+      + ' "rate" não podem ser vazios' });
+  }
+  next();
+};
+
 module.exports = {
   createdTalker,
+  authorizationToken,
+  checkName,
+  checkAge,
+  checkWatchedAt,
+  checkRate,
+  checkTalk,
 };

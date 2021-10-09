b7ed722fd2b97928a68e13a8166d3b751ae2552a
Wilquer82 <wilquer.fig@gmail.com>
2021-10-09 09:43:56 -0300

    one + models

---
+++ models/utils.js
@@ -0,0 +1,12 @@
+
+const { readFile, writeFile } = require('fs').promises;
+
+const readFileTalker = () => readFile('./talker.json', 'utf-8')
+  .then((data) => JSON.parse(data));
+
+const writeFileTalker = (newData) => writeFile('./talker.json', JSON.stringify(newData));
+
+module.exports = {
+  readFileTalker,
+  writeFileTalker,
+};

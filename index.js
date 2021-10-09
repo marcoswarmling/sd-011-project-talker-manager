b7e578663d1d60d05059bf50311962fc39278b38
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-10-09 12:50:04 -0300

    code clean

--- index.js
+++ index.js
@@ -12,14 +12,17 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+// talker router
 const talkerRt = require('./routers/talkerRt.js');
 
 app.use('/talker', talkerRt);
 
+// login router
 const loginRt = require('./routers/loginRt.js');
 
 app.use('/login', loginRt);
 
+// app
 app.listen(PORT, () => {
   console.log('Online');
 });
--- routers/talkerRt.js
+++ routers/talkerRt.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 
 const router = express.Router();
 
+// utils functions
 const {
   loadSpeakers, 
   valToken,
@@ -12,21 +13,27 @@ const {
   valWatchedRated,
 } = require('../utils/utils');
 
+// load speakears
 router.use(loadSpeakers);
 
 // task 1
 // status(200) = Request Success
 router.get('/', (req, res) => {
-  if (req.speakers.length === 0) {
+  if (req.speakers.length <= 0) {
     return res.status(200).json([]);
   }
+
   res.status(200).json(req.speakers);
 });
 
 // task 7
 router.get('/search', valToken, (req, res) => {
   const spkfilter = req.speakers.filter((e) => e.name.includes(req.query.q));
-  if (req.query.q === undefined) return res.status(200).json(req.speakers);
+
+  if (req.query.q === undefined) {
+    return res.status(200).json(req.speakers);
+  }
+
   res.status(200).json(spkfilter);
 });
 
@@ -35,9 +42,11 @@ router.get('/search', valToken, (req, res) => {
 router.get('/:id', (req, res) => {
   const { id } = req.params;
   const spkfilter = req.speakers.filter((e) => e.id === parseInt(id, 10)); // decimal radix
-  if (!id || spkfilter.length === 0) {
+
+  if (!id || spkfilter.length <= 0) {
     return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   }
+
   return res.status(200).json(spkfilter[0]);
 });
 
@@ -48,27 +57,30 @@ router.post('/', valToken, valName, valAge, valTalker, valWatchedRated,
     const { name, age, talk } = req.body;
     const { speakers } = req;
     const id = speakers.length + 1;
-    speakers.push({ name, age, id, talk });
+
+    const aux = { name, age, id, talk };
+
+    speakers.push(aux);
+
     fs.writeFileSync('./talker.json', JSON.stringify(speakers));
-    return res.status(201).json({ name, age, id, talk });
+
+    return res.status(201).json(aux);
 });
 
 // task 5
 router.put('/:id', valToken, valName, valAge, valTalker, valWatchedRated,
- (req, res) => {
+  (req, res) => {
    let { id } = req.params;
    const { name, age, talk } = req.body;
    const { speakers } = req;
    const spkIndex = speakers.findIndex((e) => e.id === parseInt(id, 10));
+
    id = parseInt(id, 10);
-   speakers[spkIndex] = {
-     ...speakers[spkIndex],
-     name,
-     age,
-     talk,
-     id,
-   };
+
+   speakers[spkIndex] = { ...speakers[spkIndex], name, age, talk, id };
+
    fs.writeFileSync('./talker.json', JSON.stringify(speakers));
+
    return res.status(200).json({ name, age, id, talk });
 });
 
@@ -77,8 +89,11 @@ router.delete('/:id', valToken, (req, res) => {
   const { id } = req.params;
   const { speakers } = req;
   const spkIndex = speakers.findIndex((e) => e.id === parseInt(id, 10));
+
   speakers.splice(spkIndex, 1);
+
   fs.writeFileSync('./talker.json', JSON.stringify(speakers));
+
   return res.status(200).json({ message: 'Pessoa palestrante deletada com sucesso' });
 });
 

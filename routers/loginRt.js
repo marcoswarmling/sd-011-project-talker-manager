f9eb7ecb0bf51991545e7e5f12b63103fc696776
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-10-09 10:17:25 -0300

    loginRouter update

--- routers/loginRt.js
+++ routers/loginRt.js
@@ -1,5 +1,13 @@
 const express = require('express');
 
 const router = express.Router();
+const { createToken, validadeEmail, validadePassword } = require('../utils/utils.js');
+
+router.use(createToken);
+
+router.post('/', validadeEmail, validadePassword,
+  (req, res) => {
+  res.status(200).json({ token: `${req.newToken}` });
+});
 
 module.exports = router;

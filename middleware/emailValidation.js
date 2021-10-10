d336bd0dccc801903ceab66911ebf58b23afb7ad
jhonatavbrg <jhonatavbrg@gmail.com>
2021-10-09 23:37:20 -0300

    refactored third req

--- middleware/emailValidation.js
+++ middleware/emailValidation.js
@@ -1,20 +1,19 @@
 const HTTP_BAD_REQUEST_STATUS = 400;
 
-const emailExistAndNotBlank = (email, status) => {
+const emailExistAndNotBlank = (email, req) => {
   if (!email || email === '') {
-    return status(HTTP_BAD_REQUEST_STATUS).json({ message: 'O campo "email" é obrigatório' });
+    return req.status(HTTP_BAD_REQUEST_STATUS).json({ message: 'O campo "email" é obrigatório' });
   }
 };
 
 const emailValidation = (req, res, next) => {
   const { email } = req.body;
-  const { status } = res;
   const emailRegex = /\S+@\S+\.\S+/;
 
-  emailExistAndNotBlank(email, status);
+  emailExistAndNotBlank(email, req);
 
   if (!emailRegex.test(email)) {
-    return status(HTTP_BAD_REQUEST_STATUS).json(
+    return req.status(HTTP_BAD_REQUEST_STATUS).json(
       { message: 'O "email" deve ter o formato "email@email.com"' },
     );
   }

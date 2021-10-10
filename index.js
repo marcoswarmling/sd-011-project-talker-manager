0df3a03d3d71aacb946069dc3c68038b80c43b41
IsaacMagno <isaacmagno98@gmail.com>
2021-10-10 10:32:32 -0300

    desafios

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const fs = require('fs');
+const {
+  emailValidation,
+  tokenGenerate,
+  passValidation,
+  tokenRequest,
+  nameValidation,
+  ageValidation,
+  talkValidation,
+  talkDetailsValidation,
+} = require('./validations/validations');
 
 const app = express();
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_ERROR_STATUS = 500;
+const HTTP_NOTFOUND_STATUS = 404;
 const PORT = '3000';
 const talkers = 'talker.json';
 
@@ -24,7 +37,7 @@ app.get('/talker', (_req, res) => {
     const data = JSON.parse(fs.readFileSync(talkers, 'utf-8'));
     return res.status(HTTP_OK_STATUS).json(data);
   } catch (err) {
-    return res.status(500).json({ message: err });
+    return res.status(HTTP_ERROR_STATUS).json({ message: err });
   }
 });
 
@@ -35,10 +48,107 @@ app.get('/talker/:id', (req, res) => {
     const data = JSON.parse(fs.readFileSync(talkers, 'utf-8'));
     const filterTalker = data.find((target) => target.id === Number(id));
     if (!filterTalker) {
-      return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+      return res.status(HTTP_NOTFOUND_STATUS).json({
+        message: 'Pessoa palestrante não encontrada',
+      });
     }
     return res.status(HTTP_OK_STATUS).json(filterTalker);
   } catch (err) {
-    return res.status(500).json({ message: err });
+    return res.status(HTTP_ERROR_STATUS).json({ message: err });
+  }
+});
+
+// d3
+app.post('/login', emailValidation, passValidation, tokenGenerate, (req, res) => {
+  const { token } = req;
+  res.status(HTTP_OK_STATUS).json({ token: `${token}` });
+});
+
+// d4
+app.post(
+  '/talker',
+  tokenRequest,
+  nameValidation,
+  ageValidation,
+  talkValidation,
+  talkDetailsValidation,
+  (req, res) => {
+  try {
+    const data = JSON.parse(fs.readFileSync(talkers, 'utf-8'));
+    const { name, age, talk } = req.body;
+    const newData = { name, age, talk: { ...talk }, id: data.length + 1 };
+
+    data.push(newData);
+
+    fs.writeFileSync(talkers, JSON.stringify(data));
+    return res.status(201).json(newData);
+  } catch (error) {
+    return res.status(400).json({
+      message: error.message,
+    });
+  }
+  },
+);
+
+// d5
+app.put(
+  '/talker/:id',
+  tokenRequest,
+  nameValidation,
+  ageValidation,
+  talkValidation,
+  talkDetailsValidation,
+  (req, res) => {
+    const { id } = req.params;
+    const { name, age, talk} = req.body;
+    try {
+      const data = JSON.parse(fs.readFileSync(talkers, 'utf-8'));
+      const talker = data.findIndex((person) => person.id === Number(id));
+      if (talker === -1) {
+        return res.status(404).json({ message: 'Talker not found!' });
+      }
+
+      data[talker] = { ...data[talker], name, age, talk };
+      
+      fs.writeFileSync(talkers, JSON.stringify(data));
+      return res.status(200).json(data[talker]);
+    } catch (error) {
+      return res.status(500).json({
+        message: error.message,
+      });
+    }
+  },
+);
+
+// d6
+app.delete('/talker/:id', tokenRequest, (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = JSON.parse(fs.readFileSync(talkers, 'utf-8'));
+    const talker = data.findIndex((person) => person.id === Number(id));
+
+    if (talker === -1) {
+      return res.status(404).json({ message: 'Talker not found!' });
+    }
+
+    data.splice(talker, 1);
+    fs.writeFileSync(talkers, JSON.stringify(data));
+    
+    return res.status(200).json({ message: 'Pessoa palestrante deletada com sucesso' });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+// d7
+app.get('/talker/search', tokenRequest, (req, res) => {
+  const { q } = req.query;
+  try {
+    const data = JSON.parse(fs.readFileSync(talkers, 'utf-8'));
+    const filtaker = data.filter((t) => t.name.toLowerCase().includes(q.toLowerCase()));
+
+    return res.status(200).json(filtaker);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
   }
 });
---
+++ validations/validations.js
@@ -0,0 +1,109 @@
+const { v4: uuidv4 } = require('uuid');
+
+const emailValidation = (req, res, next) => {
+  const regex = /^[^\s@]+@[^\s@]+$/i;
+  const { email } = req.body;
+  if (!email || email === '') {
+    return res.status(400).json({
+      message: 'O "email" deve ter o formato "email@email.com"',
+    });
+  }
+  if (!email.match(regex)) {
+    return res.status(400).json({
+      message: 'O "email" deve ter o formato "email@email.com"',
+    });
+  }
+  next();
+};
+
+const passValidation = (req, res, next) => {
+  const { password } = req.body;
+  if (!password || password === '') {
+    return res.status(400).json({
+      message: 'O campo "password" é obrigatório',
+    });
+  }
+  if (password.length < 6) {
+    return res.status(400).json({
+      message: 'O "password" deve ter pelo menos 6 caracteres',
+    });
+  }
+  next();
+};
+
+const tokenGenerate = (req, _res, next) => {
+  const token = uuidv4().slice(16);
+  req.token = token;
+  next();
+};
+
+const tokenRequest = (req, res, next) => {
+  const { token } = req.headers;
+  
+  if (!token) return res.status(401).json({ message: 'Token não encontrado' });
+  if (token.length < 16) return res.status(401).json({ message: 'Token inválido' });
+
+  next();
+};
+
+const nameValidation = (req, res, next) => {
+  const { name } = req.body;
+  if (!name || name === '') {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+
+  next();
+};
+
+const ageValidation = (req, res, next) => {
+  const { age } = req.body;
+  if (!age || age === '') {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (age < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+
+  next();
+};
+
+const talkValidation = (req, res, next) => {
+  const { talk } = req.body;
+  const { watchedAt, rate } = talk;
+  if (!talk || !watchedAt || !rate) {
+    return res.status(400).json({
+      message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+    });
+  }
+
+  next();
+};
+
+const talkDetailsValidation = (req, res, next) => {
+  const { talk } = req.body;
+  const { watchedAt, rate } = talk;
+  const regexData = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+  if (!watchedAt.match(regexData)) {
+    return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+
+  if (rate < 1 || rate > 5) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+
+  next();
+};
+
+module.exports = {
+  emailValidation,
+  passValidation,
+  tokenGenerate,
+  tokenRequest,
+  nameValidation,
+  ageValidation,
+  talkValidation,
+  talkDetailsValidation,
+};

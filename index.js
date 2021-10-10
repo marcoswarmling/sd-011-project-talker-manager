61b7bd1c8b010cacfd48dd02788237a3dd94c5cf
jhonatavbrg <jhonatavbrg@gmail.com>
2021-10-09 21:07:23 -0300

    add first files

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promisses;
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,9 +8,15 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-// não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res) => {
+  const data = await fs.readFile('./talker.json', 'utf-8');
+  const fetchData = await JSON.parse(data);
+
+  res.status(HTTP_OK_STATUS).json(fetchData);
 });
 
 app.listen(PORT, () => {

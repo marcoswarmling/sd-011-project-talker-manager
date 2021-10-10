9326aeb7cb2badfc7b51c5d3ec1700be46da9b16
Johnny Rubi <johnnyrubischreiner@gmail.com>
2021-10-10 15:53:07 -0300

    tentando resolver erro

---
+++ middlewares/talker/attPerson.js
@@ -0,0 +1,7 @@
+// const attPerson = async (req, res, next ) => {
+//   const { id } = req.params;
+//   const { name, age, talk } = req.body;
+//   try {
+//     const data = await fs.readFile('talker.json', 'utf8').then((f) => JSON.parse(f));
+//   }
+// }

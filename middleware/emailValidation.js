06453f3630082045dacd3056f7c806a4969e3286
jhonatavbrg <jhonatavbrg@gmail.com>
2021-10-09 23:54:29 -0300

    fix third req

--- middleware/emailValidation.js
+++ middleware/emailValidation.js
@@ -5,11 +5,11 @@ const emailValidation = (req, res, next) => {
   const emailRegex = /\S+@\S+\.\S+/;
 
   if (!email || email === '') {
-    return req.status(HTTP_BAD_REQUEST_STATUS).json({ message: 'O campo "email" é obrigatório' });
+    return res.status(HTTP_BAD_REQUEST_STATUS).json({ message: 'O campo "email" é obrigatório' });
   }
 
   if (!emailRegex.test(email)) {
-    return req.status(HTTP_BAD_REQUEST_STATUS).json(
+    return res.status(HTTP_BAD_REQUEST_STATUS).json(
       { message: 'O "email" deve ter o formato "email@email.com"' },
     );
   }

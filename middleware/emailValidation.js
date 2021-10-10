9ee0ef0bc92697dc4be6007ea403315364f552b9
jhonatavbrg <jhonatavbrg@gmail.com>
2021-10-09 23:15:51 -0300

    fix third req

--- middleware/emailValidation.js
+++ middleware/emailValidation.js
@@ -2,7 +2,7 @@ const HTTP_BAD_REQUEST_STATUS = 400;
 
 const emailValidation = (req, res, next) => {
   const { email } = req.body;
-  const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
+  const emailRegex = /\S+@\S+\.\S+/;
 
   if (!email || email === '') {
     return res.status(HTTP_BAD_REQUEST_STATUS).json({ message: 'O campo "email" é obrigatório' });

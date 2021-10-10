9b4a22cdbb41bf2c3a650d51d2910fedb33b6626
nandovbr <nandovbr15@hotmail.com>
2021-10-10 11:11:59 -0300

    adiciona desafio 1

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+app.use(express.json());
+const talkerRoute = require('./routes/talker');
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
@@ -11,6 +13,8 @@ const PORT = '3000';
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+app.use(talkerRoute);
 
 app.listen(PORT, () => {
   console.log('Online');

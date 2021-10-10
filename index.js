eb9c6466b5cc07c3ec4c873ad3ec0f9e19c42391
Caio Oliveira <parafrenteeadiante@gmail.com>
2021-10-09 22:36:44 -0300

    Requisito 2 finalizado!

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_STATUS_404 = 404;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -22,4 +23,17 @@ app.get('/talker', async (req, res) => {
   const talkers = await fs.readFile('./talker.json', 'utf-8');
 
   res.status(HTTP_OK_STATUS).json(JSON.parse(talkers));
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const readData = await fs.readFile('./talker.json', 'utf-8');
+  const talkers = JSON.parse(readData);
+  const { id } = req.params;
+  const talkerFound = talkers.find((talker) => talker.id === parseInt(id, 10));
+
+  if (!talkerFound) {
+    return res.status(HTTP_STATUS_404).send({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  res.status(HTTP_OK_STATUS).json(talkerFound);
 });

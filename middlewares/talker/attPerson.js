43d55fd8727f873adf2e44730e8300d9e60542e1
Johnny Rubi <johnnyrubischreiner@gmail.com>
2021-10-10 19:34:06 -0300

    vamo q vamo

--- middlewares/talker/attPerson.js
+++ middlewares/talker/attPerson.js
@@ -6,18 +6,18 @@ const {
   validationAge } = require('../../utils/validatePerson');
 
 const attPerson = async (req, res, next) => {
-  const token = req.headers.authorization;
-  const { id } = req.params;
-  const { name, age, talk } = req.body;
-  validationToken(token);
-  validationAge(age);
-  validationTalk(talk);
-  validationName(name);
   try {
-  const data = await fs.readFile('talker.json', 'utf8').then((f) => JSON.parse(f));
-  const dataAtt = data.map((e) => (e.id === Number(id) ? { name, age, id, talk } : e));
-  await fs.writeFile('./talker.json', JSON.stringify(dataAtt));
-  return res.status(200).json({ name, age, id, talk });
+    const token = req.headers.authorization;
+    const { id } = req.params;
+    const { name, age, talk } = req.body;
+    validationToken(token);
+    validationAge(age);
+    validationTalk(talk);
+    validationName(name);
+    const data = await fs.readFile('talker.json', 'utf8').then((f) => JSON.parse(f));
+    const dataAtt = data.map((e) => (e.id === Number(id) ? { name, age, id, talk } : e));
+    await fs.writeFile('./talker.json', JSON.stringify(dataAtt));
+    return res.status(200).json({ name, age, id, talk });
   } catch (err) {
     next({ status: 400, message: 'Houve algum erro' });
   }

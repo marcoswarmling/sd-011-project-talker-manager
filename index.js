b3faaf21995d644cf2aa035c19d8155d4fc45b47
Caio Oliveira <parafrenteeadiante@gmail.com>
2021-10-09 23:45:12 -0300

    Desafio 3 finalizado!

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const HTTP_STATUS_404 = 404;
+const HTTP_STATUS_400 = 400;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -36,4 +37,35 @@ app.get('/talker/:id', async (req, res) => {
   }
 
   res.status(HTTP_OK_STATUS).json(talkerFound);
+});
+
+function validatePassword(password, res) {
+  if (!password || password === '') {
+    return res.status(HTTP_STATUS_400).json({ message: 'O campo "password" é obrigatório' });
+  }
+
+  if (password.length < 6) {
+    return res.status(HTTP_STATUS_400).json({
+      message: 'O "password" deve ter pelo menos 6 caracteres',
+    });
+  }
+}
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  const emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
+
+  if (!email || email === '') {
+    return res.status(HTTP_STATUS_400).json({ message: 'O campo "email" é obrigatório' });
+  }
+
+  if (!emailRegex.test(email)) {
+    return res.status(HTTP_STATUS_400).json({
+      message: 'O "email" deve ter o formato "email@email.com"',
+    });
+  }
+
+  validatePassword(password, res);
+
+  res.status(HTTP_OK_STATUS).json({ token: '7mqaVRXJSp886CGr' });
 });

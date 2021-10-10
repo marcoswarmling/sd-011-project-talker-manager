8bb3ab27eda68f883cb2f027070159a4f16ecc79
jhonatavbrg <jhonatavbrg@gmail.com>
2021-10-10 00:23:28 -0300

    add fifth req

---
+++ middleware/updateTalker.js
@@ -0,0 +1,26 @@
+const fs = require('fs').promises;
+
+const HTTP_OK_STATUS = 200;
+
+const updateTalker = async (req, res) => {
+  const data = await fs.readFile('./talker.json', 'utf-8');
+  const fetchData = await JSON.parse(data);
+  const { name, age, talk } = req.body;
+  const { id } = req.params;
+  const talkerId = {
+    id: Number(id),
+    name,
+    age,
+    talk,
+  };
+
+  const updateTalkers = fetchData.map((talker) => {
+    if (talker.id === Number(id)) return talkerId;
+    return talker;
+  });
+
+  await fs.writeFile('./talker.json', JSON.stringify(updateTalkers));
+  res.status(HTTP_OK_STATUS).json(talkerId);
+};
+
+module.exports = updateTalker;

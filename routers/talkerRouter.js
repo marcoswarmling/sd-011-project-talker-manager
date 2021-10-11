037205b2ee797fbbd0c1edb5a7eecabcf723dddd
Luan Xavier <luantenorio3@gmail.com>
2021-10-11 13:57:15 -0300

    Requisito 2 finalizado

--- routers/talkerRouter.js
+++ routers/talkerRouter.js
@@ -7,4 +7,14 @@ router.get('/', rescue(async (_req, res) => {
   res.status(200).json(talkers);
 }));
 
+router.get('/:id', rescue(async (req, res) => {
+  const { id } = req.params;
+  const talkers = JSON.parse(await fs.readFile('./talker.json', 'utf-8'));
+  const selectedTalker = talkers.find((talker) => talker.id === parseInt(id, 10));
+
+  if (!selectedTalker) res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  
+  res.status(200).json(selectedTalker);
+}));
+
 module.exports = router;

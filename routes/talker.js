e6e691a9ac4238c72704fef121b41480ddc84e16
nandovbr <nandovbr15@hotmail.com>
2021-10-11 10:33:10 -0300

    restaura ajusta requisito 2 v1.4

--- routes/talker.js
+++ routes/talker.js
@@ -9,11 +9,11 @@ router.get('/', async (_req, res) => {
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
   const talkers = await fs.readFile('./talker.json', 'utf8');
-  const talkerId = JSON.parse(talkers).find((item) => item.id === Number(id));
-  if (!talkerId) {
+  const search = JSON.parse(talkers).find((item) => item.id === Number(id));
+  if (!search) {
     return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   }
-  return res.status(200).json(talkerId);
+  return res.status(200).json(search);
 });
 
 module.exports = router;

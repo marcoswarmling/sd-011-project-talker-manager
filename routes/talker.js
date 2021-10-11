bac4568e1c8b1aa5f3487f6a0e6e74db7ad4cbe9
nandovbr <nandovbr15@hotmail.com>
2021-10-11 09:36:50 -0300

    requisito 1 quebrado

--- routes/talker.js
+++ routes/talker.js
@@ -6,4 +6,14 @@ router.get('/talker', async (_req, res) => {
   res.status(200).json(JSON.parse(talkers));
 });
 
+router.get('/:id', async (req, res) => {
+  const talkers = await fs.readFile('./talker.json', 'utf8');
+  const talker = JSON.parse(talkers).find((talker) => talker.id === req.params.id);
+  if (talker) {
+    return res.status(200).json(talker);
+  } else {
+   return res.status(404).json({ error: 'Pessoa palestrante não encontrada' });
+  }
+});
+
 module.exports = router;

874e2c7c169bb5d741bbf1bb7bd9a93422ada1bd
nandovbr <nandovbr15@hotmail.com>
2021-10-11 09:59:56 -0300

    restaura ajusta requisito 2 v1.1

--- routes/talker.js
+++ routes/talker.js
@@ -9,7 +9,7 @@ router.get('/talker', async (_req, res) => {
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
   const talkers = await fs.readFile('./talker.json', 'utf8');
-  const talkerId = JSON.parse(talkers).find(item => item.id === Number(id));
+  const talkerId = JSON.parse(talkers).find((item) => item.id === Number(id));
   if (talkerId) {
     return res.status(200).json(talkerId);
   }

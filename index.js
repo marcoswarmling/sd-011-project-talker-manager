e0af359fd4982bf6e9310d71e5bf85af3ab76a3b
Alan Gonçalves <vekio.soft@gmail.com>
2021-10-11 11:39:58 -0300

    requisito 1

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const talkerRoute = require('./routes/talkerRoute');
+
 const app = express();
 app.use(bodyParser.json());
 
@@ -11,6 +13,8 @@ const PORT = '3000';
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+app.use('/talker', talkerRoute);
 
 app.listen(PORT, () => {
   console.log('Online');

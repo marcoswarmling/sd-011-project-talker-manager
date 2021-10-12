0174c47b821b2eb870b37ed0bbf3a8213eabeeb6
Laura Gusmao <laurargus@gmail.com>
2021-10-12 20:08:09 -0300

    Correcoes Evaluator

--- middlewares/validateName.js
+++ middlewares/validateName.js
@@ -4,7 +4,7 @@ function validateName(req, res, next) {
   if (!name) return res.status(400).json({ message: 'O campo "name" é obrigatório' });
 
   if (name.length < 3) {
-    return res.status(400).json({ message: 'O "name" deve ter pelo menos 6 caracteres' });
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
   }
 
   next(); 
--- middlewares/validateTalk.js
+++ middlewares/validateTalk.js
@@ -31,13 +31,13 @@ function validateWatchedAt(req, res, next) {
 function validateRate(req, res, next) {
   const { talk } = req.body;
 
+  if (talk.rate < 1 || talk.rate > 5) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  
   if (!talk.rate) {
     return res.status(400)
     .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
-  }
-
-  if (talk.rate < 1 || talk.rate > 5) {
-    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   }
 
   next(); 

179a7e3bea5e78fb7bd4863c206a52c1e760aaa1
Douglas-Santana <douguinhodns@gmail.com>
2021-10-12 17:21:05 -0300

    req 2

--- routers/talker.js
+++ routers/talker.js
@@ -5,8 +5,22 @@ router.get('/', (_req, res) => {
   try {
     const talkers = JSON.parse(fs.readFileSync('talker.json', 'utf-8'));
     if (!talkers) return res.status(200).json([]);
-    
+
     res.status(200).json(talkers);
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  try {
+    const searchById = JSON.parse(fs.readFileSync('talker.json', 'utf-8'))
+    .find((talker) => talker.id === Number(id));
+
+    if (!searchById) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+    res.status(200).json(searchById);
   } catch (err) {
     res.status(500).json({ err });
   }

c146523bd2680aaa0141967d46258ef5d0a49d8a
Laura Gusmao <laurargus@gmail.com>
2021-10-12 19:31:36 -0300

    Correcao Lint

--- helpers/fs-utils.js
+++ helpers/fs-utils.js
@@ -1,14 +1,14 @@
-const fs = require('fs/promises');
+const fs = require('fs');
 
 const getFile = async (file) => {
-  const data = await fs.readFile(file, 'utf-8');
+  const data = await fs.promises.readFile(file, 'utf-8');
   const dataJSON = await JSON.parse(data);
 
   return dataJSON;
 };
 
 const writeFile = async (file, data) => {
-  await fs.writeFile(file, JSON.stringify(data));
+  await fs.promises.writeFile(file, JSON.stringify(data));
 };
 
 module.exports = { getFile, writeFile };

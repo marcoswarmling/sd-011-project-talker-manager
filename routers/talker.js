af0e8711bdad5e5d804d591a3b46bc920e64a80f
Gustavo Lemes <gustavolemes997@gmail.com>
2021-10-12 19:24:16 -0300

    Corrige lint

--- routers/talker.js
+++ routers/talker.js
@@ -18,10 +18,10 @@ router.get('/:id', async (req, res) => {
   res.status(200).json(personById);
 });
 
-router.post('/', async (_req, res) => {
-  const result = await getFileData();
+// router.post('/', async (_req, res) => {
+//   const result = await getFileData();
 
-  res.status(200).json(result);
-});
+//   res.status(200).json(result);
+// });
 
 module.exports = router;

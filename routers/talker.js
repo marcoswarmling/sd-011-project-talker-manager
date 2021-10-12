ba351a3ba0668c7b5f893b2594a602bbd19ab331
Gustavo Lemes <gustavolemes997@gmail.com>
2021-10-12 17:50:10 -0300

    Requisito 2

--- routers/talker.js
+++ routers/talker.js
@@ -4,7 +4,17 @@ const { getFileData } = require('../services/CRUD');
 
 router.get('/', async (_req, res) => {
   const result = await getFileData();
+
   res.status(200).json(result);
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const result = await getFileData();
+  const personById = result.find((person) => person.id === parseInt(id, 10));
+  
+  if (personById) res.status(200).json(personById);
+  res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 });
 
 module.exports = router;

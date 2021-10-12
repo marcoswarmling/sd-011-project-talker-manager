0e58aec8815de79c8554366d4a566f2fac5791d8
DiegoCS777 <diegomethal@gmail.com>
2021-10-12 12:41:31 -0300

    requisito 1

---
+++ helpers/readFile.js
@@ -0,0 +1,14 @@
+const fs = require('fs').promises;
+
+const readContentTalker = async (path) => {
+    try {
+      const contentTalker = await fs.readFile(path, 'utf-8');
+      return JSON.parse(contentTalker);
+    } catch (error) {
+      return null;
+    }
+  };
+
+module.exports = {
+    readContentTalker,
+};
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { readContentTalker } = require('./helpers/readFile');
+const PATH = require('./talker.json');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +12,11 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res) => {
+  const dataTalker = await readContentTalker(PATH) || [];
+  res.status(200).json(dataTalker);
 });
 
 app.listen(PORT, () => {

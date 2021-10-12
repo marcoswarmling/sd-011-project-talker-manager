daa8f849cb0527a7627eda3543e958c54f20b1cb
Gustavo Lemes <gustavolemes997@gmail.com>
2021-10-12 19:04:40 -0300

    Requisito 3

--- routers/login.js
+++ routers/login.js
@@ -1,11 +1,22 @@
 const router = require('express').Router();
 
-// const { getFileData } = require('../services/CRUD');
+const { checkEmail, checkPassword } = require('../middlewares/loginMiddleware');
 
-// router.get('/', async (_req, res) => {
-//   const result = await getFileData();
+// Adaptado de: https://qastack.com.br/programming/1349404/generate-random-string-characters-in-javascript
+function tokenGenerator() {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < 16; i += 1) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 
-//   res.status(200).json(result);
-// });
+router.post('/', checkEmail, checkPassword, async (_req, res) => {
+  const token = tokenGenerator();
+
+  res.status(200).json({ token });
+});
 
 module.exports = router;

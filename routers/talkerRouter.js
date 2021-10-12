f3fcd1eb5ef62897d2724040d1b761094e8e60e7
Laura Gusmao <laurargus@gmail.com>
2021-10-12 17:49:23 -0300

    Requisito 2

--- routers/talkerRouter.js
+++ routers/talkerRouter.js
@@ -11,4 +11,15 @@ router.get('/', async (req, res) => {
   res.status(200).json(response);
 });
 
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const response = await getFile('./talker.json');
+
+  const talker = response.find((t) => t.id === Number(id));
+
+  if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+  res.status(200).json(talker);
+});
+
 module.exports = router;

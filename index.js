ddb8563b3bb699bb5481d0d5f8b6c2e0e70b3697
Juliana Happatsch <jhappatsch@gmail.com>
2021-10-12 20:26:43 -0300

    req 1 dnv

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const registeredSpeakers = require('./talker.json');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,7 +8,14 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-app.get('/talker', (_req, res) => res.status(200).json(registeredSpeakers));
+app.get('/talker', async (_req, res) => {
+  try {
+    const file = await fs.readFile('./talker.json');
+    res.status(200).send(file.toString('utf-8'));
+  } catch (e) {
+    res.status(404).send({ message: e.message });
+  }
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {

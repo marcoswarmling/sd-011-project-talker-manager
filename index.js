4c944d35a4e18c193cb2cc413d6db2f76c2741a2
Juliana Happatsch <jhappatsch@gmail.com>
2021-10-12 20:18:34 -0300

    req 1

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const registeredSpeakers = require('./talker.json');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+app.get('/talker', (_req, res) => res.status(200).json(registeredSpeakers));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {

78fa4aa722e080b6f69afb231a4eafb93c56d5bc
Nick-713 <pereiranicolas713@gmail.com>
2021-10-12 18:11:12 -0300

    separando funcitions de vailidaçao

---
+++ middlewares/isvalidEmail.js
@@ -0,0 +1,14 @@
+const isValidEmail = (req, res, next) => {
+  const { email } = req.body;
+
+  if (!email || email === '') { 
+    return res.status(200).json({ message: 'O campo "email" é obrigatório' }); 
+  }
+
+  if (!email.includes('@') || !email.includes('.com')) {
+    return res.status(200).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  next();
+};
+
+module.exports = isValidEmail;

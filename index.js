50f48dbafdd89e776d2b708e2f2dccdd325197fa
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-10-13 17:10:10 -0300

    feat: starting...

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const talker = 'talker.json';
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+app.get('/talker', (_request, response) => {
+  const data = JSON.parse(fs.readFileSync(talker, 'utf8'));
+  if (data.length === 0) return response.status(HTTP_OK_STATUS).json([]);
+  response.status(200).send(data);
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {

168a49e3a8febe18e15dcded3739d9dfe020409a
nsilva90 <nikolas.silva1990@gmail.com>
2021-10-13 08:01:01 -0300

    Requisito 1 em desenvolvimento

--- index.js
+++ index.js
@@ -7,9 +7,52 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const talker = [
+  {
+    name: 'Henrique Albuquerque',
+    age: 62,
+    id: 1,
+    talk: {
+      watchedAt: '23/10/2020',
+      rate: 5,
+    },
+  },
+  {
+    name: 'Heloísa Albuquerque',
+    age: 67,
+    id: 2,
+    talk: {
+      watchedAt: '23/10/2020',
+      rate: 5,
+    },
+  },
+  {
+    name: 'Ricardo Xavier Filho',
+    age: 33,
+    id: 3,
+    talk: {
+      watchedAt: '23/10/2020',
+      rate: 5,
+    },
+  },
+  {
+    name: 'Marcos Costa',
+    age: 24,
+    id: 4,
+    talk: {
+      watchedAt: '23/10/2020',
+      rate: 5,
+    },
+  },
+];
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
+  response.status(HTTP_OK_STATUS).send('OLÁ');
+});
+
+app.get('/talker', (req, res) => {
+  res.status(HTTP_OK_STATUS).json(talker);
 });
 
 app.listen(PORT, () => {

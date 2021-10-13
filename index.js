de54b561d106abc6ca4d78f7e473564563e6e6bf
Reginara Gomes <refagomes@gmail.com>
2021-10-13 00:41:02 -0300

    Refatora o talkerRouter para requisito 4

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs').promises;
+const loginRouter = require('./routers/loginRouter');
+const talkerRouter = require('./routers/talkerRouter');
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,28 +9,9 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-const loginRouter = require('./routers/loginRouter');
-const talkerRouter = require('./routers/talkerRouter');
-
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
-});
-
-app.get('/talker', async (req, res) => {
-  const talker = await fs.readFile('./talker.json', 'utf-8');
-  if (!talker) return res.status(HTTP_OK_STATUS).json([]);
-  return res.status(HTTP_OK_STATUS).json(JSON.parse(talker));
-});
-
-app.get('/talker/:id', async (req, res) => {
-  const talker = await fs.readFile('./talker.json', 'utf-8');
-  const talkerParse = JSON.parse(talker);
-  const { id } = req.params;
-  const talkerId = talkerParse.find((t) => t.id === Number(id));
-
-  if (!talkerId) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
-  return res.status(200).json(talkerId);
 });
 
 app.use('/', loginRouter);
--- routers/talkerRouter.js
+++ routers/talkerRouter.js
@@ -2,25 +2,46 @@ const express = require('express');
 
 const router = express.Router();
 const fs = require('fs').promises;
+const { readFile } = require('../helper/readFile');
 
 const {
   validateName,
   validateAge,
   validateToken,
   validateTalk,
+  validateWatchedAt,
+  validateRate,
 } = require('../middlewares/newTalker');
 
-router.post('/talker', validateName, validateAge, validateToken, validateTalk, async (req, res) => {
+router.get('/talker', async (_req, res) => {
+  const talker = await readFile();
+  return res.status(200).json(JSON.parse(talker));
+});
+
+router.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talker = readFile();
+  const talkerId = talker.find((t) => t.id === Number(id));
+
+  if (!talkerId) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  return res.status(200).json(talkerId);
+});
+
+router.post('/talker',
+  validateName,
+  validateAge,
+  validateToken,
+  validateTalk,
+  validateWatchedAt,
+  validateRate,
+  async (req, res) => {
   const { name, age, talk } = req.body;
-
-  const data = await fs.readFile('../talker.json', 'utf-8');
-  const talker = JSON.parse(data);
-
+  const talker = await readFile();
   const id = talker.length + 1;
 
   talker.push({ id, name, age, talk });
-  await fs.newfile('../talker.json', JSON.stringify(talker));
-  return res.status(201).json({ name, age, talk, id });
+  await fs.newFile('../talker.json', JSON.stringify(talker));
+  return res.status(201).status({ name, age, talk, id });
 });
 
 module.exports = router;

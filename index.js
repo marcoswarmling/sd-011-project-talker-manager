798e2539fd47aa8858a94c8a474c8f65c8aac776
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-10-13 17:28:13 -0300

    feat: req02

--- index.js
+++ index.js
@@ -13,7 +13,17 @@ const PORT = '3000';
 app.get('/talker', (_request, response) => {
   const data = JSON.parse(fs.readFileSync(talker, 'utf8'));
   if (data.length === 0) return response.status(HTTP_OK_STATUS).json([]);
-  response.status(200).send(data);
+  response.status(HTTP_OK_STATUS).send(data);
+});
+
+app.get('/talker/:id', (request, response) => {
+  const { id } = request.params;
+  const data = JSON.parse(fs.readFileSync(talker));
+  const talkerById = data.find((talkId) => talkId.id === Number(id));
+  if (!talkerById) {
+    return response.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return response.status(HTTP_OK_STATUS).json(talkerById);
 });
 
 // não remova esse endpoint, e para o avaliador funcionar

e6d521395a2fa2ecc6a23fa1cec2c09e7860bf34
Thiago Gomes <thiagog.macedom@gmail.com>
2021-10-13 02:27:38 -0300

    add - task 2 [now i will stop using nodemon]

--- routers/talkerRouter.js
+++ routers/talkerRouter.js
@@ -7,4 +7,18 @@ router.get('/', async (req, res) => {
   res.status(200).json(talkers);
 });
 
+router.get('/:id', async (req, res) => {
+  const talkers = await getTalkersData();
+  const { id } = req.params;
+
+  const filteredTalker = talkers.find((talker) => talker.id === parseInt(id, 0));
+
+  if (!filteredTalker) {
+    return res.status(404).json({
+      message: 'Pessoa palestrante não encontrada', 
+    });
+}
+  res.status(200).json(filteredTalker);
+});
+
 module.exports = router;

f355b8329fe00265bfc8c649c8f95d95a4f6cda2
eraldoalmeida <eraldo.salmeida@gmail.com>
2021-10-13 11:07:03 -0300

    Desafio 4

--- routers/talkerRouter.js
+++ routers/talkerRouter.js
@@ -4,6 +4,14 @@ const fs = require('fs').promises;
 const router = express.Router();
 
 const TALKER_FILE = 'talker.json';
+const authToken = require('../middlewares/authtoken');
+const { 
+  nameAuth,
+  ageAuth,
+  watchedAtAuth,
+  rateAuth,
+  talkAuth, 
+} = require('../middlewares/authtalker');
 
 function readTalkerFile() {
   return fs.readFile(TALKER_FILE, 'utf-8').then((data) => JSON.parse(data)); 
@@ -27,5 +35,31 @@ router.get('/:id', (req, res) => {
       res.status(200).json(talkerById);
     });
 });
+
+router.post(
+  '/', 
+  authToken,
+  nameAuth,
+  ageAuth,
+  talkAuth,
+  watchedAtAuth,
+  rateAuth,
+  (req, res) => {
+    readTalkerFile()
+    .then((data) => {
+      const talkers = data;
+      const newTalker = {
+        id: talkers.length + 1,
+        ...req.body,
+      };
+
+      talkers.push(newTalker);
+
+      fs.writeFile('./talker.json', JSON.stringify(talkers));
+
+      res.status(201).json({ ...newTalker });
+    });
+  },
+);
 
 module.exports = router; 

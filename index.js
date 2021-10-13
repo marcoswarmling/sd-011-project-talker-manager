fe30279bb6e53b04318f4812f094d5d9381d7163
nsilva90 <nikolas.silva1990@gmail.com>
2021-10-13 19:03:09 -0300

    Requisito 7 - Posição do endpoint GET /talker/search?q=searchTerm corrigida para não conflitar com o endpoint GET /talker/:id do requisito 2

--- index.js
+++ index.js
@@ -32,6 +32,22 @@ app.get('/talker', async (req, res) => {
   return res.status(HTTP_OK_STATUS).json(JSON.parse(returnedTalkers));
 });
 
+// Requisito 7 - Crie o endpoint GET /talker/search?q=searchTerm
+app.get('/talker/search',
+  tokenValidation,
+  async (req, res) => {
+    const returnedTalkers = await fs.readFile(talkerJSON, 'utf-8');
+    const talkersDB = JSON.parse(returnedTalkers);
+    const searchTerm = req.query.q;
+
+    if (!searchTerm || searchTerm === '') {
+      return res.status(HTTP_OK_STATUS).json(talkersDB);
+    }
+    const filteredTalkers = talkersDB.filter((talker) => talker.name.includes(searchTerm));
+
+    return res.status(HTTP_OK_STATUS).json(filteredTalkers);
+  });
+
 // Requisito 2 - Crie o endpoint GET /talker/:id
 app.get('/talker/:id', async (req, res) => {
   const returnedTalkers = await fs.readFile(talkerJSON, 'utf-8');
@@ -102,22 +118,6 @@ app.delete('/talker/:id',
     fs.writeFile(talkerJSON, JSON.stringify(filteredIds));
     return res.status(HTTP_OK_STATUS).json({ message: 'Pessoa palestrante deletada com sucesso' });
   });
-
-  // Requisito 7 - Crie o endpoint GET /talker/search?q=searchTerm
-app.get('/talker/search',
-tokenValidation,
-async (req, res) => {
-  const returnedTalkers = await fs.readFile(talkerJSON, 'utf-8');
-  const talkersDB = JSON.parse(returnedTalkers);
-  const searchTerm = req.query.q;
-
-  if (!searchTerm || searchTerm === '') {
-    return res.status(HTTP_OK_STATUS).json(talkersDB);
-  }
-  const filteredTalkers = talkersDB.filter((talker) => talker.name.includes(searchTerm));
-
-  return res.status(HTTP_OK_STATUS).json(filteredTalkers);
-});
 
 app.listen(PORT, () => {
   console.log('Online');

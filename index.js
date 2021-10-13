f2a09198cac45a358b63b18f0c17c887a270f521
nsilva90 <nikolas.silva1990@gmail.com>
2021-10-13 08:20:56 -0300

    Requisito 1 em desenvolvimento

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const talkersInfo = require('./talker.json');
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,52 +8,14 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-const talker = [
-  {
-    name: 'Henrique Albuquerque',
-    age: 62,
-    id: 1,
-    talk: {
-      watchedAt: '23/10/2020',
-      rate: 5,
-    },
-  },
-  {
-    name: 'Heloísa Albuquerque',
-    age: 67,
-    id: 2,
-    talk: {
-      watchedAt: '23/10/2020',
-      rate: 5,
-    },
-  },
-  {
-    name: 'Ricardo Xavier Filho',
-    age: 33,
-    id: 3,
-    talk: {
-      watchedAt: '23/10/2020',
-      rate: 5,
-    },
-  },
-  {
-    name: 'Marcos Costa',
-    age: 24,
-    id: 4,
-    talk: {
-      watchedAt: '23/10/2020',
-      rate: 5,
-    },
-  },
-];
-
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send('OLÁ');
+  response.status(HTTP_OK_STATUS).send();
 });
 
 app.get('/talker', (req, res) => {
-  res.status(HTTP_OK_STATUS).json(talker);
+  if (!talkersInfo) return res.status(HTTP_OK_STATUS).json([]);
+  res.status(HTTP_OK_STATUS).json(talkersInfo);
 });
 
 app.listen(PORT, () => {

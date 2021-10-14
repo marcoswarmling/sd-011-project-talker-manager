7fb757d87f3cfb2ad4344d288b4df23635787f02
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-10-14 00:35:20 -0300

    feat: req04

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs');
+const fs = require('fs').promises;
+const generateToken = require('./authorizationToken.js');
 
 const talker = 'talker.json';
 
@@ -12,9 +13,9 @@ const PORT = '3000';
 
 const validateEmail = (request, response, next) => {
   const { email } = request.body;
-  const regex = /\S+@\S+\.\S+/;
+  const emailRegex = /\S+@\S+\.\S+/;
   // Referência: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
-  const validationEmail = regex.test(email);
+  const validationEmail = emailRegex.test(email);
 
   if (!email || email === '') {
     return response.status(400).json({ message: 'O campo "email" é obrigatório' });
@@ -23,6 +24,12 @@ const validateEmail = (request, response, next) => {
     return response.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
   next();
+};
+
+const talkersJSON = async () => {
+  const list = await fs.readFile(talker);
+  const responseList = await JSON.parse(list);
+  return responseList;
 };
 
 const validatePassword = (request, response, next) => {
@@ -36,6 +43,73 @@ const validatePassword = (request, response, next) => {
   }
   next();
 };
+
+const validateToken = (request, response, next) => {
+  const { authorization } = request.headers;
+
+  if (!authorization) {
+    return response.status(401).json({ message: 'Token não encontrado' });
+  }
+  if (authorization.length !== 16) {
+    return response.status(401).json({ message: 'Token inválido' });
+  }
+  next();
+};
+
+const validateName = (request, response, next) => {
+  const { name } = request.body;
+  if (!name || name === '') {
+    return response.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
+  if (name.length < 3) {
+    return response.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  next();
+};
+const validateAge = (request, response, next) => {
+  const { age } = request.body;
+  if (!age || age === '') {
+    return response.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (Number(age) < 18) {
+    return response.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  next();
+};
+
+const validateTalk = (request, response, next) => {
+  const { talk } = request.body;
+  const { watchedAt, rate } = talk;
+  if (!talk || talk === '' || !watchedAt || !rate) {
+    return response.status(400).json({ message: 
+      'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+    });
+  } 
+  next();
+};
+
+const validateWatchedAt = (request, response, next) => {
+  const { talk: { watchedAt } } = request.body;
+  const dateRegex = /^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/;
+  const validationDate = dateRegex.test(watchedAt);
+  if (!validationDate) {
+    return response.status(400).json({ 
+      message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa',
+    });
+  }
+  // Referência: https://stackoverflow.com/questions/15491894/regex-to-validate-date-formats-dd-mm-yyyy-dd-mm-yyyy-dd-mm-yyyy-dd-mmm-yyyy
+  // https://www.regular-expressions.info/javascriptexample.html
+  next();
+};
+
+const validateRate = (request, response, next) => {
+  const { talk: { rate } } = request.body;
+  if (rate < 1 || rate > 5) {
+    return response.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  next();
+};
+
 app.get('/talker', (_request, response) => {
   const data = JSON.parse(fs.readFileSync(talker, 'utf8'));
   if (data.length === 0) return response.status(HTTP_OK_STATUS).json([]);
@@ -58,7 +132,23 @@ app.get('/', (_request, response) => {
 });
 
 app.post('/login', validateEmail, validatePassword, (request, response) => {
-  response.status(HTTP_OK_STATUS).json({ token: '7mqaVRXJSp886CGr' });
+  response.status(HTTP_OK_STATUS).json({ token: generateToken() });
+});
+
+app.post('/talker', 
+  validateToken,
+  validateName,
+  validateAge,
+  validateTalk,
+  validateWatchedAt,
+  validateRate,
+  async (request, response) => {
+    const talkers = await talkersJSON();
+    const postTalker = { id: talkers.length + 1, ...request.body };
+    talkers.push(postTalker);
+    await fs.writeFile('./talker.json', JSON.stringify(talkers));
+    return response.status(201).json(postTalker);
+  // Referência: Ajuda com assincronicidade do arquivo com Anderso Silva - Andy - Turma 10-A
 });
 
 app.listen(PORT, () => {

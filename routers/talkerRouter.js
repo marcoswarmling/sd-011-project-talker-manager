6a547d3297a6a59fb927e76371aa3f2985360249
Michel Pereira <michelbecauro@gmail.com>
2021-10-14 02:59:37 -0300

    troca ordem das rotas, pois o req. 5 quebrou

--- routers/talkerRouter.js
+++ routers/talkerRouter.js
@@ -43,6 +43,25 @@ ageValidator, talkValidator, rescue(async (req, res) => {
     // Outra maneira de fazer é o modo try/catch). Nesse caso não usaria o "rescue".
 }));
 
+router.delete('/:id', tokenValidator, rescue(async (req, res) => {
+    const { id } = req.params;
+    const foundMessage = 'Pessoa palestrante não encontrada';
+    readFile(FILE)
+    .then((data) => JSON.parse(data))
+    .then((talkers) => {
+        const updatedTalkers = talkers;
+        const foundIndex = talkers.findIndex((item) => item.id.toString() === id);
+        if (!foundIndex) return res.status(404).json({ message: foundMessage });
+        updatedTalkers.splice(foundIndex, 1);
+
+      writeFileFunc(FILE, updatedTalkers);
+      return res.status(200).json({ message: 'Pessoa palestrante deletada com sucesso' });
+    })
+    .catch((error) => error);
+
+    // Outra maneira de fazer é o modo try/catch). Nesse caso não usaria o "rescue".
+}));
+
 router.put('/:id', tokenValidator, nameValidator,
 ageValidator, talkValidator, rescue(async (req, res) => {
     const { id } = req.params;
@@ -57,25 +76,6 @@ ageValidator, talkValidator, rescue(async (req, res) => {
 
       writeFileFunc(FILE, updatedTalkers);
       return res.status(200).json(updatedTalkers[foundIndex]);
-    })
-    .catch((error) => error);
-
-    // Outra maneira de fazer é o modo try/catch). Nesse caso não usaria o "rescue".
-}));
-
-router.delete('/:id', tokenValidator, rescue(async (req, res) => {
-    const { id } = req.params;
-    const foundMessage = 'Pessoa palestrante não encontrada';
-    readFile(FILE)
-    .then((data) => JSON.parse(data))
-    .then((talkers) => {
-        const updatedTalkers = talkers;
-        const foundIndex = talkers.findIndex((item) => item.id.toString() === id);
-        if (!foundIndex) return res.status(404).json({ message: foundMessage });
-        updatedTalkers.splice(foundIndex, 1);
-
-      writeFileFunc(FILE, updatedTalkers);
-      return res.status(200).json({ message: 'Pessoa palestrante deletada com sucesso' });
     })
     .catch((error) => error);
 

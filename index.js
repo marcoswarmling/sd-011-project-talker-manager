6a40aacc584bbd5facc6f24b803e66282fbe2a93
roberto <robertonnv@gmail.com>
2021-10-15 14:41:46 -0300

    Desafio 1

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const fs = require('fs');
+
+app.get('/talker', async (_req, res) => {
+  try {
+    const dados = await fs.readFile('./talker.json', 'utf-8');
+    res.status(HTTP_OK_STATUS).json(dados);
+  } catch(e) {
+    res.status(404).json({ message: e.message });
+  }
+});
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();

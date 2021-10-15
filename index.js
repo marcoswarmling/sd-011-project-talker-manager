c7ac45b97c3941b864860effc51d306b0eb69a8a
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-10-15 08:15:35 -0300

    Second requirement

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FIND_STATUS = 404;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -21,13 +22,29 @@ const paths = {
 };
 
 const { FileRead } = require('./services/FilesHandler');
+const { findId } = require('./services/SearchById.js');
 
 app.route('/talker').get(async (_request, response) => {
   const contentFromFile = await FileRead(paths.talker);
 
   if (contentFromFile) {
-    return response.status(200).json(contentFromFile);
+    return response.status(HTTP_OK_STATUS).json(contentFromFile);
   }
 
   response.status(200).json([]);
+});
+
+app.route('/talker/:id').get(async (request, response) => {
+  const { id } = request.params;
+
+  const talkersDatabase = await FileRead(paths.talker);
+  const findedTalker = findId(talkersDatabase, id);
+
+  if (findedTalker) {
+    return response.status(HTTP_OK_STATUS).json(findedTalker);
+  }
+
+  return response
+    .status(HTTP_NOT_FIND_STATUS)
+    .json({ message: 'Pessoa palestrante não encontrada' });
 });

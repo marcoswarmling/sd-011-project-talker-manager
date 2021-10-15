e894eab794b20922bb624060891c4560c9d72494
Michael Mascarenhas <michaelmascarenhastrybe@gmail.com>
2021-10-15 13:15:15 -0300

    avaliador rodando

--- middlewares/validations/validationTalk.js
+++ middlewares/validations/validationTalk.js
@@ -1,6 +1,6 @@
 export default function validationTalk(req, res, next) {
     const { talk } = req.body;
-    if (!talk || !talk.rate || !talk.watchedAt) {
+    if (!talk || !talk.rate || !talk.watchedAt === undefined) {
         return res.status(400)
         .json(
             { message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' },

dcba77cc9bca4ff20d45ae02370e9f3e25079ebb
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-10-15 14:50:11 -0300

    req: req05 - FIXED!

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ app.post('/talker',
   // Referência: Ajuda com assincronicidade do arquivo com Anderson Silva - Andy - Turma 10-A
 });
 
-app.put('talker/:id', 
+app.put('/talker/:id', 
   validateToken,
   validateName,
   validateAge,

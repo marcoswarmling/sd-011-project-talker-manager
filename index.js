50af9a9b81d21b567709697a8599290df2be849e
roberto <robertonnv@gmail.com>
2021-10-16 00:50:24 -0300

    desafio 1

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const app = express();
-app.use(bodyParser.json());
-
-const HTTP_OK_STATUS = 200;
-const PORT = '3000';
-
-const fs = require('fs');
-
-app.get('/talker', async (_req, res) => {
-  try {
-    const dados = JSON.parse(await fs.readFile('./talker.json', 'utf-8'));
-    res.status(HTTP_OK_STATUS).json(dados);
-  } catch (error) {
-    res.status(404).json({ message: error.message });
-  }
-});
+const getTalker = require('./mid/getTalker');
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
-
+app.get('/talker', getTalker);
 app.listen(PORT, () => {
   console.log('Online');
 });

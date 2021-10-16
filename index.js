20d6af46c87a5be242fd28496b9d66d26e79db48
Gabriel Castro <kastro.gabriel17@gmail.com>
2021-10-16 14:25:55 -0400

    req: 1 complet

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const talker = require('./routes/talker');
+
 const app = express();
 app.use(bodyParser.json());
 
-const HTTP_OK_STATUS = 200;
-const PORT = '3000';
+app.use('/talker', talker);
 
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
-});
+const PORT = '3000';
 
 app.listen(PORT, () => {
   console.log('Online');

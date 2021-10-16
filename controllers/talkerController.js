24f7167a20250d95ddd6151c3310f09c349d818b
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-10-16 10:26:26 -0300

    Requisito 2 concluido

--- controllers/talkerController.js
+++ controllers/talkerController.js
@@ -3,17 +3,32 @@ const TalkerModel = require('../models/talkerModel');
 
 const HTTP_OK_STATUS = 200;
 
-router.get('/', async (req, res) => {
+router.get('/', async (_req, res) => {
   try {
     const response = await TalkerModel.getAll();
 
     if (!response) {
-      res.status(HTTP_OK_STATUS).json([]);
+      res.status(404).json([]);
     }
 
     res.status(HTTP_OK_STATUS).json(JSON.parse(response));
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const response = await TalkerModel.getById(id);
+
+    if (!response) {
+      res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+
+    res.status(HTTP_OK_STATUS).json(response);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
--- models/talkerModel.js
+++ models/talkerModel.js
@@ -7,4 +7,13 @@ const getAll = async () => {
   return result;
 };
 
-module.exports = { getAll };
+const getById = async (id) => {
+  const result = JSON.parse(await fs.readFile(file, 'utf8'));
+  const talker = result.find((item) => item.id === parseInt(id, 10));
+
+  if (!talker) return null;
+  
+  return talker;
+};
+
+module.exports = { getAll, getById };

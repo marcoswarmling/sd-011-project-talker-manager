7faf4174073125ef6cdf3a141d87120b3451df4d
roberto <robertonnv@gmail.com>
2021-10-16 01:33:02 -0300

    desafio 1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const talkersRoutes = require('../routes/talkerRoutes');
+const talkersRoutes = require('./routes');
 
 const app = express();
 app.use(bodyParser.json());

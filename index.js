6bb52468d740c6e6d960ee82674edf0772e5eda0
Matheus Costa <mathws.costag@gmail.com>
2021-10-16 10:01:49 -0300

    Requisito 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const talkers = require('./talker.json');
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,4 +15,8 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+app.get('/talker', (req, res) => {
+   res.status(200).json(!talkers ? [] : talkers);
 });

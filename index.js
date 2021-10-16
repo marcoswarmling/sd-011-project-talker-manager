4f4ce278aa0a6728928a0ff0a7c73e924115143f
roberto <robertonnv@gmail.com>
2021-10-16 00:58:49 -0300

    desafio 1

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// REFACTORING...
 const getTalker = require('./mid/getTalker');
 
+
+const app = express();
+app.use(bodyParser.json());
+
+const HTTP_OK_STATUS = 200;
+const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+// REFACTORING...
+
 app.get('/talker', getTalker);
+
 app.listen(PORT, () => {
   console.log('Online');
 });

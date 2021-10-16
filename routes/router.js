65f1b6e8fc03b9f78b1f020c05a9861cd27581d9
Diego Figueirêdo <8kball@gmail.com>
2021-10-16 20:32:39 -0300

    Requirement 2

--- routes/router.js
+++ routes/router.js
@@ -10,4 +10,16 @@ router.get('/talker', (_req, res) => {
   res.status(200).json(file);
 });
 
+router.get('/talker/:id', (req, res) => {
+  const { id } = req.params;
+  const file = JSON.parse(fs.readFileSync('./talker.json', 'utf-8'));
+  const talker = file.find((person) => person.id === Number(id));
+
+  if (!talker) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  
+  res.status(200).json(talker);
+});
+
 module.exports = router;

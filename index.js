802ab5044441e18d6b832897272da5c06ff2a059
getuliogutemberg <getulio.dev@gmail.com>
2021-10-17 19:45:14 -0300

    Resolve requisito 2

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const INTERNAL_SERVER_ERROR = 500;
+const NOT_FOUND = 404;
+
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -17,18 +19,31 @@ app.get('/', (_request, response) => {
   });
 });
 
+app.get('/talker/:id', (_request, response) => {
+  const { id } = _request.params;
+  try {
+    const talkerData = fs.readFileSync('./talker.json', 'utf8');
+    const talkersJson = JSON.parse(talkerData);
+    const talker = talkersJson.find((e) => e.id == id);
+    if (talker) {
+      response.status(HTTP_OK_STATUS).json(talker);
+    }
+    response.status(NOT_FOUND).json({ messenge: 'Pessoa palestrante não encontrada' });
+  } catch (error) {
+    return response.status(INTERNAL_SERVER_ERROR).json({ error });
+  }
+});
+
 app.get('/talker', (_request, response) => {
   try {
     const talkerData = fs.readFileSync('./talker.json', 'utf8');
-    const talkerFilter = JSON.parse(talkerData);
-    return response.status(HTTP_OK_STATUS).json(talkerFilter);
+    const talkersJson = JSON.parse(talkerData);
+    return response.status(HTTP_OK_STATUS).json(talkersJson);
   } catch (error) {
     return response.status(INTERNAL_SERVER_ERROR).json({ error });
   }
 });
 
 app.listen(PORT, () => {
-  const talkerData = fs.readFileSync('./talker.json', 'utf8');
   console.log('Online');
-  console.log(talkerData);
 });

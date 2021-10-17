639455b16c121dd013bc3812d4a1c4da9f9e4aaf
Matheus Costa <mathws.costag@gmail.com>
2021-10-17 20:33:19 -0300

    Desafio 4

--- middlewares/talkerMiddlewares.js
+++ middlewares/talkerMiddlewares.js
@@ -12,7 +12,66 @@ const readTalkers = () => {
   return result;
 };
 
-const getAllTalkers = (_req, res) => res.status(200).json(readTalkers());
+const verifyToken = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+  if (authorization.length !== 16) {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+  next();
+};
+
+const verifyName = (req, res, next) => {
+  const { name } = req.body;
+  if (!name) {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  next();
+};
+
+const verifyAge = (req, res, next) => {
+  const { age } = req.body;
+  if (!age) {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (age < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  next();
+};
+
+const verifyTalkRate = (req, res, next) => {
+  const { rate } = req.body.talk;
+  const validRate = rate > 1 && rate < 6;
+  if (!validRate) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  next();
+};
+
+const verifyTalkDate = (req, res, next) => {
+  const { watchedAt } = req.body.talk;
+  const dateRegex = /(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d/;
+  if (!dateRegex.test(watchedAt)) {
+    return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  next();
+};
+
+const verifyTalk = (req, res, next) => {
+  const { talk } = req.body;
+  if (!talk || !talk.watchedAt || !talk.rate) {
+    return res.status(400).json({
+      message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+    });
+  }
+  next();
+};
 
 const getTalkerById = (req, res) => {
   const { id } = req.params;
@@ -24,7 +83,32 @@ const getTalkerById = (req, res) => {
   );
 };
 
+const getAllTalkers = (_req, res) => res.status(200).json(readTalkers());
+
+const addTalker = (req, res, next) => {
+  const talkers = readTalkers();
+  const id = Math.max(...talkers.map((talker) => talker.id), 0) + 1;
+  const newTalker = { id, ...req.body };
+
+  const newTalkers = [...talkers, newTalker];
+  try {
+    fs.writeFileSync('talker.json', JSON.stringify(newTalkers));
+  } catch (err) {
+    console.error(`Erro ao ler o arquivo: ${err.path}`);
+    console.log(err);
+  }
+  res.status(201).json(newTalker);
+  next();
+};
+
 module.exports = {
   getAllTalkers,
   getTalkerById,
+  addTalker,
+  verifyToken,
+  verifyName,
+  verifyAge,
+  verifyTalk,
+  verifyTalkDate,
+  verifyTalkRate,
 };
--- routes/talkerRoutes.js
+++ routes/talkerRoutes.js
@@ -2,9 +2,27 @@ const router = require('express').Router();
 const {
   getAllTalkers,
   getTalkerById,
+  addTalker,
+  verifyToken,
+  verifyName,
+  verifyAge,
+  verifyTalk,
+  verifyTalkRate,
+  verifyTalkDate,
 } = require('../middlewares/talkerMiddlewares');
 
 router.get('/', getAllTalkers);
 router.get('/:id', getTalkerById);
+
+router.post(
+  '/',
+  verifyToken,
+  verifyName,
+  verifyAge,
+  verifyTalk,
+  verifyTalkRate,
+  verifyTalkDate,
+  addTalker,
+);
 
 module.exports = router;

17dd9396dd691596f476c7806b09ea37b3bef834
Gabriel Castro <kastro.gabriel17@gmail.com>
2021-10-17 10:35:34 -0400

    req: 1 complet error: 1

--- controllers/talker.js
+++ controllers/talker.js
@@ -1,9 +1,8 @@
-const { StatusCodes } = require('http-status-codes');
 const readFile = require('../services/readFile');
 
 const getAllTalkers = async (req, res) => {
   const talkers = await readFile('talker.json');
-  res.status(StatusCodes.OK).json(talkers);
+  res.status(200).json(talkers);
 };
 
 module.exports = {

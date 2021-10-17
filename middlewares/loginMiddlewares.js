e38f5bcbad4877f77d95c97370c5c8856d014480
roberto <robertonnv@gmail.com>
2021-10-17 13:04:53 -0300

    desafio

--- middlewares/loginMiddlewares.js
+++ middlewares/loginMiddlewares.js
@@ -1,10 +1,10 @@
 const emaill = (req, res, next) => {
     const { email } = req.body;
-    if(!email) {
-        return res.status(400).json({message: '"O campo "email" é obrigatório"'})
+    if (!email) {
+        return res.status(400).json({ message: '"O campo "email" é obrigatório"' });
     }
-    if(!email.includes('@') || !email.includes('.com')) {
-        return res.status(400).json({message: 'O "email" deve ter o formato "email@email.com"',});
+    if (!email.includes('@') || !email.includes('.com')) {
+        return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
     }
     next();
 }
--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -5,5 +5,4 @@ const router = express.Router();
 
 router.post('/', login.emaill);
 
-
 module.exports = router;

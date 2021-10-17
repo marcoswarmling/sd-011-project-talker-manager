86717d99b468cd3ebbc9cfe3536a4506350f4048
getuliogutemberg <getulio.dev@gmail.com>
2021-10-17 19:15:25 -0300

    Resolve Requisito 1

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
+
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const INTERNAL_SERVER_ERROR = 500;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
+  response.status(HTTP_OK_STATUS).send({
+    messenger: 'helloWorld',
+  });
+});
+
+app.get('/talker', (_request, response) => {
+  try {
+    const talkerData = fs.readFileSync('./talker.json', 'utf8');
+    const talkerFilter = JSON.parse(talkerData);
+    return response.status(HTTP_OK_STATUS).json(talkerFilter);
+  } catch (error) {
+    return response.status(INTERNAL_SERVER_ERROR).json({ error });
+  }
 });
 
 app.listen(PORT, () => {
+  const talkerData = fs.readFileSync('./talker.json', 'utf8');
   console.log('Online');
+  console.log(talkerData);
 });

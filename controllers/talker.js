228ce1cbc05958587282770d5b93fa420456d57f
Gabriel Castro <kastro.gabriel17@gmail.com>
2021-10-17 11:11:54 -0400

    req: 1 complet error: 2

--- controllers/talker.js
+++ controllers/talker.js
@@ -2,7 +2,7 @@ const readFile = require('../services/readFile');
 
 const getAllTalkers = async (req, res) => {
   const talkers = await readFile('talker.json');
-  res.status(200).json(talkers);
+  return res.status(200).json(talkers);
 };
 
 module.exports = {

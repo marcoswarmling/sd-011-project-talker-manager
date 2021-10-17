c7d9a0bbcca0df07b30775b4b45245ae33ef84d3
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-10-17 00:15:16 -0300

    Requisito 2 - /talker/:id

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(bodyParser.json());
 const talkers = './talker.json';
 const HTTP_OK_STATUS = 200;
 const HTTP_ERROR_STATUS = 500;
+const HTTP_NOT_FOUND_STATUS = 404;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -21,6 +22,24 @@ app.get('/talker', (_request, response) => {
     const data = fs.readFileSync(talkers, 'utf8');
     const talker = JSON.parse(data);
     return response.status(HTTP_OK_STATUS).json(talker);
+  } catch (error) {
+    return response.status(HTTP_ERROR_STATUS).json(error);
+  }
+});
+
+app.get('/talker/:id', (request, response) => {
+  const { id } = request.params;
+  try {
+    const data = fs.readFileSync(talkers, 'utf8');
+    const talker = JSON.parse(data);
+    const getTalkerById = talker.find((r) => r.id === parseInt(id, 10));
+
+    if (!getTalkerById) {
+      return response
+        .status(HTTP_NOT_FOUND_STATUS)
+        .json({ message: 'Pessoa palestrante não encontrada' });
+    }
+    return response.status(HTTP_OK_STATUS).json(getTalkerById);
   } catch (error) {
     return response.status(HTTP_ERROR_STATUS).json(error);
   }

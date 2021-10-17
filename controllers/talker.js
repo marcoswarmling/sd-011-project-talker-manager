bb969ad6e89e4bf6de69df90170824f48f3f8f2e
Gabriel Castro <kastro.gabriel17@gmail.com>
2021-10-17 13:17:15 -0400

    req: 2 complet

--- controllers/talker.js
+++ controllers/talker.js
@@ -6,6 +6,20 @@ const getAllTalkers = async (req, res) => {
   return res.status(StatusCodes.OK).json(talkers);
 };
 
+const getIdTalkers = async (req, res) => {
+  const { id } = req.params;
+  const talkersList = await readFile('talker.json');
+
+  const talkerId = talkersList.find((talker) => talker.id === parseInt(id, 10));
+
+  if (!talkerId) {
+    return res.status(StatusCodes.NOT_FOUND).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  return res.status(StatusCodes.OK).json(talkerId);
+};
+
 module.exports = {
   getAllTalkers,
+  getIdTalkers,
 };

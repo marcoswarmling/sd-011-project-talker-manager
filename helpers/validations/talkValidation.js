a2cdb1f42e122e68ea7a26ff365f53f3117e25d1
DiegoCS777 <diegomethal@gmail.com>
2021-10-18 20:10:35 -0300

    corrindo validação do talk

--- helpers/validations/talkValidation.js
+++ helpers/validations/talkValidation.js
@@ -1,7 +1,7 @@
 const talkValidate = (req, res, next) => {
     const { talk } = req.body;
   
-    if (!talk || !talk.rate || !talk.watchedAt) {
+    if (!talk || !((talk.rate || talk.rate === 0) && talk.watchedAt)) {
       return res.status(400).json({
         message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
       });

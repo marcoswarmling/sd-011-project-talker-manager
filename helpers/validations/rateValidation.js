313524340c2b5c2765ede340f94d375f4c249240
DiegoCS777 <diegomethal@gmail.com>
2021-10-18 19:55:38 -0300

    corrindo validação do rate

--- helpers/validations/rateValidation.js
+++ helpers/validations/rateValidation.js
@@ -2,7 +2,7 @@ const rateValidate = (req, res, next) => {
     const { talk } = req.body;
     const { rate } = talk;
   
-    if (rate < 1 || rate > 5) {
+    if (!Number(rate) >= 1 && !Number(rate) <= 5 && !Number(rate) % 1 === 0) {
       return res.status(400)
         .json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
     }

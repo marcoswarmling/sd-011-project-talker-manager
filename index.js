9fa9eb2433a34a8db809266491b16c2831874509
deltadigo <deltadigo@gmail.com>
2021-10-18 11:47:26 -0300

    Requisit 2

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const BAD_REQUEST = 400;
+const NOT_FOUND = 404;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -21,6 +23,16 @@ app.get('/talker', (_req, res) => {
   }).catch((error) => {
     console.log(error);
   });
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  if (!id) res.status(BAD_REQUEST).json({ message: 'ID não informado' });
+  const fileContent = await JSON.parse(fs.readFile('talker.json', 'utf8'));
+  console.log(fileContent);
+  const talker = fileContent.find((t) => t.id === id);
+  if (!talker) res.status(NOT_FOUND).json({ message: 'Pessoa palestrante não encontrada' });
+  res.status(HTTP_OK_STATUS).json(talker);
 });
 
 app.listen(PORT, () => {

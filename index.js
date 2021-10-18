f76a0a029ad7aff127cec844bddb04f5de78528e
deltadigo <deltadigo@gmail.com>
2021-10-18 10:43:05 -0300

    Requisito 1

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+// const { response } = require('express');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +12,15 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (_req, res) => {
+  fs.readFile('talker.json', 'utf8').then((fileContent) => {
+    if (!fileContent) res.status(HTTP_OK_STATUS).json([]);
+    res.status(HTTP_OK_STATUS).json(JSON.parse(fileContent));
+  }).catch((error) => {
+    console.log(error);
+  });
 });
 
 app.listen(PORT, () => {

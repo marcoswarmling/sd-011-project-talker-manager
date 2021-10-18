c5d738167f2961fc04a141ba2443c1610489786b
roberto <robertonnv@gmail.com>
2021-10-18 01:32:39 -0300

    desafio

---
+++ routes/getRouter.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const fs = require('fs').promises;
+
+const { checarToken } = require('../middlewares/delete');
+
+const rota = express.Router();
+const lista = './talker.json';
+
+rota.get('/search', checarToken,
+async (req, res) => {
+  const { q } = req.query;
+  const conteudo = JSON.parse(await fs.readFile(lista, 'utf8'));
+  const index = conteudo.filter((t) => t.name.includes(q));
+  return res.status(200).json(index);
+});
+
+module.exports = rota;

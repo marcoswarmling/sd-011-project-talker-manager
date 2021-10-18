21f971305cd5d21e4049dbd98a66a6f878d0003f
Pedro Piquini <piquini.pedro@gmail.com>
2021-10-18 18:26:06 -0300

    Requisito 2 completo

--- Routes/talkerRoute.js
+++ Routes/talkerRoute.js
@@ -2,5 +2,6 @@ const routes = require('express').Router();
 const talkerController = require('../controllers/talkerController');
 
 routes.get('/', talkerController.getAllTalkers);
+routes.get('/:id', talkerController.getTalkersID);
 
 module.exports = routes;
--- controllers/talkerController.js
+++ controllers/talkerController.js
@@ -9,6 +9,17 @@ const getAllTalkers = (_req, res) => {
   return res.status(HTTP_OK_STATUS).json(JSON.parse(data));
 };
 
+const getTalkersID = (req, res) => {
+  const { id } = req.params;
+  const data = fs.readFileSync(talkersData, 'utf-8');
+  const talker = JSON.parse(data).find((talkerId) => talkerId.id === parseInt(id, 10));
+
+  if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+  return res.status(HTTP_OK_STATUS).json(talker);
+};
+
 module.exports = {
   getAllTalkers,
+  getTalkersID,
 };

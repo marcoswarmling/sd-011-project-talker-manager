68191394c417b16add04f38e4079599ba4a05b59
DiegoCS777 <diegomethal@gmail.com>
2021-10-18 19:40:53 -0300

    corrindo validação do rate

--- helpers/validations/rateValidation.js
+++ helpers/validations/rateValidation.js
@@ -2,13 +2,9 @@ const rateValidate = (req, res, next) => {
     const { talk } = req.body;
     const { rate } = talk;
   
-    if (!rate && rate !== 0) {
-      return res.status(400).json({ message:
-        'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
-    }
-  
-    if (Number(rate) < 1 || Number(rate) > 5) {
-      return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+    if (rate < 1 || rate > 5) {
+      return res.status(400)
+        .json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
     }
     
     next();

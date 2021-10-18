3100561d3d9e1a19c8000ce472b5870d2f252b3f
igordmf <igordanielmf@gmail.com>
2021-10-18 20:42:43 -0300

    Corrige requisito 1

--- routes/talkerRouter.js
+++ routes/talkerRouter.js
@@ -3,8 +3,10 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 
-router.get('/talker', async (_req, res) => {
+router.get('/', async (_req, res) => {
   const data = await fs.readFileSync('../talker.json', 'utf-8');
   if (!JSON.parse(data).length) return res.status(200).send([]);
   res.status(200).send(JSON.parse(data));
 });
+
+module.exports = router;

3e4f37a043ac10d7774d7f669983591a8e7f47d5
Matheus Antonio <matheusantonio232@gmail.com>
2021-10-18 01:42:58 -0300

    req 1 e 2 teste

---
+++ routers/talkerRouter.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const rescue = require('express-rescue');
+const fsUtils = require('../services/fs-utils');
+
+router.get('/', rescue(async (_req, res) => {
+  const talkers = await fsUtils.getTalker();
+  if (!talkers) {
+    return res.status(200).json(talkers);
+  } 
+  return res.status(200).json(talkers);
+}));
+
+router.get('/:id', rescue(async (req, res) => {
+  const { id } = req.params;
+
+  const talkers = await fsUtils.getTalker();
+  const talker = talkers.find((t) => t.id === id);
+
+  if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  return res.status(200).json(talker);
+}));
+
+module.exports = router;

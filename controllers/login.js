ebc422da771b1ab1817c3de138b550a64ce29a44
Gabriel Castro <kastro.gabriel17@gmail.com>
2021-10-18 00:26:20 -0400

    req: 3 validate password

--- controllers/login.js
+++ controllers/login.js
@@ -16,6 +16,22 @@ const emailValidate = (req, res, next) => {
   next();
 };
 
+const passwordValidate = (req, res, next) => {
+  const { password } = req.body;
+  if (!password) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  next();
+};
+
 module.exports = {
   emailValidate,
+  passwordValidate,
 };

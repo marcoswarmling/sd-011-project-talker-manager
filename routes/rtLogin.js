f904b316f51fc24c3f3fa584f04a901b60c38689
getuliogutemberg <getulio.dev@gmail.com>
2021-10-18 00:05:43 -0300

    altera rotas rtLogin

--- routes/rtLogin.js
+++ routes/rtLogin.js
@@ -1,5 +1,8 @@
 const express = require('express');
-const { emailChk, passwordChk, mkToken } = require('../middlewares/middlewaresLogin');
+const { 
+    emailChk,
+    passChk,
+    mkToken } = require('../middlewares/middlewaresLogin');
 
 const rtLogin = express.Router();
 // rest codes:
@@ -7,8 +10,12 @@ const rtLogin = express.Router();
 // const INTERNAL_SERVER_ERROR = 500;
 // const NOT_FOUND = 404;
 
-rtLogin.post('/', emailChk, passwordChk, (_request, response) => {
+rtLogin.post('/', emailChk, passChk, (_request, response) => {
     response.status(200).json({ token: mkToken(16) });    
 });
+
+// rtLogin.get('/', (_request, response) => {
+//     response.status(200).json({ token: mkToken(16) });    
+// });
 
 module.exports = rtLogin;

97ad1db32e60f68b803f12e230dbbf512dfb1441
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2021-10-17 22:25:25 -0300

    Consertando async

--- routes/talker.js
+++ routes/talker.js
@@ -3,17 +3,18 @@ const fs = require('fs');
 
 const talkers = './talker.json';
 
-router.get('/', async (_req, res) => {
+router.get('/', (_req, res) => {
     const data = fs.readFileSync(talkers, 'utf-8');
     const results = JSON.parse(data);
      return res.status(200).json(results);
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', (req, res) => {
     const { id } = req.params;
     const data = fs.readFileSync(talkers, 'utf-8');
     const results = JSON.parse(data);
     const Matchid = results.find((result) => result.id === Number(id));
+    if (!Matchid) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
     return res.status(200).json(Matchid);
 });
 

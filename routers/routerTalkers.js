c83deda742b9afa5a924b97e11445b205d57a378
juliabaptista <juliabaptistadeoliveira@gmail.com>
2021-10-18 17:40:48 -0300

    Feat: req-02

--- routers/routerTalkers.js
+++ routers/routerTalkers.js
@@ -1,4 +1,4 @@
-const router = require('express').Router();
+const router = require( express ).Router();
 
 const { getTalkers } = require('../helpers/readFile');
 
@@ -7,6 +7,16 @@ router.get('/', async (_request, response) => {
   if (!talkers) response.status(200).json([]);
 
   response.status(200).json(talkers);
+});
+
+router.get('/:id', async (request, response) => {
+  const { id } = request.params;
+  const response = await fs.readFile('./talker.json', 'utf-8');
+  const result = JSON.parse(response).find((talker) => Number(talker.id) === Number(id));
+  if (!result) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return response.status(200).json(result);
 });
 
 module.exports = router;

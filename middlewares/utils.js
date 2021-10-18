58fddec2a5847713335eb4cc684157fcaedfeac6
roberto <robertonnv@gmail.com>
2021-10-18 12:50:55 -0300

    desafio

---
+++ middlewares/utils.js
@@ -0,0 +1,10 @@
+const fs = require('fs').promises;
+
+const file = './talker.json';
+
+const getTalker = () => fs.readFile(file, 'utf-8')
+    .then((fileContent) => JSON.parse(fileContent));
+
+const setTalker = (talkers) => fs.writeFile(file, JSON.stringify(talkers));
+
+module.exports = { getTalker, setTalker };

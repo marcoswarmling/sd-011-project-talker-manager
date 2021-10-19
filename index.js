f1e05c6fb02310f3020c7973b174dfc5d3348250
Apoc35 <alexv_silveira@outlook.com>
2021-10-19 10:49:43 -0300

    feat: Tirei o crypto por enquanto.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
-const crypto = require('crypto');
 
 const app = express();
 app.use(bodyParser.json());

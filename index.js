bfa00eeed227ed5ec54d91af07fa10f194a4e8e6
Apoc35 <alexv_silveira@outlook.com>
2021-10-19 10:55:27 -0300

    feat: Aleluia! Requisito 2 refeito também.

--- index.js
+++ index.js
@@ -13,9 +13,23 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+// 1
+
 app.get('/talker', async (_req, res) => {
   const talkerList = await fs.readFile('./talker.json', 'utf-8');
     return res.status(HTTP_OK_STATUS).json(JSON.parse(talkerList));
+});
+
+// 2
+
+app.get('/talker/:id', async (req, res) => {
+  const talkerList = await fs.readFile('./talker.json', 'utf-8');
+  const { id } = req.params;
+  const talker = JSON.parse(talkerList).find((t) => t.id === +id);
+
+  if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+  res.status(HTTP_OK_STATUS).json(talker);
 });
 
 app.listen(PORT, () => {

14ad3684d63f3861f0704a24a23c60868a515275
Pedro Piquini <piquini.pedro@gmail.com>
2021-10-19 10:11:49 -0300

    Requisito 4 completo

---
+++ Helpers/validations.js
@@ -0,0 +1,79 @@
+const BAD_REQUEST = 400;
+const UNAUTHORIZED = 401;
+const minimumTokenLength = 16;
+const minNameLength = 3;
+const minAge = 18;
+
+const tokenValidation = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) return res.status(UNAUTHORIZED).json({ message: 'Token não encontrado' });
+  if (token.length !== minimumTokenLength) {
+    return res.status(UNAUTHORIZED).json({ message: 'Token inválido' });
+  }
+  next();
+};
+
+const nameValidation = (req, res, next) => {
+  const { name } = req.body;
+  if (!name) return res.status(BAD_REQUEST).json({ message: 'O campo "name" é obrigatório' });
+  if (name.length < minNameLength) {
+    return res.status(BAD_REQUEST).json({ message: 'O campo "name" ter pelo menos 3 caracteres' });
+  }
+  next();
+};
+
+const ageValidation = (req, res, next) => {
+  const { age } = req.body;
+  if (!age) return res.status(BAD_REQUEST).json({ message: 'O campo "age" é obrigatório' });
+  if (age < minAge) {
+    return res.status(BAD_REQUEST)
+    .json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  next();
+};
+
+const talkValidation = (req, res, next) => {
+  const { talk } = req.body;
+  if (!talk) {
+    return res.status(BAD_REQUEST)
+    .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
+  }
+  next();
+};
+
+const rateValidation = (req, res, next) => {
+  const { talk: { rate } } = req.body;
+  if (!rate) {
+    return res.status(BAD_REQUEST)
+    .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
+  }
+  if (rate < 1 || rate > 5) {
+    return res.status(BAD_REQUEST).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  next();
+};
+
+const dateValidation = (req, res, next) => {
+  const { talk: { watchedAt } } = req.body;
+  const date = /^[0-9]{2}[/][0-9]{2}[/][0-9]{4}$/.test(watchedAt); // // Regex utilizado encontrodo em: https://stackoverflow.com/questions/5465375/javascript-date-regex-dd-mm-yyyy
+  if (!date) {
+    return res.status(BAD_REQUEST)
+    .json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  if (!watchedAt) {
+    return res.status(BAD_REQUEST)
+    .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
+  }
+  next();
+};
+
+const validations = [
+  tokenValidation,
+  nameValidation,
+  ageValidation,
+  talkValidation,
+  rateValidation,
+  dateValidation,
+];
+
+module.exports = validations;
--- Routes/talkerRoute.js
+++ Routes/talkerRoute.js
@@ -1,7 +1,20 @@
+const fs = require('fs');
 const routes = require('express').Router();
 const talkerController = require('../controllers/talkerController');
+const validations = require('../Helpers/validations');
+
+const CREATED = 201;
 
 routes.get('/', talkerController.getAllTalkers);
 routes.get('/:id', talkerController.getTalkersID);
+routes.post('/', validations, (req, res) => {
+  const rawData = fs.readFileSync('./talker.json');
+  const talkers = JSON.parse(rawData);
+  const newTalker = req.body;
+  newTalker.id = talkers.length + 1;
+  talkers.push(newTalker);
+  fs.writeFileSync('./talker.json', JSON.stringify(talkers, null, 2));
+  res.status(CREATED).json(newTalker);
+});
 
 module.exports = routes;

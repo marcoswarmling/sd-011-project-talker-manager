d969cae7d569cb9b7496d741acedd1f5eac61e3a
Arlesson Moura <arlessonmss@gmail.com>
2021-10-19 03:22:43 -0300

    4°!!!!

--- index.js
+++ index.js
@@ -60,6 +60,85 @@ app.post('/login', validateLogin, (req, res) => {
   res.status(HTTP_OK_STATUS).json({ token });
 });
 
+const validateToken = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+  if (token.length !== 16) {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+  next();
+};
+
+const validateTalkerName = (req, res, next) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  next();
+};
+
+const validateTalkerAge = (req, res, next) => {
+  const { age } = req.body;
+
+  if (!age) {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (Number(age) < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  next();
+};
+
+const validateTalk = (req, res, next) => {
+  const { talk } = req.body;
+  if (!talk) {
+    return res.status(400)
+    .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
+  }
+  if (!talk.watchedAt || !talk.rate) {
+    return res.status(400)
+    .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
+  }
+  next();
+};
+
+const validateTalkKeys = (req, res, next) => {
+  const { talk } = req.body;
+  const { watchedAt, rate } = talk;
+  const dateRegex = /([0-2][0-9]|3[0-1])\/(0[0-9]|1[0-2])\/[0-9]{4}/;
+  if (!dateRegex.test(watchedAt)) {
+    return res.status(400)
+    .json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  if (!(Number(rate) >= 1 && Number(rate) <= 5)) {
+    return res.status(400)
+    .json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+
+  next();
+};
+
+app.post('/talker',
+ validateToken,
+ validateTalkerName,
+ validateTalkerAge,
+ validateTalk,
+ validateTalkKeys, async (req, res) => {
+  const { name, age, talk } = req.body;
+  const talkersArr = JSON.parse(await fsAsync.readFile(listTalkers, 'utf8'));
+  const availableId = talkersArr.length + 1;
+  const newTalker = { name, age, id: availableId, talk };
+  talkersArr.push(newTalker);
+  res.status(201).json(newTalker);
+  fsAsync.writeFile(listTalkers, JSON.stringify(talkersArr));
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });

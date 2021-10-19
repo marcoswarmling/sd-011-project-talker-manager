0499ddb412c956b160af8d9ed44c2c707dcb6426
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2021-10-19 09:07:12 -0300

    Todos os requisitos finalizados

--- middlewares/talker.js
+++ middlewares/talker.js
@@ -20,7 +20,7 @@ const validateName = (req, res, next) => {
 
 const validateTalkBody = (req, res, next) => {
   const { talk } = req.body;
-  if (!talk || !talk.watchedAt || !talk.rate) {
+  if (!talk || !talk.watchedAt || (!talk.rate && talk.rate !== 0)) {
      return res.status(400)
      .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
   }

f2a5424a2ef87d7f1619a64966e087369ec05880
Arlesson Moura <arlessonmss@gmail.com>
2021-10-19 05:23:46 -0300

    5°!!!!*

--- index.js
+++ index.js
@@ -153,11 +153,23 @@ app.put('/talker/:id',
   const onChangeProfileIndex = talkersArr.findIndex((talker) => Number(talker.id) === Number(id));
   const changesOnDemand = { name, age, id: Number(id), talk };
   talkersArr[onChangeProfileIndex] = changesOnDemand;
-  res.status(200).json(changesOnDemand);
   fsAsync.writeFile(listTalkers, JSON.stringify(talkersArr));
+  res.status(200).json(changesOnDemand);
 });
 
 // 6°
+
+// app.delete('/talker/:id',
+//   validateToken, async (req, res) => {
+//   const { name, age, talk } = req.body;
+//   const { id } = req.params;
+//   const talkersArr = JSON.parse(await fsAsync.readFile(listTalkers, 'utf8'));
+//   const onChangeProfileIndex = talkersArr.findIndex((talker) => Number(talker.id) === Number(id));
+//   const changesOnDemand = { name, age, id: Number(id), talk };
+//   talkersArr[onChangeProfileIndex] = changesOnDemand;
+//   res.status(200).json(changesOnDemand);
+//   fsAsync.writeFile(listTalkers, JSON.stringify(talkersArr));
+// });
 
 app.listen(PORT, () => {
   console.log('Online');

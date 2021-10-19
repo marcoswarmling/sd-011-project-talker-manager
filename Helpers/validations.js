987f334fcefb0438034f265ff9b58e33e593b2c7
Pedro Piquini <piquini.pedro@gmail.com>
2021-10-19 10:59:35 -0300

    Fix req 5, convertendo id para nmero e req 6 completo

--- Helpers/validations.js
+++ Helpers/validations.js
@@ -43,12 +43,12 @@ const talkValidation = (req, res, next) => {
 
 const rateValidation = (req, res, next) => {
   const { talk: { rate } } = req.body;
+  if (rate < 1 || rate > 5) {
+    return res.status(BAD_REQUEST).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
   if (!rate) {
     return res.status(BAD_REQUEST)
     .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
-  }
-  if (rate < 1 || rate > 5) {
-    return res.status(BAD_REQUEST).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   }
   next();
 };
--- Routes/talkerRoute.js
+++ Routes/talkerRoute.js
@@ -3,29 +3,40 @@ const routes = require('express').Router();
 const talkerController = require('../controllers/talkerController');
 const validations = require('../Helpers/validations');
 
+const dataPath = './talker.json';
+
 const CREATED = 201;
 const HTTP_OK_STATUS = 200;
 
 routes.get('/', talkerController.getAllTalkers);
 routes.get('/:id', talkerController.getTalkersID);
 routes.post('/', validations, (req, res) => {
-  const rawData = fs.readFileSync('./talker.json');
+  const rawData = fs.readFileSync(dataPath);
   const talkers = JSON.parse(rawData);
   const newTalker = req.body;
   newTalker.id = talkers.length + 1;
   talkers.push(newTalker);
-  fs.writeFileSync('./talker.json', JSON.stringify(talkers, null, 2));
+  fs.writeFileSync(dataPath, JSON.stringify(talkers, null, 2));
   res.status(CREATED).json(newTalker);
 });
 routes.put('/:id', validations, (req, res) => {
-  const rawData = fs.readFileSync('./talker.json');
+  const rawData = fs.readFileSync(dataPath);
   const talkers = JSON.parse(rawData);
   const { id } = req.params;
   const { name, age, talk } = req.body;
-  const editedTalkerIndex = talkers.findIndex((talker) => talker.id === id);
-  talkers[editedTalkerIndex] = { id, name, age, talk };
-  fs.writeFileSync('./talker.json', JSON.stringify(talkers, null, 2));
+  const editedTalkerIndex = talkers.findIndex((talker) => talker.id === +id);
+  talkers[editedTalkerIndex] = { id: +id, name, age, talk };
+  fs.writeFileSync(dataPath, JSON.stringify(talkers, null, 2));
   res.status(HTTP_OK_STATUS).json(talkers[editedTalkerIndex]);
+});
+routes.delete('/:id', validations.tokenValidation, (req, res) => {
+  const rawData = fs.readFileSync(dataPath);
+  const talkers = JSON.parse(rawData);
+  const { id } = req.params;
+  const deletedTalkerIndex = talkers.findIndex((talker) => talker.id === +id);
+  talkers.splice(deletedTalkerIndex, 1);
+  fs.writeFileSync(dataPath, JSON.stringify(talkers, null, 2));
+  res.status(HTTP_OK_STATUS).json({ message: 'Pessoa palestrante deletada com sucesso' });
 });
 
 module.exports = routes;

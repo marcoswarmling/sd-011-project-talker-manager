19a705ab2ccefbfffab3c1c4d4c3cac85c3fd548
anacarol-r-santos <kroline0693@gmail.com>
2021-10-18 22:17:24 -0300

    [FIX]desafio 1

--- index.js
+++ index.js
@@ -7,10 +7,18 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const talkerRoutes = require('./routes/talkerRoutes');
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+// começo:
+
+app.use('/talker', talkerRoutes);
+
+// final
 
 app.listen(PORT, () => {
   console.log('Online');

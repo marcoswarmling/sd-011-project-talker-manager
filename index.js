3c874e0bb97d33895711d5e55e5e2ac4b6086142
Arlesson Moura <arlessonmss@gmail.com>
2021-10-18 23:27:59 -0300

    1° e 2°

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+// const fs = require('fs');
+const fsAsync = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
+
+const listTalkers = './talker.json';
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
@@ -12,6 +16,30 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+// 1°
+app.get('/talker', (_req, res) => {
+  fsAsync.readFile(listTalkers, 'utf8')
+  .then((data) => { res.status(HTTP_OK_STATUS).json(JSON.parse(data)); })
+  .catch(() => { res.status(HTTP_OK_STATUS).json(JSON.parse([])); });
+});
+
+// 2°
+app.get('/talker/:id', (req, res) => {
+  const { id } = req.params;
+  fsAsync.readFile(listTalkers, 'utf8')
+  .then((response) => JSON.parse(response).find((talker) => talker.id === parseInt(id, 10)))
+  .then((data) => res.status(HTTP_OK_STATUS).json(data))
+  .catch(() => res.status(404).json(JSON.parse({ message: 'Pessoa palestrante não encontrada' })));
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
+
+// // app.get('/talker/:id', async (req, res) => {
+// //   const { id } = req.params;
+// //   const talkerSync = await listTalkers();
+// //   const talkerFind = talkerSync.find((talker) => talker.id === parseInt(id, 10));
+// //   if (!talkerFind) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+// //   res.status(200).json(talkerFind);
+// // });

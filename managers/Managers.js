3b64eb93b1075b1101acb8f85274d3f5c5562966
Rafael Schmitz <schimitaum@gmail.com>
2021-10-18 23:31:47 -0300

    fix. req. 5

--- managers/Managers.js
+++ managers/Managers.js
@@ -102,16 +102,17 @@ const isValidWatchedAt = (req, res, next) => {
 const isValidRate = (req, res, next) => {
   const { talk } = req.body;
 
+  if (talk.rate < 1 || talk.rate > 5) {
+    return res
+    .status(400)
+    .json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+
   if (!talk.rate) {
     return res.status(400).json({
       message:
         'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
     });
-  }
-  if (talk.rate < 1 || talk.rate > 5) {
-    return res
-    .status(400)
-    .json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   }
 
   next();

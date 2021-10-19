9d1729a36f74d386ae2177703728447d8598c4ae
igordmf <igordanielmf@gmail.com>
2021-10-18 21:32:59 -0300

    Corrige requisito 1

--- routes/talkerRouter.js
+++ routes/talkerRouter.js
@@ -4,15 +4,19 @@ const fs = require('fs');
 const router = express.Router();
 
 router.get('/', async (_req, res) => {
-  const talkers = JSON.parse(await fs.readFileSync('./talker.json', 'utf-8'));
+  const data = await fs.readFileSync('./talker.json', 'utf-8');
+  const talkers = JSON.parse(data);
+
   if (!talkers.length) return res.status(200).send([]);
+  
   return res.status(200).send(JSON.parse(talkers));
 });
 
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
 
-  const talkers = JSON.parse(await fs.readFileSync('./talker.json', 'utf-8'));
+  const data = await fs.readFileSync('./talker.json', 'utf-8');
+  const talkers = JSON.parse(data);
   const talker = talkers.find((t) => t.id === Number(id));
 
   if (!talker) return res.status(404).send({ message: 'Pessoa palestrante não encontrada' });

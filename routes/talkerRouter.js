219482e09ac622198595ece1e9d1553f8626de5a
igordmf <igordanielmf@gmail.com>
2021-10-18 21:15:13 -0300

    Requisito 2

--- routes/talkerRouter.js
+++ routes/talkerRouter.js
@@ -4,9 +4,20 @@ const fs = require('fs');
 const router = express.Router();
 
 router.get('/', async (_req, res) => {
-  const data = await fs.readFileSync('./talker.json', 'utf-8');
-  if (!JSON.parse(data).length) return res.status(200).send([]);
-  return res.status(200).send(JSON.parse(data));
+  const talkers = JSON.parse(await fs.readFileSync('./talker.json', 'utf-8'));
+  if (!talkers.length) return res.status(200).send([]);
+  return res.status(200).send(JSON.parse(talkers));
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const talkers = JSON.parse(await fs.readFileSync('./talker.json', 'utf-8'));
+  const talker = talkers.find((t) => t.id === Number(id));
+
+  if (!talker) return res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
+
+  return res.status(200).send(talker);
 });
 
 module.exports = router;

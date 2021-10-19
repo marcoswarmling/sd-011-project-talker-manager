2620b4e7e42e0fa8692085cf422b77c58bdaf18b
Rafael Schmitz <schimitaum@gmail.com>
2021-10-18 23:21:02 -0300

    req. 5

--- routes/TalkerRouter.js
+++ routes/TalkerRouter.js
@@ -11,10 +11,11 @@ const {
 } = require('../managers/Managers');
 
 const talkerRouter = express.Router();
+const PATH = './talker.json';
 
 talkerRouter.get('/', (_req, res) => {
   try {
-    const talkers = fs.readFileSync('./talker.json', 'utf-8');
+    const talkers = fs.readFileSync(PATH, 'utf-8');
     return res.status(200).json(JSON.parse(talkers));
   } catch (error) {
     return res.status(500).json({ error });
@@ -25,7 +26,7 @@ talkerRouter.get('/:id', (req, res) => {
   const { id } = req.params;
 
   try {
-    const talkers = JSON.parse(fs.readFileSync('./talker.json', 'utf-8'));
+    const talkers = JSON.parse(fs.readFileSync(PATH, 'utf-8'));
 
     const foundTalker = talkers.find((talker) => talker.id === Number(id));
 
@@ -49,15 +50,45 @@ talkerRouter.post(
     const { body } = req;
 
     try {
-      const talkers = JSON.parse(fs.readFileSync('./talker.json', 'utf-8'));
+      const talkers = JSON.parse(fs.readFileSync(PATH, 'utf-8'));
 
       const newTalker = { id: talkers[talkers.length - 1].id + 1, ...body };
 
       const updatedTalkers = [...talkers, newTalker];
 
-      fs.writeFileSync('./talker.json', JSON.stringify(updatedTalkers));
+      fs.writeFileSync(PATH, JSON.stringify(updatedTalkers));
 
       return res.status(201).json({ ...newTalker });
+    } catch (error) {
+      return res.status(500).json({ error });
+    }
+  },
+);
+
+talkerRouter.put(
+  '/:id',
+  isValidToken,
+  isValidAge,
+  isValidName,
+  isValidTalk,
+  isValidWatchedAt,
+  isValidRate,
+  (req, res) => {
+    const { id } = req.params;
+    const { body } = req;
+    try {
+      const talkers = JSON.parse(fs.readFileSync(PATH, 'utf-8'));
+
+      const filteredTalkers = talkers.filter((talker) => talker.id !== Number(id));
+
+      const updatedTalker = { id: Number(id), ...body };
+
+      const updatedTalkers = [...filteredTalkers, updatedTalker]
+        .sort((first, second) => first.id - second.id);
+
+      fs.writeFileSync(PATH, JSON.stringify(updatedTalkers));
+
+      return res.status(200).json({ ...updatedTalker });
     } catch (error) {
       return res.status(500).json({ error });
     }

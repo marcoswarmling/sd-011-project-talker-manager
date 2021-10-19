139fc5e00bb1659838f9091645fbb0b9c39e9017
Apoc35 <alexv_silveira@outlook.com>
2021-10-19 11:51:51 -0300

    feat: Last try.

--- index.js
+++ index.js
@@ -8,30 +8,18 @@ const talkerArray = './talker.json';
 const app = express();
 app.use(bodyParser.json());
 
+const CREATED = 201;
+const BAD_REQUEST = 400;
+const UNAUTHORIZED = 401;
+const NOT_FOUND = 404;
 const HTTP_OK_STATUS = 200;
+
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
-
-const checkPassword = (req, res, next) => {
-  const { password } = req.body;
-  const passwordSize = 6;
-
-  if (!password || password === '') {
-    return res.status(400)
-      .json({ message: 'O campo "password" é obrigatório' });
-  }
-
-  if (password.length < passwordSize) {
-    return res.status(400)
-      .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
-  }
-
-  next();
-};
 
 // https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail
 // Referência do regex.
@@ -41,12 +29,29 @@ const checkEmail = (req, res, next) => {
   const { email } = req.body;
 
   if (!email || email === '') {
-    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+    return res.status(BAD_REQUEST).json({ message: 'O campo "email" é obrigatório' });
   }
 
   if (!regexCheck.test(email)) {
-    return res.status(400)
+    return res.status(BAD_REQUEST)
       .json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+
+  next();
+};
+
+const checkPassword = (req, res, next) => {
+  const { password } = req.body;
+  const passwordSize = 6;
+
+  if (!password || password === '') {
+    return res.status(BAD_REQUEST)
+      .json({ message: 'O campo "password" é obrigatório' });
+  }
+
+  if (password.length < passwordSize) {
+    return res.status(BAD_REQUEST)
+      .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
 
   next();
@@ -57,11 +62,11 @@ const checkName = (req, res, next) => {
   const nameSize = 3;
 
   if (!name || name === '') {
-    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+    return res.status(BAD_REQUEST).json({ message: 'O campo "name" é obrigatório' });
   }
 
   if (name.length < nameSize) {
-    return res.status(400)
+    return res.status(BAD_REQUEST)
       .json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
   }
 
@@ -73,11 +78,11 @@ const checkAge = (req, res, next) => {
   const minAge = 18;
 
   if (!age || age === '') {
-    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+    return res.status(BAD_REQUEST).json({ message: 'O campo "age" é obrigatório' });
   }
 
   if (age < minAge) {
-    return res.status(400)
+    return res.status(BAD_REQUEST)
       .json({ message: 'A pessoa palestrante deve ser maior de idade' });
   }
   next();
@@ -87,7 +92,7 @@ const checkTalk = (req, res, next) => {
   const { talk } = req.body;
 
   if (!talk || talk === {} || !talk.watchedAt || talk.rate === undefined) {
-    return res.status(400)
+    return res.status(BAD_REQUEST)
     .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
   }
 
@@ -98,12 +103,12 @@ const checkBothRateWatchedAt = (req, res, next) => {
   const { talk } = req.body;
   const watchedDate = /^\d{1,2}\/\d{1,2}\/\d{4}$/;
   if (talk.rate < 1 || talk.rate > 5) {
-    return res.status(400)
+    return res.status(BAD_REQUEST)
       .json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   }
 
   if (!watchedDate.test(talk.watchedAt)) {
-    return res.status(400)
+    return res.status(BAD_REQUEST)
       .json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
   }
 
@@ -115,36 +120,48 @@ const checkToken = (req, res, next) => {
   const tokenSize = 16;
 
   if (!authorization) {
-    return res.status(401).json({ message: 'Token não encontrado' });
+    return res.status(UNAUTHORIZED).json({ message: 'Token não encontrado' });
   }
 
   if (authorization.length < tokenSize) {
-    return res.status(401).json({ message: 'Token inválido' });
+    return res.status(UNAUTHORIZED).json({ message: 'Token inválido' });
   }
 
   next();
 };
 
-// 1
+app.get('/talker/search', checkToken,
 
-app.get('/talker', async (_req, res) => {
-  const talkerList = await fs.readFile(talkerArray, 'utf-8');
-    return res.status(HTTP_OK_STATUS).json(JSON.parse(talkerList));
+async (req, res) => {
+  const talkers = JSON.parse(await fs.readFile(talkerArray, 'utf-8'));
+  const { q } = req.query;
+  const searchTerm = talkers.filter((t) => t.name.includes(q));
+
+  if (searchTerm === undefined || searchTerm === '') {
+    return res.status(NOT_FOUND).json(talkers);
+  }
+
+  if (!searchTerm) {
+    return res.status(NOT_FOUND).json([]);
+  }
+
+  res.status(HTTP_OK_STATUS).json(searchTerm);
 });
 
-// 2
-
 app.get('/talker/:id', async (req, res) => {
-  const talkerList = await fs.readFile(talkerArray, 'utf-8');
+  const content = await fs.readFile(talkerArray, 'utf-8');
   const { id } = req.params;
-  const talker = JSON.parse(talkerList).find((t) => t.id === +id);
+  const talker = JSON.parse(content).find((t) => t.id === +id);
 
   if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 
   res.status(HTTP_OK_STATUS).json(talker);
 });
 
-// 3
+app.get('/talker', async (_req, res) => {
+  const content = await fs.readFile(talkerArray, 'utf-8');
+  return res.status(HTTP_OK_STATUS).json(JSON.parse(content));
+});
 
 app.post('/login', checkEmail, checkPassword,
 (_req, res) => {
@@ -153,14 +170,8 @@ app.post('/login', checkEmail, checkPassword,
   return res.status(HTTP_OK_STATUS).json({ token });
 });
 
-// 4
-
-app.post('/talker',
-checkToken,
-checkName,
-checkAge,
-checkTalk,
-checkBothRateWatchedAt,
+app.post('/talker', checkToken, checkName,
+checkAge, checkTalk, checkBothRateWatchedAt,
 async (req, res) => {
   const content = JSON.parse(await fs.readFile(talkerArray, 'utf-8'));
   const { name, age, talk } = req.body;
@@ -170,7 +181,46 @@ async (req, res) => {
   const talker1 = await content[content.length - 1];
   const talkers = JSON.stringify(content);
   fs.writeFile(talkerArray, talkers);
-  return res.status(talkerArray).json(talker1);
+  return res.status(CREATED).json(talker1);
+});
+
+app.put('/talker/:id',
+checkToken,
+checkName,
+checkAge,
+checkTalk,
+checkBothRateWatchedAt,
+async (req, res) => {
+  const content = JSON.parse(await fs.readFile(talkerArray, 'utf-8'));
+  const { id } = req.params;
+  const talkerIndex = content.findIndex((c) => c.id === parseInt(id, 10));
+  const { name, age, talk } = req.body;
+  const { rate, watchedAt } = talk;
+  content[talkerIndex] = {
+    id: Number(id),
+    name,
+    age: Number(age),
+    talk: {
+      rate: Number(rate),
+      watchedAt,
+    },
+  };
+  const talkers = JSON.stringify(content);
+  await fs.writeFile(talkerArray, talkers);
+
+  return res.status(HTTP_OK_STATUS).json(content[talkerIndex]);
+});
+
+app.delete('/talker/:id', checkToken,
+
+async (req, res) => {
+  const content = JSON.parse(await fs.readFile(talkerArray));
+  const { id } = req.params;
+  const talkerIndex = content.findIndex((c) => c.id === parseInt(id, 10));
+
+  content.splice(talkerIndex, 1);
+  await fs.writeFile(talkerArray, JSON.stringify(content));
+  return res.status(HTTP_OK_STATUS).json({ message: 'Pessoa palestrante deletada com sucesso' });
 });
 
 app.listen(PORT, () => {

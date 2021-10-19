9d6b095b7333a01b04d718e234ec56a2aed9a191
Sabrina Alves Delpache <sabrinaalvesdelpache@pop-os.localdomain>
2021-10-19 12:44:56 -0300

    Requirement 5

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -40,22 +40,26 @@ const validateAge = (req, res, next) => {
     if (!age) {
       res.status(400).json({ message: 'O campo "age" é obrigatório' });
     }
-    if (!age >= validAge) {
+    if (age <= validAge) {
       res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
     }
     next();
 };
 const validateTalk = (req, res, next) => {
     const { talk } = req.body;
-    if (!talk.watchedAt || !talk.rate || !talk) {
+    if (!talk) {
       res.status(400).json({
         message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
       });
   }
+  if (!talk.watchedAt || (!talk.rate && Number(talk.rate) !== 0)) {
+    return res.status(400)
+    .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
+  }
     next();
 };
 const validateTalkItems = (req, res, next) => {
-  const dateRegex = /^(0[1-9]|[12][0-9]|3[01])[- /.]/;
+  const dateRegex = /([0-2][0-9]|3[0-1])\/(0[0-9]|1[0-2])\/[0-9]{4}/;
   const talkRateRegex = /^[1-5]$/;
   const { talk } = req.body;
   if (!dateRegex.test(talk.watchedAt)) {
